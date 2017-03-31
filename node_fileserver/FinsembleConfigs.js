54256ef7c1dee053c081190938b4751de66c8f62
Brad Carter <carterbs89@gmail.com>
2017-03-31 13:29:37 -0400

    Default workspaces are configurable.

--- node_fileserver/FinsembleConfigs.js
+++ node_fileserver/FinsembleConfigs.js
@@ -2,7 +2,8 @@
 var Finsemble = require("@chartiq/finsemble/libs/Server");
 Finsemble.addServices(require("../configs/services.json"));
 Finsemble.addComponents(require("../configs/components.json"));
-
+Finsemble.setDefaultWorkspaces(require("../configs/defaultWorkspaces.json"));
+console.log(require("../configs/defaultWorkspaces.json"));
 module.exports = function () {
     return Finsemble.FinsembleConfigs
 }
