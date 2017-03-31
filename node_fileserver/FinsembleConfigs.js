5166ee7cf4c2bedba9e6cddff803fbd0742efdd0
Brad Carter <carterbs89@gmail.com>
2017-03-31 12:42:00 -0400

    Bugfixes

--- node_fileserver/FinsembleConfigs.js
+++ node_fileserver/FinsembleConfigs.js
@@ -1,9 +1,8 @@
 
-var Finsemble = require("finsemble/libs/Server");
-console.log(Finsemble.FinsembleConfigs);
-console.log("KEYS", Object.keys(Finsemble));
-Finsemble.addServices(require("../configs/services.json"))
-Finsemble.addComponents(require("../configs/components.json"))
+var Finsemble = require("@chartiq/finsemble/libs/Server");
+Finsemble.addServices(require("../configs/services.json"));
+Finsemble.addComponents(require("../configs/components.json"));
+
 module.exports = function () {
     return Finsemble.FinsembleConfigs
 }
