400eaa34395636e2323a9aac33d732bcff1d8ba3
Brad Carter <carterbs89@gmail.com>
2017-05-02 12:45:18 -0400

    Fix finsembleConfigs

--- node_fileserver/FinsembleConfigs.js
+++ node_fileserver/FinsembleConfigs.js
@@ -2,6 +2,12 @@
 var Finsemble = require("@chartiq/finsemble/libs/Server");
 var path = require('path');
 
+//Gets your hostname/port/etc for your application.
+var StartupConfig = require("../configs/startup");
+var env = process.env.NODE_ENV ? process.env.NODE_ENV : "local";
+Finsemble.setUUID("Finsemble" + "-" + env);
+var hostname = StartupConfig[env].clientRoute;
+
 //Adding your local services/components into the project so that the Finsemble core is aware of them.
 Finsemble.addServices(require("../configs/services.json"));
 Finsemble.addComponents(require("../configs/components.json"));
@@ -13,18 +19,13 @@ Finsemble.setDefaultWorkspaces(require("../configs/defaultWorkspaces.json"));
 //**FULL PATH REQUIRED IF YOU WANT IT TO WORK WITH USER DEFINED COMPONENTS**
 //Uncomment for a McDonald's themed experience
 //Modify `./src/components/assets/css/finsemble-overrides.scss` to change the theming.
-//Finsemble.setCSSOverridesPath('http://localhost:3375/yourSubDirectory/components/assets/css/finsemble-overrides.css');
+//Finsemble.setCSSOverridesPath(StartupConfig[env].clientRoute +'/components/assets/css/finsemble-overrides.css');
 
 
 //Soon you will be able to change your default storage
 Finsemble.setDefaultStorage("localStorage");
 
 
-//Gets your hostname/port/etc for your application.
-var StartupConfig = require("../configs/startup");
-var env = process.env.NODE_ENV ? process.env.NODE_ENV : "local";
-Finsemble.setUUID("Finsemble" + "-" + env);
-var hostname = StartupConfig[env].clientRoute;
 Finsemble.updateBaseUrl(hostname);
 
 module.exports = function () {
