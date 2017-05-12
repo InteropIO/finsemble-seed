a791249881a9a6554c62a1f0a23ee8f062b6bc70
mhmcclung <mm@chartiq.com>
2017-05-12 12:06:59 -0400

    seed changes stemming from authentication testings
    1) server.js changed so files in seed will take precendence over files
    in module
    2) finsemble.css made available (same as used in module) to support auth
    example
    3) _dialogs added to finsemble-overrides.sass to match master

--- node_fileserver/server.js
+++ node_fileserver/server.js
@@ -7,8 +7,9 @@ var path = require('path');
 var finConfig = require("./FinsembleConfigs");
 //This is where express will server our system components and the components that you create.
 //It should match what's at the end of your `clientRoute` in `configs/startup.json`.
-app.use("/yourSubDirectory", express.static(path.join(__dirname, "..", '/node_modules/@chartiq/finsemble/dist')));
 app.use("/yourSubDirectory", express.static(path.join(__dirname, "..", '/built/')));
+app.use("/yourSubDirectory", express.static(path.join(__dirname, "..", '/node_modules/@chartiq/finsemble/dist')));
+
 var PORT = process.env.PORT || 3375;
 
 var server = app.listen(PORT, function () {
