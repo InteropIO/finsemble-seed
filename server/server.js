57d487d382adc1d9d26e74b804d13a3d26c312d6
mhmcclung <mm@chartiq.com>
2017-07-13 14:11:50 -0400

    more server documentation

--- server/server.js
+++ server/server.js
@@ -35,17 +35,19 @@ app.use("/hosted", express.static(path.join(__dirname, "/../hosted"), {
 	maxage: cacheAge
 }));
 
+// Can here set up you own directory path to replace "/fin" sample below -- must align with paths thoughout openfin/minifest-*.json
 app.use("/yourSubDirectory", express.static(rootDir, {
 	maxage: cacheAge
 }));
 app.use("/yourSubDirectory", express.static(path.join(__dirname, "..", '/node_modules/@chartiq/finsemble/dist')));
 global.root = "/yourSubDirectory";
 
-// Application Root
+// Sample Application Root set to "/fin" -- must align with paths thoughout openfin/minifest-*.json
 app.use("/fin", express.static(rootDir, {
 	maxage: cacheAge
 }));
 global.root = "/fin";
+
 var PORT = process.env.PORT || 3375;
 
 var server = app.listen(PORT, function () {
