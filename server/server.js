54c47952959c1b9bb7f5c9cc6383c07b6bacf598
Andy Gardner <NinjaManatee@users.noreply.github.com>
2018-03-12 17:04:30 -0400

    Add minify (#47)
    * Added UglifyJS to webpack defaults
    
    * Changed from "dev" and "prod" to "development" and "production"
    Based on expected values for express server: https://expressjs.com/en/advanced/best-practice-performance.html#set-node_env-to-production
    
    * Define default port based on startup config
    
    * Set environment in webpack

--- server/server.js
+++ server/server.js
@@ -138,7 +138,7 @@
 						});	
 					};
 
-					if (process.env.NODE_ENV === "dev") {
+					if (process.env.NODE_ENV === "development") {
 						// Setup hot reload in the dev environment
 						console.log(outputColor("start hot reload"));
 						hotReload(app, server, done);
