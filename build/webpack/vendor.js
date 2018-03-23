7b35fda9600e1dc21e00beb2022646720c3db179
Brad Carter <carterbs89@gmail.com>
2018-03-23 14:27:31 -0400

    Add comment

--- build/webpack/vendor.js
+++ build/webpack/vendor.js
@@ -1,10 +1,10 @@
+//Add to this file any modules that are used across many of your component bundles.
+
 const React = require("react"),
     ReactDom = require("react-dom"),
     FinsembleControls = require("@chartiq/finsemble-react-controls"),
     async = require("async");
-// import ReactDom from "react-dom";
-// import async from "async";
-// import * as Controls from "@chartiq/finsemble-react-controls";
+
 module.exports = {
     React, ReactDom, FinsembleControls, async
 }
