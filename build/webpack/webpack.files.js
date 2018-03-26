4e33760bc6414ebd82f16bf06ed3e10ee4045350
Brad Carter <carterbs89@gmail.com>
2018-03-26 09:59:03 -0400

    Make webpack.files.js work even if one of the entry files doesn't exist.

--- build/webpack/webpack.files.js
+++ build/webpack/webpack.files.js
@@ -1,10 +1,11 @@
 //  config settings
 const path = require('path');
 const glob_entries = require('webpack-glob-entries');
-const webpack = require("webpack")
-const componentsToBuild = Object.assign(
-	require('./webpack.finsemble-built-in.entries.json'),
-	require('./webpack.files.entries.json'));
+const webpack = require("webpack");
+const fs = require("fs");
+const builtInComponents = fs.existsSync(path.join(__dirname, 'webpack.finsemble-built-in.entries.json')) ? require('./webpack.finsemble-built-in.entries.json') : {};
+const myComponents = fs.existsSync(path.join(__dirname, 'webpack.files.entries.json')) ? require('./webpack.files.entries.json') : {};
+const componentsToBuild = Object.assign(builtInComponents, myComponents);
 const CopyWebpackPlugin = require('copy-webpack-plugin');
 const defaultConfig = require("./defaultWebpackConfig");
 let enableHMR = false,//Enable Hot Reload
