72879c97ed1c185b15c0deaa2522f86801f405cd
ChristianMay <31958969+ChristianMay@users.noreply.github.com>
2018-03-26 16:49:48 -0400

    replaced tutorials with seedFixes version (for new docs)

--- tutorials/accountList/accountList.js
+++ tutorials/accountList/accountList.js
@@ -1,11 +1,10 @@
 /* List of Customers */
-
-var customers = [
-	["12345678", "Sam Smith"],
-	["23456789", "Bill Branson"],
-	["34567890", "Tom Thomson"],
-	["45678901", "Jim Jackson"],
-	["56789012", "Ernie Edmonson"],
+customers = [
+	{acc: "12345678", name: "Sam Smith", phone: "555-555-5555"},
+	{acc: "23456789", name: "Bill Branson", phone: "555-555-5556"},
+	{acc: "34567890", name: "Tom Thomson", phone: "555-555-5557"},
+	{acc: "45678901", name: "Jim Jackson", phone: "555-555-5558"},
+	{acc: "56789012", name: "Ernie Edmonson", phone: "555-555-5559"}
 ];
 
 /**
@@ -15,13 +14,13 @@ function renderPage() {
 	var template = $("template")[0];
 	for (let customer of customers) {
 		var row = $(document.importNode(template.content, true));
-		row.find("account").text(customer[0]);
-		row.find("name").text(customer[1]);
-		row.find("account").parent().click(customer[0], clickCustomer);
-		$("body").append(row);
+		row.find("account").text(customer.acc);
+		row.find("name").text(customer.name);
+		row.find("account").parent().click(customer.acc, clickCustomer);
+		$("#customers").append(row);
 	}
 
-	var relocate = $("<button>Relocate Detail</button>");
+	var relocate = $("<relocate>Relocate Detail</relocate>");
 	relocate.click(function () {
 		relocateAccountDetail();
 	});
@@ -29,121 +28,67 @@ function renderPage() {
 }
 
 var customerIndex = -1;
-function setCustomerIndex(accountNumber) {
+function setCustomer(accountNumber) {
+	$("#customers customer").removeClass("selected");
 	var i;
 	for (i = 0; i < customers.length; i++) {
-		if (customers[i][0] === accountNumber) {
+		if (customers[i].acc === accountNumber) {
 			customerIndex = i;
+			$('#customers customer account:contains('+accountNumber+')').parent().addClass("selected");
+			break;
 		}
 	}
-	if (i == customers.length) customerIndex = 0;
 	setState();
 }
-
 
 var advancedIsRunning = false;
 var accountDetailSpawnResponse = null;
 
 function clickCustomer(event) {
-	launchAccountDetailAdvanced(event.data); // --> Step 3
-	launchAccountDetail(event.data); // --> Step 5
+
+	launchAccountDetailAdvanced(event.data); // --> Step 2.2.7
+	launchAccountDetail(event.data); // --> Step 2.2.2
 }
 
-// STEP 5
+// STEP 2.2.2
 function launchAccountDetail(selectedAccountNumber) {
 	if (advancedIsRunning) return;
-	setCustomerIndex(selectedAccountNumber);
+	setCustomer(selectedAccountNumber);
 
-	/*
-	FSBL.Clients.LauncherClient.spawn("accountDetail",
-		{
-			addToWorkspace: true,
-			left: "adjacent",
-			data: {accountNumber: selectedAccountNumber}
-		}, function(err, response){
-			console.log("spawn() returns information about the new component", response);
-			accountDetailSpawnResponse=response;
-		}
-	);
-	*/
+	// FSBL.Clients.LauncherClient.spawn("accountDetail",
+	// 	{
+	// 		addToWorkspace: true,
+	// 		left: "adjacent",
+	// 		data: {customer: customers[customerIndex]}
+	// 	}, function(err, response){
+	// 		console.log("spawn() returns information about the new component", response);
+	// 		accountDetailSpawnResponse=response;
+	// 	}
+	// );
 }
 
-// STEP 6
+// STEP 2.2.7
 function launchAccountDetailAdvanced(selectedAccountNumber) {
-	/*
-	advancedIsRunning=true;
+	// advancedIsRunning=true;
+	// setCustomer(selectedAccountNumber);
 
-	// A windowIdentifier describes a component window. We create a unique windowName by using our current window's name and appending.
-	// showWindow() will show this windowName if it's found. If not, then it will launch a new accountDetail coponent, and give it this name.
-	var windowIdentifier={
-		componentType: "accountDetail",
-		windowName: FSBL.Clients.WindowClient.options.name + ".accountDetail"
-	};
+	// var windowIdentifier={
+	// 	componentType: "accountDetail",
+	// 	windowName: FSBL.Clients.WindowClient.options.name + ".accountDetail"
+	// };
 
-	FSBL.Clients.LauncherClient.showWindow(windowIdentifier,
-		{
-			addToWorkspace: true,
-			left: "adjacent",
-			spawnIfNotFound: true,
-			data: {accountNumber: selectedAccountNumber}
-		}, function(err, response){
-			console.log("spawn() returns information about the new component", response);
-			accountDetailSpawnResponse=response;
-			// After the component is launched, or displayed, we tell the child which account number to use.
-			FSBL.Clients.RouterClient.transmit(windowIdentifier.windowName, selectedAccountNumber);
-		}
-	);
-	*/
-}
-
-// STEP 7
-/**
- * Set up a responder to respond to requests from clients.
- * When clients send requests for the next customer in the list, we will respond by traversing through the customer list and sending the response.
- */
-function communicateBetweenComponents() {
-	/*
-	FSBL.Clients.RouterClient.addResponder("accountTraversal", function (err, query) {
-		if (err) return;
-		if (query.data.action === "next") {
-			customerIndex++;
-			if (customerIndex == customers.length) customerIndex = 0;
-		} else if (query.data.action === "prev") {
-			customerIndex--;
-			if (customerIndex === -1) customerIndex = customers.length - 1;
-		}
-		setState();
-
-		// Respond to the accountDetail client with the next customer
-		query.sendQueryResponse(null, customers[customerIndex][0]);
-	});
-	*/
-}
-
-//STEP 9
-/**
- * Sets the state of a component to the Workspace
- */
-function setState() {
-	/*
-	FSBL.Clients.WindowClient.setComponentState({ field: 'customerIndex', value: customerIndex });
-	*/
-}
-
-/**
- * Gets the the stored state of a component
- */
-function getState() {
-	/*
-	FSBL.Clients.WindowClient.getComponentState({
-		field: 'customerIndex',
-	}, function (err, state) {
-		if (state === null) {
-			return;
-		}
-		customerIndex = state;
-	});
-	*/
+	// FSBL.Clients.LauncherClient.showWindow(windowIdentifier,
+	// 	{
+	// 		addToWorkspace: true,
+	// 		left: "adjacent",
+	// 		spawnIfNotFound: true,
+	// 		data: {customer: customers[customerIndex]}
+	// 	}, function(err, response){
+	// 		console.log("spawn() returns information about the new component", response);
+	// 		accountDetailSpawnResponse=response;
+	// 		FSBL.Clients.RouterClient.transmit(windowIdentifier.windowName, customers[customerIndex]);
+	// 	}
+	// );
 }
 
 function relocateAccountDetail() {
@@ -154,14 +99,48 @@ function relocateAccountDetail() {
 		});
 }
 
+//STEP 3.1
+function setState() {
+	// FSBL.Clients.WindowClient.setComponentState({ field: 'customerIndex', value: customerIndex });
+}
+
+function getState() {
+	// FSBL.Clients.WindowClient.getComponentState({
+	// 	field: 'customerIndex',
+	// }, function (err, state) {
+	// 	if (state === null) {
+	// 		return;
+	// 	}
+	// 	setCustomer(customers[state].acc);
+	// });
+}
+
+// STEP 4.1
 /**
- * Everything needs to happen after Finsemble is ready
+ * Set up a responder to respond to requests from clients.
+ * When clients send requests for the next customer in the list, we will respond by traversing through the customer list and sending the response.
  */
+function communicateBetweenComponents() {
+	// FSBL.Clients.RouterClient.addResponder("accountTraversal", function (err, query) {
+	// 	if (err) return;
+	// 	var newIndex = customerIndex;
+	// 	if (query.data.action === "next") {
+	// 		newIndex++;
+	// 		if (newIndex >= customers.length) newIndex = 0;
+	// 	} else if (query.data.action === "prev") {
+	// 		newIndex--;
+	// 		if (newIndex < 0) newIndex = customers.length - 1;
+	// 	}
+	// 	setCustomer(customers[newIndex].acc);
+	// 	query.sendQueryResponse(null, customers[customerIndex]);
+	// });
+}
+
 FSBL.addEventListener("onReady", function () {
-	//alert(FSBL.Clients.WindowClient.options.customData.component["account-type"]); // --> STEP 3
+	//alert(FSBL.Clients.WindowClient.options.customData.component["account-type"]); // --> Step 1.4
 
 	FSBL.Clients.WindowClient.setWindowTitle("Account List");
 	renderPage();
-	communicateBetweenComponents(); // --> Step 7
-	getState(); // --> Step 9
+	getState(); // --> Step 3.1
+	communicateBetweenComponents(); // --> Step 4.1
 });
