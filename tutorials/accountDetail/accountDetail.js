fcbad9995f43cfaf97c8eaa75b4b76df36b266f7
ChristianMay <31958969+ChristianMay@users.noreply.github.com>
2018-03-27 15:03:21 -0400

    replaced tutorials with seedFixes version (for new docs) (#55)
    * replaced tutorials with seedFixes version (for new docs)
    
    * adding vendor.bundle.js to tutorial components
    
    * Removed an old-tutorials comment
    
    He was the last of his kind, the sole survivor of the massacre that was. His presence and every memory of it had to be erased.

--- tutorials/accountDetail/accountDetail.js
+++ tutorials/accountDetail/accountDetail.js
@@ -1,65 +1,70 @@
+var customer = null;
 
-function setAccountNumber(accountNumber) {
-	$("input[name=accountNumber]").val(accountNumber);
-	FSBL.Clients.WindowClient.setWindowTitle(accountNumber);
-	FSBL.Clients.LinkerClient.publish({ dataType: "account", data: accountNumber }); // STEP 8
-	setState(accountNumber);
+function setCustomer(cust) {
+	customer = cust;
+	$("input[name=accountNumber]").val(customer.acc);
+	$("input[name=name]").val(customer.name);
+	$("input[name=phone]").val(customer.phone);
+	FSBL.Clients.WindowClient.setWindowTitle(customer.acc);
+	FSBL.Clients.LinkerClient.publish({ dataType: "account", data: customer.acc });
+	setState();
 }
 
-// STEP 5
+// STEP 2.2.3
 function getInitialCustomer() {
-	var accountNumber = FSBL.Clients.WindowClient.getSpawnData()["accountNumber"];
-	setAccountNumber(accountNumber);
+	var customer = FSBL.Clients.WindowClient.getSpawnData()["customer"];
+	if (customer) {
+		setCustomer(customer);
+	}
 }
 
-// STEP 6
+
+// STEP 2.2.10
 function listenForCustomer() {
 	FSBL.Clients.RouterClient.addListener(FSBL.Clients.WindowClient.options.name, function (err, response) {
 		if (err) return;
-		setAccountNumber(response.data);
+		setCustomer(response.data);
 	});
 }
 
-// STEP 7
-function communicateBetweenComponents() {
-	/*
-	$("next").click(function () {
-		FSBL.Clients.RouterClient.query("accountTraversal", { action: "next" }, function (err, response) {
-			if (err) {
-				alert("Error: " + err);
-			} else {
-				setAccountNumber(response.data);
-			}
-		});
-	});
-	*/
-}
 
-// STEP 9
-function setState(accountNumber) {
-	/*
-	FSBL.Clients.WindowClient.setComponentState({ field: 'accountNumber', value: accountNumber });
-	*/
+// STEP 3.1
+function setState() {
+	// FSBL.Clients.WindowClient.setComponentState({ field: 'customer', value: customer });
 }
 function getState() {
-	/*
-	FSBL.Clients.WindowClient.getComponentState({
-		field: 'accountNumber',
-	}, function (err, state) {
-		if (state === null) {
-			getInitialCustomer();
-			return;
-		}
-		setAccountNumber(state);
-	});
-	*/
+	// FSBL.Clients.WindowClient.getComponentState({
+	// 	field: 'customer',
+	// }, function (err, state) {
+	// 	if (state) {
+	// 		setCustomer(state);
+	// 	}
+	// });
+}
+
+// STEP 4.1
+function communicateBetweenComponents() {
+	// function traversalFn(direction) {
+	// 	console.log("traveral dir: " + direction);
+	// 	FSBL.Clients.RouterClient.query("accountTraversal", { action: direction }, function (err, response) {
+	// 		if (err) {
+	// 			alert("Error: " + err);
+	// 		} else {
+	// 			setCustomer(response.data);
+	// 		}
+	// 	});
+	// }
+	
+	// $("next").click(function() {console.log("NEXT"); traversalFn("next")});
+	// $("prev").click(function() {console.log("PREV"); traversalFn("prev")});
 }
 
 /**
  * Everything needs to happen after Finsemble is ready
  */
 FSBL.addEventListener("onReady", function () {
+	getInitialCustomer();
+	getState();
 	listenForCustomer();
 	communicateBetweenComponents();
-	getState();
 });
