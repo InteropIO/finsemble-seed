c789e40684727d02e5a28538f4434a15a152123a
Brad Carter <carterbs89@gmail.com>
2018-03-27 16:12:29 -0400

    Add minification to vendor bundle

--- build/webpack/defaultWebpackConfig.js
+++ build/webpack/defaultWebpackConfig.js
@@ -8,9 +8,7 @@ const env = process.env.NODE_ENV ? process.env.NODE_ENV : "development";
 let plugins =
 	[
 		new DefinePlugin({
-			"process.env": {
-				"NODE_ENV": JSON.stringify(env)
-			}
+			"process.env.NODE_ENV": JSON.stringify(env)
 		})
 	]
 
--- build/webpack/webpack.vendor.js
+++ build/webpack/webpack.vendor.js
@@ -1,5 +1,36 @@
 var webpack = require('webpack');
 const path = require('path');
+const env = process.env.NODE_ENV ? process.env.NODE_ENV : "development";
+const UglifyJsPlugin = require('uglifyjs-webpack-plugin');
+const { DefinePlugin } = require("webpack");
+let plugins = [
+    new webpack.DllPlugin({
+        name: 'vendor_lib',
+        path: 'build/webpack/vendor-manifest.json',
+    }),
+    new DefinePlugin({
+        "process.env.NODE_ENV": JSON.stringify(env)
+    })
+]
+
+if (env === "production") {
+	// When building the production environment, minify the code.
+	plugins.push(new UglifyJsPlugin());
+} else {
+	plugins.push(new hardSource({
+		//root dir here is "dist". Back out so we dump this file into the root.
+		cacheDirectory: '../.webpack-file-cache/[confighash]',
+		// Either an absolute path or relative to webpack's options.context.
+		// Sets webpack's recordsPath if not already set.
+		environmentHash: {
+			root: process.cwd(),
+			directories: [],
+			files: ['package-lock.json'],
+		}
+	}));
+}
+
+
 module.exports = {
     entry: {
         vendor: [path.join(__dirname, './vendor')],
@@ -9,8 +40,5 @@ module.exports = {
         path: path.join(__dirname, "../../dist"),
         library: 'vendor_lib',
     },
-    plugins: [new webpack.DllPlugin({
-        name: 'vendor_lib',
-        path: 'build/webpack/vendor-manifest.json',
-    })]
+    plugins: plugins
 };
