9d4a011891a60848ede1dbc021f739703d39ac7c
thorsent <terry@chartiq.com>
2018-04-06 16:13:10 -0400

    Removed unused resolves

--- build/webpack/defaultWebpackConfig.js
+++ build/webpack/defaultWebpackConfig.js
@@ -104,10 +104,7 @@ module.exports = class WebpackDefaults {
 			resolve: {
 				extensions: ['.js', '.jsx', '.json', 'scss', 'html'],
 				modules: [
-					'./node_modules',
-					'./src/components',
-					'./src/clients',
-					'./src/services'
+					'./node_modules'
 				],
 			}
 		}
