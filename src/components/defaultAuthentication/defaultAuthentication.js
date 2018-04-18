93c90283d96978e4b08f5bc7fcd1f78dd7e8b4cf
NinjaManatee <andy@chartiq.com>
2018-04-18 08:06:00 -0400

    cherry picking commit from dynamic-config-example branch (which has a convoluted history) to migrate to later seed verison

---
+++ src/components/defaultAuthentication/defaultAuthentication.js
@@ -0,0 +1,62 @@
+(() => {
+	"use strict";
+
+	// Load user configurations from file
+	const users = require("./users.json");
+
+	/**
+	 * Gets the configuration for a user and applies it to Finsemble application.
+	 * 
+	 * @param {string} username the username
+	 */
+	const applyUserConfig = username => {
+		// Get user configuration. Replace with get from database/remote service
+		const config = users[username];
+
+		if (!config) {
+			console.error(`No configuration found for user: ${username}`);
+			return;
+		}
+
+		// Apply the configuration to Finsemble
+		FSBL.Clients.ConfigClient.processAndSet(
+			{
+				newConfig: config,
+				overwrite: true,
+				replace: true
+			},
+			(err, config) => {
+				if (err) {
+					console.error(err);
+					return;
+				}
+
+				// Applied successfully, publish authorization and close window
+				FSBL.Clients.AuthenticationClient.publishAuthorization(username);
+				FSBL.Clients.WindowClient.getCurrentWindow().close();
+			});
+	};
+
+	window.onload = () => {
+		// Populate dropdown with available users
+		const select = document.getElementById("user");
+		Object.keys(users).forEach((key, index) => {
+			select.options[index] = new Option(key);
+		});
+
+		// Add onclick for quit button
+		document.getElementById("quit").onclick = () => {
+			FSBL.shutdownApplication();
+			FSBL.Clients.WindowClient.getCurrentWindow().close();
+		};
+
+		// Add onclick for submit button
+		document.getElementById("submit").onclick = () => {
+			// Get selected user from form
+			const username = document.getElementById("user").value;
+
+			// Apply configuration to Finsemble.
+			applyUserConfig(username);
+		};
+	};
+})()
