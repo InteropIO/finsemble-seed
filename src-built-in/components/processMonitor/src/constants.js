d767f99629e4748b754e576efed17ed16a866810
thorsent <terry@chartiq.com>
2018-05-08 14:43:04 -0400

    UI table fixes.

--- src-built-in/components/processMonitor/src/constants.js
+++ src-built-in/components/processMonitor/src/constants.js
@@ -1,5 +1,23 @@
+//Stats to parse from openfin. Comment out statistics if they interest you.
+export const statsWeCareAbout = ["uuid",
+	"processId",
+	"cpuUsage",
+	"workingSetSize",
+	"peakWorkingSetSize",
+	// "nonPagedPoolUsage",
+	// "peakNonPagedPoolUsage",
+	// "pagedPoolUsage",
+	// "peakPagedPoolUsage",
+	// "pagefileUsage",
+	// "peakPagefileUsage",
+	// "pageFaultCount"
+];
+
+/** Metrics get transformed. */
 export const NOT_A_METRIC = ["uuid", "processid"];
+//UUID is left-aligned.
 export const NOT_RIGHT_ALIGNED = ["uuid"];
+//
 export const CPU_HEADER = "cpuusage";
 export const MEMORY_HEADER = "workingsetsize";
 
@@ -14,25 +32,11 @@ export const HIGH_MEMORY_USAGE = 400;
 //so the constants above can be in MB. This is just a multiplier.
 export const TO_MB = 1000000;
 
-//Table options
+//React-table options
 export const SHOW_PAGINATION = false;
 export const COLLAPSE_ON_DATA_CHANGE = false;
 export const TABLE_CLASSES = "app-list -highlight";
 
-//Stats to parse from openfin.
-export const statsWeCareAbout = ["uuid",
-	"processId",
-	"cpuUsage",
-	"workingSetSize",
-	"peakWorkingSetSize",
-	// "nonPagedPoolUsage",
-	// "peakNonPagedPoolUsage",
-	// "pagedPoolUsage",
-	// "peakPagedPoolUsage",
-	// "pagefileUsage",
-	// "peakPagefileUsage",
-	// "pageFaultCount"
-];
 
 let emptyTotals = {};
 statsWeCareAbout.forEach(stat => {
