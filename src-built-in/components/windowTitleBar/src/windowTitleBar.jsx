1cc7d84c6adf51b5d60c34562aca3c617b82d7e0
Lauren <lauren@chartiq.com>
2018-05-09 13:40:24 -0400

    moving to making just the tab it's own component. tab class will only update once, pushing for help

--- src-built-in/components/windowTitleBar/src/windowTitleBar.jsx
+++ src-built-in/components/windowTitleBar/src/windowTitleBar.jsx
@@ -22,6 +22,7 @@ import DockingButton from "./components/right/DockingButton.jsx";
 import Maximize from "./components/right/MaximizeButton.jsx";
 import Close from "./components/right/CloseButton.jsx";
 import BringSuiteToFront from "./components/right/BringSuiteToFront.jsx";
+import Tab from './tab.jsx'
 import "../../assets/css/finsemble.css";
 
 /**
@@ -167,51 +168,16 @@ class WindowTitleBar extends React.Component {
 					<Sharer />
 				</div>
 				<div className="fsbl-header-center cq-drag"><div draggable={true} onDragStart={this.startDrag} onDragEnd={this.stopDrag} onDragExit={this.cancelDrag} onMouseOver={this.toggleDrag.bind(this, "true")} onMouseOut={this.toggleDrag.bind(this, "false")} className={this.state.titleBarIsHoveredOver==="true" ? "header-title cq-no-drag header-title-hover" : "header-title cq-no-drag"}>{self.state.windowTitle}</div></div>
+				<Tab title={self.state.windowTitle} showTabs={self.state.titleBarIsHoveredOver} />
 				<div className="fsbl-header-right">
 					<BringSuiteToFront />
 					{this.state.minButton && showMinimizeIcon ? <Minimize /> : null}
 					{showDockingIcon ? <DockingButton /> : null}
 					{this.state.maxButton ? <Maximize /> : null}
 					{this.state.closeButton ? <Close /> : null}
-
 				</div>
 			</div>
 		);
-	}
-}
-
-
-function dragElement(elmnts) {
-	let pos1 = 0, pos2 = 0;
-	for (let i = 0; i < elmnts.length; i++) {
-		elmnts[i].onmousedown = dragMouseDown;
-	}
-
-	function dragMouseDown(e) {
-		e = e || window.event;
-		// get the mouse cursor position at startup:
-		pos2 = e.clientX;
-		document.onmouseup = closeDragElement;
-		// call a function whenever the cursor moves:
-		document.onmousemove = elementDrag;
-	}
-
-	function elementDrag(e) {
-		e = e || window.event;
-		// calculate the new cursor position:
-		pos1 = pos2 - e.clientX;
-		pos2 = e.clientX;
-		// set the element's new position:
-		e.target.style.left = (e.target.offsetLeft - pos1) + "px";
-		if (e.target.style.left <= "0px") {
-			// emit the tear out event
-		}
-	}
-
-	function closeDragElement(e) {
-		/* stop moving when mouse button is released:*/
-		document.onmouseup = null;
-		document.onmousemove = null;
 	}
 }
 
