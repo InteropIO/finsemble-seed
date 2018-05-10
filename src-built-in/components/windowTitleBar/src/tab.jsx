af28bc0ee68eececdbb18355c92177669db55ebb
Lauren <lauren@chartiq.com>
2018-05-10 10:09:11 -0400

    checking in tab.jsx

---
+++ src-built-in/components/windowTitleBar/src/tab.jsx
@@ -0,0 +1,58 @@
+import React from "react";
+import ReactDOM from "react-dom";
+
+export default class Tab extends React.Component{
+	constructor(props) {
+        super(props);
+    }
+    render(){
+        return(<div className={this.props.showTabs?"tab":"tab hidden"}>
+            <div className="style">
+                <svg id='Layer_1' data-name='Layer 1' xmlns='http://www.w3.org/2000/svg'
+    viewBox='0 0 176.49 20'>
+        <polygon points='8.57 0.5 167.93 0.5 175.75 19.5 0.75 19.5 8.57 0.5' fill='#617383'
+        stroke='#67829a' strokeMiterlimit='10' />
+    </svg>
+            </div>
+            <div className="title">{this.props.title}</div>
+        </div>);
+    }
+}
+
+function dragElement(elmnts) {
+	let pos1 = 0, pos2 = 0;
+	for (let i = 0; i < elmnts.length; i++) {
+		elmnts[i].onmousedown = dragMouseDown;
+	}
+
+	function dragMouseDown(e) {
+		e = e || window.event;
+		// get the mouse cursor position at startup:
+		pos2 = e.clientX;
+		document.onmouseup = closeDragElement;
+		// call a function whenever the cursor moves:
+		document.onmousemove = elementDrag;
+	}
+
+	function elementDrag(e) {
+		e = e || window.event;
+		// calculate the new cursor position:
+		pos1 = pos2 - e.clientX;
+		pos2 = e.clientX;
+		// set the element's new position:
+		e.target.style.left = (e.target.offsetLeft - pos1) + "px";
+		if (e.target.style.left <= "0px") {
+			// emit the tear out event
+		}
+	}
+
+	function closeDragElement(e) {
+		/* stop moving when mouse button is released:*/
+		document.onmouseup = null;
+		document.onmousemove = null;
+	}
+}
+
+FSBL.addEventListener("onReady", function () {
+	dragElement(document.getElementsByClassName("tab"));
+});
