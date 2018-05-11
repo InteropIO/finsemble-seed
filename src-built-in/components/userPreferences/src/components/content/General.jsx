4f9bb7c5e70aa0fee311d5b289370f1a49f6076a
Brad Carter <carterbs89@gmail.com>
2018-05-10 20:51:18 -0400

    More comments

--- src-built-in/components/userPreferences/src/components/content/General.jsx
+++ src-built-in/components/userPreferences/src/components/content/General.jsx
@@ -1,13 +1,12 @@
 import React from 'react';
 import { Store as UserPreferencesStore } from "../../stores/UserPreferencesStore";
-import ScheduledRestart from "./ScheduledRestart";
+import ScheduledRestart from "../general/ScheduledRestart";
 export default class General extends React.Component {
     constructor(props) {
         super(props);
     }
     render() {
         return <div>
-
             <ScheduledRestart/>
          </div>
     }
