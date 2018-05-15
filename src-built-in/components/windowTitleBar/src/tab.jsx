d80579ea691dd5fc4dcecdceae4113db2ad9e028
thorsent <terry@chartiq.com>
2018-05-14 20:51:22 -0400

    Fading controls.

--- src-built-in/components/windowTitleBar/src/tab.jsx
+++ src-built-in/components/windowTitleBar/src/tab.jsx
@@ -10,9 +10,9 @@ export default class Tab extends React.Component{
 		let titleWidth=this.props.tabWidth-20;
 		return (
 			<div className="tab" style={{width:this.props.tabWidth + 'px'}}>
-				<div className="style">
-					<svg id='Layer_1' data-name='Layer 1' xmlns='http://www.w3.org/2000/svg' viewBox='0 0 176.49 20' width="100%" height="20px" preserveAspectRatio="none">
-						<polygon points='8.57 0.5 167.93 0.5 175.75 19.5 0.75 19.5 8.57 0.5' fill='#617383' stroke='#67829a' strokeMiterlimit='10' />
+				<div className="tab-style">
+					<svg id='Layer_1' data-name='Layer 1' xmlns='http://www.w3.org/2000/svg' width="100%" height="100%" preserveAspectRatio="none">
+						<polygon points='8.57,0.5 165.5,0.5 174.5,23.5 0.25,23.5 8.57,0.5' fill='#617383' stroke='#67829a' strokeMiterlimit='10' />
 					</svg>
 				</div>
 				<div className="title" style={{width:titleWidth}}>{this.props.title}</div>
