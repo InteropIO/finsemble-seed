d999ff49ae9fdcf6951b35e9cb129c204f996091
Brad Carter <carterbs89@gmail.com>
2018-06-04 12:42:21 -0400

    Add logo file

---
+++ src-built-in/components/windowTitleBar/src/components/center/logo.jsx
@@ -0,0 +1,60 @@
+import React from "react";
+export default class Logo extends React.Component {
+  constructor() {
+    super();
+    this.state = {
+      tabLogo: {}
+    }
+  }
+  componentWillReceiveProps() {
+    let wrap = FSBL.FinsembleWindow.wrap(this.props.windowIdentifier, (err, wrapper) => {
+      wrapper.getOptions((err, opts) => {
+        debugger
+        let fontIcon;
+        try {
+          fontIcon = opts.customData.foreign.components.Toolbar.iconClass;
+        } catch (e) {
+          fontIcon = "";
+        }
+
+        var imageIcon;
+        try {
+          imageIcon = opts.customData.foreign.components.Toolbar.iconURL;
+        } catch (e) {
+          imageIcon = "";
+        }
+        if (fontIcon && fontIcon != "") {
+          this.setState({
+            tabLogo: {
+              type: "icon",
+              class: "ff-" + fontIcon
+            }
+          })
+        } else if (imageIcon && imageIcon !== "") {
+          this.setState({
+            tabLogo: {
+              type: "image",
+              url: imageIcon
+            }
+          })
+        } else {
+          this.setState({
+            tabLogo: {
+              type: "icon",
+              url: "ff-grid"
+            }
+          })
+        }
+      });
+    });
+  }
+  render() {
+    return <div className="fsbl-tab-logo">
+      {this.state.tabLogo.type === "icon" &&
+        <i className={this.state.tabLogo.class}></i>
+      }
+      {this.state.tabLogo.type === "image" &&
+        <img src={this.state.tabLogo.url} />}
+    </div>
+  }
+}
