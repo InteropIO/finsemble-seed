0ac6cdf9d46e2bb6132dd3c793a4d5c8313348ff
Terry Thorsen <terry@chartiq.com>
2018-06-04 00:01:44 -0400

    Tabbing ui7060 (#91)
    * Make fake tabs, make them rectangular.
    
    * 7060 make space on right for dragging tabbed windows
    
    * Move tabRegion out into its own folder. move tabs into the center folder.
    
    * Put drag region into the tab area. Accept drops. Calculate dragLeave, etc.
    
    * Made ghostTab the proper color. Cleaned up some styling.
    
    * Figured out dragOver
    
    * Allow more than 2 tabs
    
    * Fix some z-index collisions.
    
    * Implement tab scrolling
    
    * Move events up to the parent
    
    * Fixed overflow on titles.
    
    * Reduce font-weight of roboto. Fix some idiosyncracies of the drag action. Style ghost-tab better. Don't show ghost-tab on the originating window.
    
    * Added hover-effect to let users know that they can drag the title of a window.
    
    * use window event instead of FSBL Ready in prep for some header optimization
    
    * Did some refactoring of scroll code to make it clearer.
    
    * Only render left section if there's something in there. This gets rid of teh right border.
    
    * Fix issue where margin-left was pushing the dropzone off of the screen. O_O
    
    * So much documentation
    
    * Fixed error
    
    * Cleanup
    
    * More cleanup and documentation
    
    * Should've been about 40 commits. Moved most of the logic out of the components and into the store. Added drag and drop reordering.
    
    * Remove unnecessary docs
    
    * Add gradient to the drag-areagit statusgit status!
    
    * In the middle of working on integration
    
    * Merge and fix gradient
    
    * Fix mouseWheel
    
    * Fixes to integrate with the tabbing API
    
    * Responsive header
    
    * 7021 responsive
    
    * Responsive tab sizesgit pushgit push
    
    * Fix UI
    
    * Fix dropping interaction with tiling
    
    * handle clearParent event in titleBar
    
    * Make window look to the stack's group state instead of its own.
    
    * YOu can reload and your tabs won't go awaygit status!
    
    * Close window now
    
    * Dropping out of a tab and tile thing works
    
    * Think i solved dragging issues
    
    * added tabbing action to stoptilingortabbing
    
    * Stop propogation on the tab close, so it doesn't set the tab active _after_ the tab was closed.
    
    * store changes
    
    * Code for closing a stack
    
    * Fix state issue with no tabs
    
    * Fix weird tab size when dragging
    
    * Clear tabs on clearParent
    
    * Initial header color
    
    * Icon color is blue
    
    * Made the active tab always the window that the tab list is in. This reduces flicker on visible window change.
    
    * Instrumentation, wait for remove before continuing, check groups when parentSet is called
    
    * Sped up initial group creation a little
    
    * Can remove a tab and then tab it againgit status
    
    * Get rid of 15px space for tabs.
    
    * Fixed window resizing. Adjusted tab colors.
    
    * Add terry-class
    
    * HoverDetector on tabs
    
    * HoverDetector on fsbl-header-title
    
    * Allow drag on cancel.
    
    * Fixed right border logic on tab-region-wrapper
    
    * Make sure there's always some room to grab header on single windows.
    
    * Remove local tab on cancel. fixed scrolling.
    
    * Move logs to log.
    
    * TYPO
    
    * Another log
    
    * TYPO
    
    * add action detaching
    
    * more instrumentation
    
    * Icon is blue, tab titles left-aligned.
    
    * fix for drop on scrim
    
    * Added fsbl-drag-region
    
    * Removed the flaky placeholder that would move to and fro.
    
    * Made tab updates look a little nicer.
    
    * Fix reordering two tabs.
    
    * fix tiling
    
    * Added ellipses on overflow for single tab
    
    * Don't do anything if we drop a tab onto ourselves.
    
    * remove tooltips
    
    * Remove log in the render function
    
    * Drop log level of some tab methods.
    
    * Make window title appear when tabs are disabled
    
    * Further changes to fix window titleBar when no tabs
    
    * Change assimilation version
    
    * Handle error in getStackedWindow
    
    * made right drag area responsive.
    
    * RESPONSIVE TABSgit status!
    
    * Allow reorder of active tab
    
    * Fix drop on self

---
+++ src-built-in/components/windowTitleBar/src/components/center/tab.jsx
@@ -0,0 +1,69 @@
+import React from "react";
+import ReactDOM from "react-dom";
+import HoverDetector from "../HoverDetector.jsx";
+import { FinsembleDraggable } from "@chartiq/finsemble-react-controls";
+const ICON_AREA = 29;
+const CLOSE_BUTTON_MARGIN = 22;
+/**
+ * This component is pretty basic. It just takes a bunch of props and renders them.
+ */
+export default class Tab extends React.Component {
+	constructor(props) {
+		super(props);
+		this.onDragOver = this.onDragOver.bind(this);
+
+		this.state = {
+			hoverState: "false"
+		};
+	}
+	onDragOver(e) {
+		let boundingBox = this.refs.Me.getBoundingClientRect();
+		if (this.crossedMidline(e, boundingBox)) {
+			this.props.onTabDraggedOver(e, this.props.windowIdentifier);
+		}
+	}
+	crossedMidline(e, box) {
+		return FSBL.Clients.WindowClient.isPointInBox({ x: e.nativeEvent.screenX, y: e.nativeEvent.screenY }, box);
+	}
+	hoverAction(newHoverState) {
+		this.setState({ hoverState: newHoverState });
+	}
+	render() {
+		let titleWidth = this.props.tabWidth - ICON_AREA - CLOSE_BUTTON_MARGIN;
+		let style = {
+			width: this.props.tabWidth
+		}
+		console.log("Listen for drag", this.props.listenForDragOver)
+		return (
+			<div
+				ref="Me"
+				onDrop={this.props.onDrop}
+				onClick={this.props.onClick}
+				onDragStart={(e) => {
+					this.props.onDragStart(e, this.props.windowIdentifier)
+				}}
+				onDragEnd={this.props.onDragEnd}
+				draggable={true}
+				className={this.props.className}
+				data-hover={this.state.hoverState}
+				style={style}
+				>
+				{this.props.listenForDragOver &&
+					<div className="tab-drop-region"
+					onDragOver={this.onDragOver}
+					></div>
+				}
+				<HoverDetector edge="top" hoverAction = {this.hoverAction.bind(this)} />
+				<div className="fsbl-tab-logo"><i className="ff-grid"></i></div>
+				<div className="fsbl-tab-title" style={{width: titleWidth}}>{this.props.title}</div>
+				<div className="fsbl-tab-close" onClick={(e) => {
+					e.preventDefault();
+					e.stopPropagation();
+					this.props.onTabClose(e);
+				}}>
+					<i className="ff-close"></i>
+				</div>
+			</div>
+		);
+	}
+}
--- src-built-in/components/windowTitleBar/src/windowTitleBar.jsx
+++ src-built-in/components/windowTitleBar/src/windowTitleBar.jsx
@@ -23,7 +23,7 @@ import Maximize from "./components/right/MaximizeButton.jsx";
 import Close from "./components/right/CloseButton.jsx";
 import BringSuiteToFront from "./components/right/BringSuiteToFront.jsx";
 import AlwaysOnTop from "./components/right/AlwaysOnTop.jsx";
-import Tab from './tab.jsx'
+import TabRegion from './components/center/TabRegion'
 import "../../assets/css/finsemble.css";
 
 /**
@@ -53,12 +53,16 @@ class WindowTitleBar extends React.Component {
 			maxButton: !windowTitleBarStore.getValue({ field: "Maximize.hide" }),
 			closeButton: !windowTitleBarStore.getValue({ field: "Close.hide" }),
 			showLinkerButton: windowTitleBarStore.getValue({ field: "Linker.showLinkerButton" }),
+			showShareButton: windowTitleBarStore.getValue({ field: "Sharer.emitterEnabled" }),
 			isTopRight: windowTitleBarStore.getValue({ field: "isTopRight" }),
 			alwaysOnTopButton: windowTitleBarStore.getValue({ field: "AlwaysOnTop.show" }),
-			tabWidth: 175,
 			tabs: [{ title: windowTitleBarStore.getValue({ field: "Main.windowTitle" }) }], //array of tabs for this window
-			showTabs: false
+			showTabs: false,
+			allowDragOnCenterRegion: true,
+			activeTab: FSBL.Clients.WindowClient.getWindowIdentifier(),
+			tabBarBoundingBox: {},
 		};
+
 	}
 	/**
      * This is necessary to make sure that the `this` inside of the callback is correct.
@@ -69,26 +73,28 @@ class WindowTitleBar extends React.Component {
 		this.onTitleChange = this.onTitleChange.bind(this);
 		this.onShowDockingToolTip = this.onShowDockingToolTip.bind(this);
 		this.onToggleDockingIcon = this.onToggleDockingIcon.bind(this);
-		this.onDocking = this.onDocking.bind(this);
-		this.onAlwaysOnTop = this.onAlwaysOnTop.bind(this);
+		this.onDockingEnabledChanged = this.onDockingEnabledChanged.bind(this);
+		this.onAlwaysOnTopChanged = this.onAlwaysOnTopChanged.bind(this);
 		this.showLinkerButton = this.showLinkerButton.bind(this);
 		this.isTopRight = this.isTopRight.bind(this);
-		this.toggleDrag = this.toggleDrag.bind(this);
-		this.startDrag = this.startDrag.bind(this);
-		this.stopDrag = this.stopDrag.bind(this);
-		this.cancelTabbing = this.cancelTabbing.bind(this);
-		this.onWindowResize = this.onWindowResize.bind(this);
-		this.clearDragEndTimeout = this.clearDragEndTimeout.bind(this);
+		this.allowDragOnCenterRegion = this.allowDragOnCenterRegion.bind(this);
+		this.disallowDragOnCenterRegion = this.disallowDragOnCenterRegion.bind(this);
+
+		this.onShareEmitterChanged = this.onShareEmitterChanged.bind(this);
+		this.onTabsChanged = this.onTabsChanged.bind(this);
+
 	}
 	componentWillMount() {
 		windowTitleBarStore.addListeners([
 			{ field: "Main.windowTitle", listener: this.onTitleChange },
 			{ field: "Main.showDockingTooltip", listener: this.onShowDockingToolTip },
 			{ field: "Main.dockingIcon", listener: this.onToggleDockingIcon },
-			{ field: "Main.dockingEnabled", listener: this.onDocking },
-			{ field: "AlwaysOnTop.show", listener: this.onAlwaysOnTop },
+			{ field: "Main.dockingEnabled", listener: this.onDockingEnabledChanged },
+			{ field: "AlwaysOnTop.show", listener: this.onAlwaysOnTopChanged },
 			{ field: "Linker.showLinkerButton", listener: this.showLinkerButton },
+			{ field: "Sharer.emitterEnabled", listener: this.onShareEmitterChanged },
 			{ field: "isTopRight", listener: this.isTopRight },
+			{ field: "tabs", listener: this.onTabsChanged }
 		]);
 
 		FSBL.Clients.ConfigClient.getValue({ field: "finsemble" }, (err, config) => {
@@ -97,6 +103,18 @@ class WindowTitleBar extends React.Component {
 				showTabs: typeof config['Window Manager'] !== undefined ? config['Window Manager'].showTabs : false
 			});
 		})
+
+		FSBL.Clients.RouterClient.addListener("DockingService.startTilingOrTabbing", this.disallowDragOnCenterRegion);
+		console.log("Adding listener for stopTilingOrTabbing.");
+		FSBL.Clients.RouterClient.addListener("DockingService.stopTilingOrTabbing", this.allowDragOnCenterRegion);
+		FSBL.Clients.RouterClient.addListener("DockingService.cancelTilingOrTabbing", this.allowDragOnCenterRegion);
+
+	}
+
+	componentDidMount() {
+		let header = document.getElementsByClassName("fsbl-header")[0];
+		let headerHeight = window.getComputedStyle(header, null).getPropertyValue("height");
+		document.body.style.marginTop = headerHeight;
 	}
 
 	componentWillUnmount() {
@@ -104,171 +122,177 @@ class WindowTitleBar extends React.Component {
 			{ field: "Main.windowTitle", listener: this.onTitleChange },
 			{ field: "Main.showDockingTooltip", listener: this.onShowDockingToolTip },
 			{ field: "Main.dockingIcon", listener: this.onToggleDockingIcon },
-			{ field: "Main.dockingEnabled", listener: this.onDocking },
-			{ field: "AlwaysOnTop.show", listener: this.onAlwaysOnTop },
+			{ field: "Main.dockingEnabled", listener: this.onDockingEnabledChanged },
+			{ field: "AlwaysOnTop.show", listener: this.onAlwaysOnTopChanged },
 			{ field: "Linker.showLinkerButton", listener: this.showLinkerButton },
+			{ field: "Sharer.emitterEnabled", listener: this.onShareEmitterChanged },
 			{ field: "isTopRight", listener: this.isTopRight },
+			{ field: "tabs", listener: this.onTabsChanged }
 		]);
-		window.removeEventListener('resize', this.onWindowResize);
+		console.log("Removing listener from the router.");
+		FSBL.Clients.RouterClient.removeListener("DockingService.startTilingOrTabbing", this.disallowDragOnCenterRegion);
+		FSBL.Clients.RouterClient.removeListener("DockingService.stopTilingOrTabbing", this.allowDragOnCenterRegion);
 	}
 
-	componentDidMount() {
-		let header = document.getElementsByClassName("fsbl-header")[0];
-		let headerHeight = window.getComputedStyle(header, null).getPropertyValue("height");
-		document.body.style.marginTop = headerHeight;
-		this.resize = setTimeout(this.onWindowResize, 300);
-		window.addEventListener('resize', this.onWindowResize);
+	/**
+	 * When we are not tiling/tabbing, we want to allow the user to drag the window around via any available space in the tab-region. This function allows that.
+	 */
+	allowDragOnCenterRegion() {
+		console.log("In stopTilingOrTabbing")
+		this.setState({
+			allowDragOnCenterRegion: true
+		});
+	}
+	/**
+	 * When we are tiling/tabbing, we do not want to allow any window to be dragged around and moved.
+	 */
+	disallowDragOnCenterRegion() {
+		console.log("No longer allowing drag.")
+		this.setState({
+			allowDragOnCenterRegion: false
+		});
 	}
 
+	/**
+	 * Whether the component's config allows for the linker.
+	 * @param {} err
+	 * @param {*} response
+	 */
 	showLinkerButton(err, response) {
 		//console.log("showLinkerButton--", response)
 		this.setState({ showLinkerButton: response.value });
 	}
-
+	/**
+	 * Whether the window is the top-right-most window in a group of windows. If so, it renders a minimize icon for the whole group.
+	 * @param {} err
+	 * @param {*} response
+	 */
 	isTopRight(err, response) {
 		this.setState({ isTopRight: response.value });
 	}
-
+	/**
+	 * @todo generalize. We'll need to capture title changes from every window in our stack.
+	 * @param {*} err
+	 * @param {*} response
+	 */
 	onTitleChange(err, response) {
+		let { tabs } = this.state;
+		let myIdentifier = FSBL.Clients.WindowClient.getWindowIdentifier();
+		let myIndex = -1;
+		let myTab = tabs.filter((el, i) => {
+			if (el.name === myIdentifier.name && el.uuid === myIdentifier.uuid) {
+				myIndex = i;
+				return true;
+			}
+			return false;
+		});
+		myTab.title = response.value;
+		tabs.splice(myIndex, 1, myTab);
+
 		this.setState({
 			windowTitle: response.value,
-			tabs: [{ title: windowTitleBarStore.getValue({ field: "Main.windowTitle" }) }]
+			tabs: tabs
 		});
 	}
 
+	/**
+	 * The next few methods are store change handlers that sync local state with the store's state.
+	 */
 	onShowDockingToolTip(err, response) {
 		this.setState({ showDockingTooltip: response.value });
 	}
-
 	onToggleDockingIcon(err, response) {
-		// console.log("ws docking icon change")
 		this.setState({
 			dockingIcon: response.value
 		});
 	}
-
-	onDocking(err, response) {
+	onDockingEnabledChanged(err, response) {
 		this.setState({ dockingEnabled: response.value });
 	}
-	onAlwaysOnTop(err, response) {
+	onAlwaysOnTopChanged(err, response) {
 		this.setState({ alwaysOnTopButton: response.value });
 	}
 	onStoreChanged(newState) {
 		this.setState(newState);
 	}
-
-	/**
-	 * Handles mouseover of title bar. This turns the regular title to a tab on windows that aren't already tabbing.
-	 * This function is used as a prop on HoverDetector
-	 *
-	 * @memberof windowTitleBar
-	 */
-	toggleDrag() {
+	onShareEmitterChanged(err, response) {
+		this.setState({ emitterEnabled: response.value });
 	}
 
-	/**
-	 * Function that's called when this component fires the onDragStart event, this will start the tiling or tabbing process
-	 *
-	 * @param e The SyntheticEvent created by React when the startdrag event is called
-	 * @memberof windowTitleBar
-	 */
-	startDrag(e) {
-		FSBL.Clients.WindowClient.startTilingOrTabbing({ windowIdentifier: FSBL.Clients.WindowClient.getWindowIdentifier() });
-
-	}
-
-	/**
-	 * Called when the react component detects a drop (or stop drag, which is equivalent)
-	 *
-	 * @param e The SyntheticEvent created by React when the stopdrag event is called
-	 * @memberof windowTitleBar
-	 */
-	stopDrag(e) {
-		this.mousePositionOnDragEnd = {
-			x: e.nativeEvent.screenX,
-			y: e.nativeEvent.screenY
-		}
-		this.dragEndTimeout = setTimeout(this.clearDragEndTimeout, 300);
-		FSBL.Clients.RouterClient.addListener('tabbingDragEnd', this.clearDragEndTimeout);
-	}
-
-	clearDragEndTimeout(err, response) {
-		clearTimeout(this.dragEndTimeout);
-		if (!response) {
-			FSBL.Clients.WindowClient.stopTilingOrTabbing({ mousePosition: this.mousePositionOnDragEnd });
-			this.onWindowResize();
-		}
-		FSBL.Clients.RouterClient.removeListener('tabbingDragEnd', this.clearDragEndTimeout);
-	}
-	/**
-	 * Set to a timeout. An event is sent to the RouterClient which will be handled by the drop handler on the window.
-	 * In the event that a drop handler never fires to stop tiling or tabbing, this will take care of it.
-	 *
-	 * @memberof windowTitleBar
-	 */
-	cancelTabbing() {
-		FSBL.Clients.WindowClient.stopTilingOrTabbing();
-		this.onWindowResize();
-	}
-
-	onWindowResize() {
-		this.resize = null;
-		let bounds = this.tabBar.getBoundingClientRect();
-		let toolbarRightBounds = this.toolbarRight.getBoundingClientRect();
-		let newWidth = bounds.width <= this.state.tabWidth + toolbarRightBounds.width ? ((bounds.width - 10) / this.state.tabs.length) + 10 : 175;
-		if (newWidth >= 175) newWidth = 175;
+	onTabsChanged(err, response) {
 		this.setState({
-			tabWidth: newWidth
+			tabs: response.value
 		})
 	}
 
 	render() {
 		var self = this;
-
+		const RENDER_LEFT_SECTION = this.state.showLinkerButton || this.state.showShareButton;
 		let showDockingIcon = !self.state.dockingEnabled ? false : self.state.dockingIcon;
 		let isGrouped = (self.state.dockingIcon == "ejector");
 		let showMinimizeIcon = (isGrouped && self.state.isTopRight) || !isGrouped; //If not in a group or if topright in a group
-		let titleWrapperClasses = "fsbl-header-center cq-drag";
+		let titleWrapperClasses = "fsbl-header-center";
+		let rightWrapperClasses = "fsbl-header-right cq-drag";
+		let tabRegionClasses = "fsbl-tab-area";
+		let headerClasses = "fsbl-header";
+
+		//If we're showing tabs, we throw these classes on to modify styles.
 		if (this.state.showTabs) {
-			titleWrapperClasses += " fsbl-tabs-enabled";
+			headerClasses += " fsbl-tabs-enabled";
 		}
-
+		if (this.state.tabs.length > 1) {
+			headerClasses += " fsbl-tabs-multiple";
+		}
+		//See this.allowDragOnCenterRegion for more explanation.
+		// When using tabs, we'll rely on fsbl-drag-region instead of adding cq-drag directly
+		if (this.state.allowDragOnCenterRegion && !this.state.showTabs) {
+			titleWrapperClasses += " cq-drag";
+			//tabRegionClasses += " cq-drag";
+		}
 		return (
-			<div className="fsbl-header">
-				<div className="fsbl-header-left">
-					{self.state.showLinkerButton ? <Linker /> : null}
-					<Sharer />
-				</div>
-				<div className={titleWrapperClasses} onMouseEnter={this.toggleDrag} onMouseLeave={this.toggleDrag} ref={this.setTabBarRef}>
-					<div className={"fsbl-header-title"}>{self.state.windowTitle}</div>
-
-					{this.state.showTabs &&
-						<div className={"fsbl-tab-area cq-no-drag"} draggable="true" onDragStart={this.startDrag} onDragEnd={this.stopDrag} onDrop={this.drop} ref="tabArea">
-							{this.state.tabWidth >= 55 ?
-								this.state.tabs.map((tab, i) => {
-									return <Tab key={i} tabWidth={this.state.tabWidth} title={tab.title} />
-								}) :
-								null}
-						</div>
-					}
-				</div>
-				<div className="fsbl-header-right" ref={this.setToolbarRight}>
-						{this.state.alwaysOnTopButton && showMinimizeIcon ? <AlwaysOnTop /> : null}
-						<BringSuiteToFront />
-						{this.state.minButton && showMinimizeIcon ? <Minimize /> : null}
-						{showDockingIcon ? <DockingButton /> : null}
-						{this.state.maxButton ? <Maximize /> : null}
-						{this.state.closeButton ? <Close /> : null}
+			<div className={headerClasses}>
+				{/* Only render the left section if something is inside of it. The left section has a right-border that we don't want showing willy-nilly. */}
+				{RENDER_LEFT_SECTION &&
+					<div className="fsbl-header-left">
+						{self.state.showLinkerButton ? <Linker /> : null}
+						{self.state.showShareButton ? <Sharer /> : null}
 					</div>
+				}
+				{/* center section of the titlebar */}
+				<div className={titleWrapperClasses}
+					ref={this.setTabBarRef}>
+					{/* If we're suppsoed to show tabs and the window isn't babySized */}
+					{!this.state.showTabs && <div className='fsbl-header-center cq-drag'>{this.state.windowTitle}</div>}
+					{this.state.showTabs && <div className="fsbl-drag-region"></div>}
+					{this.state.showTabs &&
+						<TabRegion
+							onTabDropped={this.allowDragOnCenterRegion}
+							className={tabRegionClasses}
+							thisWindowsTitle={this.state.windowTitle}
+							boundingBox={this.state.tabBarBoundingBox}
+							listenForDragOver={!this.state.allowDragOnCenterRegion}
+							tabs={this.state.tabs}
+							ref="tabArea"
+						/>}
+					
 				</div>
-				);
-			}
-		}
-		
-FSBL.addEventListener("onReady", function () {
-					storeExports.initialize(function () {
-						HeaderActions = storeExports.Actions;
-						windowTitleBarStore = storeExports.getStore();
-						ReactDOM.render(<WindowTitleBar />, document.getElementById("FSBLHeader"));
-					});
-				});
+				<div className={rightWrapperClasses} ref={this.setToolbarRight}>
+					{this.state.alwaysOnTopButton && showMinimizeIcon ? <AlwaysOnTop /> : null}
+					<BringSuiteToFront />
+					{this.state.minButton && showMinimizeIcon ? <Minimize /> : null}
+					{showDockingIcon ? <DockingButton /> : null}
+					{this.state.maxButton ? <Maximize /> : null}
+					{this.state.closeButton ? <Close /> : null}
+				</div>
+			</div>
+		);
+	}
+}
+
+window.addEventListener("FSBLReady", function () {
+	storeExports.initialize(function () {
+		HeaderActions = storeExports.Actions;
+		windowTitleBarStore = storeExports.getStore();
+		ReactDOM.render(<WindowTitleBar />, document.getElementById("FSBLHeader"));
+	});
+});
