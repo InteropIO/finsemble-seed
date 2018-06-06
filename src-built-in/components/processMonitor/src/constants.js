82651d0659ac8121f1f4b3cea5d3b1ed5af0b3cd
Brad Carter <carterbs89@gmail.com>
2018-06-06 10:22:29 -0400

    Forgot a couple other files

--- src-built-in/components/processMonitor/src/constants.js
+++ src-built-in/components/processMonitor/src/constants.js
@@ -1,9 +1,12 @@
 //Stats to parse from openfin. Comment out statistics if they interest you.
-export const statsWeCareAbout = ["uuid",
-	"processId",
-	"cpuUsage",
-	"workingSetSize",
-	"peakWorkingSetSize",
+export const SIMPLE_MODE_STATISTICS = [
+	{ label: "CPU", value: "cpuUsage" },
+	{ label: "Memory", value: "workingSetSize" }
+];
+export const ADVANCED_MODE_STATISTICS = [
+	{ label: "CPU", value: "cpuUsage" },
+	{ label: "Memory", value: "workingSetSize" },
+	{ label: "Peak Memory", value: "peakWorkingSetSize" }
 	// "nonPagedPoolUsage",
 	// "peakNonPagedPoolUsage",
 	// "pagedPoolUsage",
@@ -12,15 +15,8 @@ export const statsWeCareAbout = ["uuid",
 	// "peakPagefileUsage",
 	// "pageFaultCount"
 ];
-
 /** Metrics get transformed. */
-export const NOT_A_METRIC = ["uuid", "processid"];
 //UUID is left-aligned.
-export const NOT_RIGHT_ALIGNED = ["uuid"];
-//
-export const CPU_HEADER = "cpuusage";
-export const MEMORY_HEADER = "workingsetsize";
-
 //CPU is in %
 export const HIGH_CPU = 10;
 export const MODERATE_CPU_USAGE = 5;
@@ -31,19 +27,3 @@ export const HIGH_MEMORY_USAGE = 400;
 
 //so the constants above can be in MB. This is just a multiplier.
 export const TO_MB = 1000000;
-
-//React-table options
-export const SHOW_PAGINATION = false;
-export const COLLAPSE_ON_DATA_CHANGE = false;
-export const TABLE_CLASSES = "app-list -highlight";
-
-
-let emptyTotals = {};
-statsWeCareAbout.forEach(stat => {
-    if (NOT_A_METRIC.includes(stat)) {
-        emptyTotals[stat] = null;
-    } else {
-        emptyTotals[stat] = 0;
-    }
-});
-export const EMPTY_TOTALS = emptyTotals;
