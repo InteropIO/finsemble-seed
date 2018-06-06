047238f4d019e76ce7cb2fb5cf6fb76eb3d2ba64
Brad Carter <carterbs89@gmail.com>
2018-06-06 12:54:55 -0400

    Sort CWs.

--- src-built-in/components/processMonitor/src/stores/ProcessMonitorStore.js
+++ src-built-in/components/processMonitor/src/stores/ProcessMonitorStore.js
@@ -38,6 +38,14 @@ var Actions = {
 						uuid: cw.uuid
 					});
 				})
+				//Alphabetize the CWs.
+				childWindows.sort((a, b) => {
+					if (a.name.toLowerCase() < b.name.toLowerCase())
+						return -1;
+					if (a.name.toLowerCase() > b.name.toLowerCase())
+						return 1;
+					return 0;
+				});
 				procs.push({
 					statistics: proc,
 					childWindows
