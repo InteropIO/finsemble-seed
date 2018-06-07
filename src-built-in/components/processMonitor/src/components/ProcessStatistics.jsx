d64c6700290ae5b9d5e80dd779e1b4c5baf92c1b
Brad Carter <carterbs89@gmail.com>
2018-06-07 16:21:03 -0400

    Put the high/moderate colors back in there

--- src-built-in/components/processMonitor/src/components/ProcessStatistics.jsx
+++ src-built-in/components/processMonitor/src/components/ProcessStatistics.jsx
@@ -1,6 +1,6 @@
 import React from "react";
 import { Store, Actions } from "../stores/ProcessMonitorStore";
-import { SIMPLE_MODE_STATISTICS, ADVANCED_MODE_STATISTICS } from "../constants";
+import { SIMPLE_MODE_STATISTICS, ADVANCED_MODE_STATISTICS, HIGH_CPU, HIGH_MEMORY_USAGE, MODERATE_CPU_USAGE, MODERATE_MEMORY_USAGE, TO_MB } from "../constants";
 import { bytesToSize, round } from "../helpers";
 //Not used right now. Currently using alerts. This is for the future.
 export default class ProcessStatistics extends React.Component {
@@ -18,8 +18,8 @@ export default class ProcessStatistics extends React.Component {
             </div>
             <div className="process-statistics">
                 {this.props.fields.map(field => {
-                    return (<div className="statistic">
-                        {statistic(this.props.stats[field.value], field.label)}
+                    return (<div className={getClassesForStat(this.props.stats[field.value], field.label)}>
+                        {prettyPrint(this.props.stats[field.value], field.label)}
                     </div>)
                 })}
                 <div className="statistic process-actions">
@@ -32,7 +32,24 @@ export default class ProcessStatistics extends React.Component {
     }
 }
 
-function statistic(number, statType) {
+function getClassesForStat(number, statType) {
+    let classes = "statistic",
+        high_comparison = HIGH_MEMORY_USAGE * TO_MB,
+        moderate_comparison = MODERATE_MEMORY_USAGE * TO_MB;
+
+    if (statType === "CPU") {
+        high_comparison = HIGH_CPU,
+        moderate_comparison = MODERATE_CPU_USAGE;
+    }
+    debugger
+    if (number > high_comparison) {
+        classes += " high-usage"
+    } else if (number > moderate_comparison) {
+        classes += " moderate-usage";
+    }
+    return classes;
+}
+function prettyPrint(number, statType) {
     if (statType === "CPU") {
         //make it a percent.
         return round(number, 2) + "%";
