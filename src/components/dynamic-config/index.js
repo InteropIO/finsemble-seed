5f49456ff12e52c3e33c65772fa83faf36b35c5a
NinjaManatee <andy@chartiq.com>
2018-06-26 15:38:22 -0400

    Dont' require components and menus Check whether components an dmens are defined Minor format changes

--- src/components/dynamic-config/index.js
+++ src/components/dynamic-config/index.js
@@ -1,40 +1,57 @@
 const form = document.forms.configs
 
-FSBL.Clients.RouterClient.onReady(() => {
-  // Get the current configurations from localstorage
-  initialize()
-  // Attach events
-  form.addEventListener('submit', saveHandler)
+FSBL.addEventListener('onReady', () => {
+	// Get the current configurations from localstorage
+	initialize()
+
+	// Attach events
+	form.addEventListener('submit', saveHandler)
 })
 
 function saveHandler() {
-  const formData = new FormData(form)
-  const configs = {}
-  try {
-    configs.components = JSON.parse(formData.get('comps'))
-    configs.menus = JSON.parse(formData.get('menus'))
-    configs.cssOverridePath = formData.get('cssOverridePath')
-  } catch (e) {
-    alert('Invalid input.')
-    return
-  }
+	const formData = new FormData(form)
+	const configs = {}
+	try {
+		const components = formData.get('comps')
+		const menus = formData.get('menus')
+		const cssOverridePath = formData.get('cssOverridePath')
 
-  FSBL.Clients.StorageClient.save({
-      topic: 'user',
-      key: 'config',
-      value: configs
-    }, () => alert('Saved.'))
+		if (components.length > 0) {
+			configs.components = JSON.parse(components)
+		}
+
+		if (menus.length > 0) {
+			configs.menus = JSON.parse(menus)
+		}
+
+		if (cssOverridePath.length > 0) {
+			configs.cssOverridePath = cssOverridePath
+		}
+	} catch (e) {
+		alert('Invalid input.')
+		return
+	}
+
+	FSBL.Clients.StorageClient.save(
+		{
+			topic: 'user',
+			key: 'config',
+			value: configs
+		},
+		() => alert('Saved.'))
 }
 
 function initialize() {
-  FSBL.Clients.StorageClient.get({
-      topic: 'user',
-      key: 'config'},
-	  (error, data) => {
-		  if (data) {
-			  form.elements.comps.value = JSON.stringify(data.components) || ''
-			  form.elements.menus.value = JSON.stringify(data.menus) || ''
-			  form.elements.cssOverridePath.value = data.cssOverridePath || ''
-		  }
-      })
+	FSBL.Clients.StorageClient.get(
+		{
+			topic: 'user',
+			key: 'config'
+		},
+		(error, data) => {
+			if (data) {
+				form.elements.comps.value = JSON.stringify(data.components) || ''
+				form.elements.menus.value = JSON.stringify(data.menus) || ''
+				form.elements.cssOverridePath.value = data.cssOverridePath || ''
+			}
+		})
 }
