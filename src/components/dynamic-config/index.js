a36bfba6f658b2837f76a6c16026d2bd012a290d
NinjaManatee <andy@chartiq.com>
2018-06-27 15:25:52 -0400

    Add processAndSet when saving config Minor changes

--- src/components/dynamic-config/index.js
+++ src/components/dynamic-config/index.js
@@ -10,55 +10,77 @@ FSBL.addEventListener('onReady', () => {
 
 function saveHandler() {
 	const formData = new FormData(form)
-	const configs = {}
+	const newConfig = {}
 	try {
 		const components = formData.get('comps')
 		const menus = formData.get('menus')
 		const workspaces = formData.get('workspaces')
 		const cssOverridePath = formData.get('cssOverridePath')
 
-		if (components.length > 0) {
-			configs.components = JSON.parse(components)
+		if (components.length > 0) 
+		{
+			newConfig.components = JSON.parse(components)
 		}
 
 		if (menus.length > 0) {
-			configs.menus = JSON.parse(menus)
+			newConfig.menus = JSON.parse(menus)
 		}
 
 		if (workspaces.length > 0) {
-			configs.workspaces = JSON.parse(workspaces)
+			newConfig.workspaces = JSON.parse(workspaces)
 		}
 
 		if (cssOverridePath.length > 0) {
-			configs.cssOverridePath = cssOverridePath
+			newConfig.cssOverridePath = cssOverridePath
 		}
 	} catch (e) {
 		alert('Invalid input.')
 		return
 	}
 
-	FSBL.Clients.StorageClient.save(
+	// Apply configuration to Finsemble
+	FSBL.Clients.ConfigClient.processAndSet(
 		{
-			topic: 'user',
-			key: 'config',
-			value: configs
+			newConfig: newConfig,
+			overwrite: true,
+			replace: true
 		},
-		() => alert('Saved.'))
+		(err, finsemble) => {
+			if (err) {
+				alert(err);
+				return;
+			}
+
+			// Configuration successfully applied, save for user config.
+			FSBL.Clients.StorageClient.save(
+				{
+					topic: 'user',
+					key: 'config',
+					value: {
+						components: finsemble.components,
+						menus: finsemble.menus,
+						workspaces: finsemble.workspaces,
+						cssOverridePath: finsemble.cssOverridePath
+					}
+				},
+				() => alert('Saved.'))
+		}
+	)
+
 }
 
 function initialize() {
-	FSBL.Clients.ConfigClient.getValue({field:'finsemble'},
+	FSBL.Clients.ConfigClient.getValue({ field: 'finsemble' },
 		(error, data) => {
-			debugger;
 			if (error) {
 				FSBL.Clients.Logger.error(error);
 				return;
 			}
 
 			if (data) {
-				form.elements.comps.value = JSON.stringify({ components: data.components }, null, '\t') || ''
-				form.elements.menus.value = JSON.stringify({ menus: data.menus }, null, '\t') || ''
-				form.elements.workspaces.value = JSON.stringify({ workspaces: data.workspaces }, null, '\t') || ''
+				form.elements.comps.value = JSON.stringify(data.components, null, '\t') || ''
+				form.elements.menus.value = JSON.stringify(data.menus, null, '\t') || ''
+				form.elements.workspaces.value = JSON.stringify(data.workspaces, null, '\t') || ''
 				form.elements.cssOverridePath.value = data.cssOverridePath || ''
 			}
 		})
