334759cd2f80dd0d4729c4c96278690711ced2d5
Jay Thomas <jay@gfax.ch>
2018-07-19 19:29:33 -0400

    Cleanup

--- src/components/dynamic-config/main.component.js
+++ src/components/dynamic-config/main.component.js
@@ -3,7 +3,22 @@
 	const Vue = window.Vue
 
 	Vue.component('cq-main', {
-		data,
+		data: function data() {
+			return {
+				form: {
+					components: '',
+					menus: '',
+					services: '',
+					cssOverridePath: '',
+					workspaces: ''
+				},
+				// Repositories are objects containing an array of components that
+				// can be enabled or disabled. Each repository is generated from a
+				// component config and given a unique id.
+				repos: [],
+				tab: 'components'
+			}
+		},
 		methods: {
 			loadFile,
 			exportConfig,
@@ -13,7 +28,14 @@
 			submitForm,
 			toggleComponent
 		},
-		mounted,
+		mounted: function mounted() {
+			// Get the current configurations from local storage
+			this.resetForm()
+			util.getRepos().then(
+				(repos) => this.repos = repos,
+				(error) => { throw new Error(error) }
+			)
+		},
 		template: `
 			<div class="container">
 				<div id="top-buttons" class="form-group">
@@ -155,145 +177,6 @@
 		`
 	})
 
-	function data() {
-		return {
-			form: {
-				components: '',
-				menus: '',
-				services: '',
-				cssOverridePath: '',
-				workspaces: ''
-			},
-			// Repositories are objects containing an array of components that
-			// can be enabled or disabled. Each repository is generated from a
-			// component config and given a unique id.
-			repos: [],
-			tab: 'components'
-		}
-	}
-
-	function mounted() {
-		// Get the current configurations from local storage
-		this.resetForm()
-	}
-
-	function getConfigFromForm() {
-		const newConfig = {}
-		try {
-			const components = this.form.components
-			const menus = this.form.menus
-			const workspaces = this.form.workspaces
-			const cssOverridePath = this.form.cssOverridePath
-			const services = this.form.services
-
-			if (components.length > 0) {
-				newConfig.components = JSON.parse(components)
-			}
-
-			if (menus.length > 0) {
-				newConfig.menus = JSON.parse(menus)
-			}
-
-			if (workspaces.length > 0) {
-				newConfig.workspaces = JSON.parse(workspaces)
-			}
-
-			if (cssOverridePath.length > 0) {
-				newConfig.cssOverridePath = cssOverridePath
-			}
-
-			if (services.length > 0) {
-				newConfig.services = JSON.parse(services)
-			}
-		} catch (e) {
-			alert("Invalid input.")
-			return
-		}
-
-		return newConfig
-	}
-
-	function submitForm() {
-		util.applyConfig(this.form).then(
-			() => alert('Settings applied'),
-			(err) => alert(err)
-		)
-	}
-
-	function resetForm() {
-		util.getConfig().then(
-			(formData) => {
-				this.form = {
-					components: data.components,
-					cssOverridePath: data.cssOverridePath,
-					menus: JSON.stringify(data.menus, null, 4),
-					services: JSON.stringify(data.services, null, 4),
-					workspaces: JSON.stringify(data.workspaces, null, 4)
-				}
-				this.repos = []
-			},
-			(error) => FSBL.Clients.Logger.error(error)
-		)
-	}
-
-	function loadFile(event) {
-		console.log('Importing file')
-		const element = event.srcElement
-		if (!element.files || !element.files.length) {
-			return
-		}
-		const reader = new FileReader()
-		reader.onload = (event) => {
-			const configObject = JSON.parse(event.target.result)
-			console.log(configObject)
-			const [status, data] = util.generateRepo(configObject)
-			if (status === 'error') {
-				throw new Error(data.msg)
-			} else {
-				this.repos.push(data)
-			}
-		}
-		reader.readAsText(element.files[0])
-	}
-
-	function importConfig() {
-		//const importURL = formData.get("importConfig")
-		const importURL = this.form.configImportUrl
-
-		if (!importURL || (importURL.length === 0)) {
-			// No URL, return
-			return
-		}
-
-		fetch(importURL)
-			.then((res) => {
-				if (res.status !== 200) {
-					throw res
-				}
-
-				return res.json()
-			})
-			.then((data) => {
-				// Import config
-				if (data.components && (typeof (data.components)) === "object") {
-					let components = JSON.parse(components)
-					this.form.components = Object.assign(components, data.components)
-				}
-
-				if (data.services && (typeof (data.services)) === "object") {
-					let services = JSON.parse(this.form.services)
-					services = Object.assign(services, data.services)
-					this.form.services = JSON.stringify(services, null, "\t")
-				}
-			})
-			.then(() => {
-				this.form.configImportUrl = ''
-			})
-			.catch((err) => {
-				FSBL.Clients.Logger.error(err)
-				alert(`Error fetching config from ${importURL}`)
-			})
-	}
 
 	function download(filename, text) {
 		const element = document.createElement("a")
@@ -317,8 +200,137 @@
 		download("userConfig.json", configStr)
 	}
 
+	function getConfigFromForm() {
+		const newConfig = {}
+		try {
+			if (this.form.components.length > 0) {
+				newConfig.components = JSON.parse(this.form.components)
+			}
+
+			if (this.form.menus.length > 0) {
+				newConfig.menus = JSON.parse(this.form.menus)
+			}
+
+			if (this.form.workspaces.length > 0) {
+				newConfig.workspaces = JSON.parse(this.form.workspaces)
+			}
+
+			if (this.form.cssOverridePath.length > 0) {
+				newConfig.cssOverridePath = this.form.cssOverridePath
+			}
+
+			if (this.form.services.length > 0) {
+				newConfig.services = JSON.parse(this.form.services)
+			}
+		} catch (e) {
+			alert("Invalid input.")
+			return
+		}
+
+		return newConfig
+	}
+
+	function importConfig() {
+		//const importURL = formData.get("importConfig")
+		const importURL = this.form.configImportUrl
+
+		if (!importURL || (importURL.length === 0)) {
+			// No URL, return
+			return
+		}
+
+		fetch(importURL)
+			.then((res) => {
+				if (res.status !== 200) {
+					throw res
+				}
+
+				return res.json()
+			})
+			.then((data) => {
+				// Import config
+				if (data.components && (typeof (data.components)) === "object") {
+					const components = JSON.parse(components)
+					this.form.components = Object.assign(components, data.components)
+				}
+
+				if (data.services && (typeof (data.services)) === "object") {
+					let services = JSON.parse(this.form.services)
+					services = Object.assign(services, data.services)
+					this.form.services = JSON.stringify(services, null, "\t")
+				}
+			})
+			.then(() => {
+				this.form.configImportUrl = ''
+			})
+			.catch((err) => {
+				FSBL.Clients.Logger.error(err)
+				alert(`Error fetching config from ${importURL}`)
+			})
+	}
+
+	function loadFile(event) {
+		console.log('Importing file')
+		const element = event.srcElement
+		if (!element.files || !element.files.length) {
+			return
+		}
+		const reader = new FileReader()
+		reader.onload = (event) => {
+			const configObject = JSON.parse(event.target.result)
+			console.log(configObject)
+			const [status, data] = util.generateRepo(configObject)
+			if (status === 'error') {
+				throw new Error(data.msg)
+			} else {
+				this.repos.push(data)
+			}
+		}
+		reader.readAsText(element.files[0])
+	}
+
 	function removeRepo(id) {
-		this.repos = this.repos.filter(el => el.id !== id)
+		util.setRepos(this.repos.filter(el => el.id !== id)).then(
+			(repos) => this.repos = repos,
+			(error) => FSBL.Clients.Logger.error(error)
+		)
+	}
+
+	function resetForm() {
+		util.getConfig().then(
+			(data) => {
+				console.log('RESET CONFIG:', data)
+				this.form = {
+					components: data.components,
+					cssOverridePath: data.cssOverridePath,
+					menus: JSON.stringify(data.menus, null, 4),
+					services: JSON.stringify(data.services, null, 4),
+					workspaces: JSON.stringify(data.workspaces, null, 4)
+				}
+			},
+			(error) => FSBL.Clients.Logger.error(error)
+		)
+		util.getRepos().then(
+			(repos) => {
+				console.log('RESET REPOS:', repos)
+				this.repos = repos
+			},
+			(error) => {
+				console.log('OOPS:', error)
+				FSBL.Clients.Logger.error(error)
+			}
+		)
+	}
+
+	function submitForm() {
+		util.applyConfig(this.form).then(
+			() => alert('Settings applied'),
+			(error) => { throw new Error(error) }
+		)
+		util.setRepos(this.repos).then(
+			() => {},
+			(error) => { throw new Error(error) }
+		)
 	}
 
 	function toggleComponent(component) {
