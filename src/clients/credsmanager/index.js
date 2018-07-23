25d38a478d95b3915b4b8a0b04b3503806952d13
Ahmad S <trythrow@gmail.com>
2018-07-23 11:31:51 +0300

    Export created clients

---
+++ src/clients/credsmanager/index.js
@@ -0,0 +1,9 @@
+const CredsManager = require('./creds-manager')
+const FormConfig = require('./form-config')
+const Loader = require('./loader')
+
+module.exports = {
+  CredsManager,
+  FormConfig,
+  Loader
+}
