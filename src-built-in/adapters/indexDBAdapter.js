9869311b8834c4275ceec5733c804ab5e0d6c1db
Kris West <kris@chartiq.com>
2018-09-09 01:07:23 +0100

    Reimplementing with base IndexedDB API and removing Dexie

--- src-built-in/adapters/indexDBAdapter.js
+++ src-built-in/adapters/indexDBAdapter.js
@@ -6,7 +6,8 @@
  * our activeWorkspace.
  */
 import finsemble from "@chartiq/finsemble";
-import dexie from "dexie";
+
+const SCHEMA_VERSION = 1;
 
 const BaseStorage = finsemble.models.baseStorage;
 const Logger = finsemble.Clients.Logger;
@@ -17,14 +18,68 @@ Logger.start();
 
 const IndexDBAdapter = function () {
 	BaseStorage.call(this, arguments);
-
-	const db = new dexie("finsemble");
-	db.version(1).stores({
-		fsbl: "key" //only index the storage keys
-	});
+	let db;
 
 	Logger.system.debug("IndexDBAdapter init");
 	console.debug("IndexDBAdapter init");
+
+	//open the IndexedDB connection
+	let request = window.indexedDB.open("finsemble", SCHEMA_VERSION);
+	// Create the object store if necessary 
+	request.onupgradeneeded = function(event) { 
+		// Save the IDBDatabase interface 
+		db = event.target.result;
+	
+		// Create an objectStore for this database
+		var objectStore = db.createObjectStore("fsbl", { keyPath: "key" });
+
+		//create an index on key
+		//objectStore.createIndex("key", "key", { unique: true });
+
+		// Use transaction oncomplete to make sure the objectStore creation is 
+		// finished before adding data into it.
+		objectStore.transaction.oncomplete = function(event) {
+			Logger.system.debug("IndexDBAdapter object store created");
+			console.debug("IndexDBAdapter object store created");
+			
+			// // Store values in the newly created objectStore.
+			// var customerObjectStore = db.transaction("customers", "readwrite").objectStore("customers");
+			// customerData.forEach(function(customer) {
+			// customerObjectStore.add(customer);
+			// });
+		};
+
+	};
+	request.onerror = function(event) {
+		Logger.system.error("IndexDBAdapter DB connection initialisation failed, Error: ", event);
+		console.error("IndexDBAdapter DB connection initialisation failed, Error: ", event);
+	};
+	request.onsuccess = function(event) {
+		db = event.target.result;
+		Logger.system.debug("IndexDBAdapter initialised successfully");
+		console.debug("IndexDBAdapter initialised successfully");
+	};
+
+	//PolyFill IDBKeyRange for a key prefix search
+	IDBKeyRange.forPrefix = function(prefix) {
+		var upperKey = successor(prefix);
+		if (upperKey === undefined)
+		  return IDBKeyRange.lowerBound(prefix);
+		return IDBKeyRange.bound(prefix, upperKey, false, true);
+	  
+		function successor(key) {
+			var len = key.length;
+			while (len > 0) {
+			  var head = key.substring(0, len - 1),
+				  tail = key.charCodeAt(len - 1);
+			  if (tail !== 0xFFFF)
+				return head + String.fromCharCode(tail + 1);
+			  key = head;
+			  --len;
+			}
+			return UPPER_BOUND.STRING;		  
+		}
+	};
 
 	/**
 	 * Save method.
@@ -39,19 +94,17 @@ const IndexDBAdapter = function () {
 		Logger.system.debug("IndexDBAdapter.save, params: ", params);
 		console.debug("IndexDBAdapter.save, params: ", params);
 		const combinedKey = this.getCombinedKey(this, params);
-
-		dexie
-			.spawn(function* () {
-				yield db.fsbl.put({ key: combinedKey, value: params.value });
-			})
-			.then(() => {
-				// spawn() returns a promise that completes when all is done.
-				return cb(null, { status: "success" });
-			})
-			.catch((err) => {
-				console.error("Failed: " + JSON.stringify(err, null, "\t"));
-				return cb(err, { status: "failed" });
-			});
+		
+		var objectStore = db.transaction(["fsbl"], "readwrite").objectStore("fsbl");
+		var request = objectStore.put({ key: combinedKey, value: params.value });
+		request.onsuccess = function(event) {
+			return cb(null, { status: "success" });
+		};
+		request.onerror = function(event) {
+			console.error("IndexDBAdapter.save Request Failed: " + JSON.stringify(err, null, "\t"));
+			return cb(err, { status: "failed" });
+		};
+		
 	};
 
 	/**
@@ -68,22 +121,22 @@ const IndexDBAdapter = function () {
 
 		const combinedKey = this.getCombinedKey(this, params);
 
-		dexie
-			.spawn(function* () {
-				const val = yield db.fsbl
-					.where("key")
-					.equals(combinedKey)
-					.first();
-				
-				const data = val && val.value ? val.value : {};
-				Logger.system.debug("IndexDBAdapter.get for key=" + combinedKey + " data=", data);
-				console.debug("IndexDBAdapter.get for key=" + combinedKey + " data=", data);
-				cb(null, data);
-			})
-			.catch((err) => {
-				Logger.system.error("IndexDBAdapter.get Error", err, "key=" + combinedKey);
-				return cb(err, { status: "failed" });
-			});
+		var objectStore = db.transaction(["fsbl"], "readwrite").objectStore("fsbl");
+		var request = objectStore.get(combinedKey);
+		request.onerror = function(event) {
+			Logger.system.error("IndexDBAdapter.get key=" + combinedKey + ", Error", event);
+			console.error("IndexDBAdapter.get key=" + combinedKey + ", Error", event);
+			return cb(err, { status: "failed" });
+		};
+		request.onsuccess = function(event) {
+			let data = {};
+			if ( event.target.result && event.target.result.value) {
+				data = event.target.result.value;
+			}
+			Logger.system.debug("IndexDBAdapter.get for key=" + combinedKey + " data=", data);
+			console.debug("IndexDBAdapter.get for key=" + combinedKey + " data=", data);
+			cb(null, data);
+		};
 	};
 
 	/**
@@ -117,19 +170,23 @@ const IndexDBAdapter = function () {
 	this.keys = (params, cb) => {
 		let keys = [];
 		const keyPreface = this.getKeyPreface(this, params);
-
-		dexie
-			.spawn(function* () {
-				keys = yield db.fsbl.where("key").startsWith(keyPreface).primaryKeys();
-				Logger.system.debug("IndexDBAdapter.keys for keyPreface=" + keyPreface + " with keys=", keys);
-				console.debug("IndexDBAdapter.keys for keyPreface=" + keyPreface + " with keys=", keys);
-				cb(null, keys);
-			})
-			.catch((err) => {
-				Logger.system.error("Failed to retrieve IndexDBAdapter.keys Error", err, "key=" + combinedKey);
-				console.error("Failed to retrieve IndexDBAdapter.keys Error", err, "key=" + combinedKey);
-				return cb(err, { status: "failed" });
-			});
+		let keyRange = IDBKeyRange.forPrefix(keyPreface);
+		var objectStore = db.transaction(["fsbl"], "readwrite").objectStore("fsbl");
+		var request = objectStore.getAllKeys(keyRange);
+		request.onerror = function(event) {
+			Logger.system.error("Failed to retrieve IndexDBAdapter.keys keyPreface=" + keyPreface + ", Error", event);
+			console.error("Failed to retrieve IndexDBAdapter.keys keyPreface=" + keyPreface + ", Error", event);
+			return cb(err, { status: "failed" });
+		};
+		request.onsuccess = function(event) {
+			let data = [];
+			if ( event.target.result && event.target.result.value) {
+				data = event.target.result;
+			}
+			Logger.system.debug("IndexDBAdapter.keys for keyPreface=" + keyPreface + " keys=", data);
+			console.debug("IndexDBAdapter.get keys keyPreface=" + keyPreface + " keys=", data);
+			cb(null, data);
+		};
 	};
 
 	/**
@@ -143,15 +200,17 @@ const IndexDBAdapter = function () {
 		const combinedKey = this.getCombinedKey(this, params);
 		Logger.system.debug("IndexDBAdapter.delete for key=" + combinedKey);
 		console.debug("IndexDBAdapter.delete for key=" + combinedKey);
-		dexie
-			.spawn(function* () {
-				yield db.fsbl.delete(combinedKey);
-				cb(null, { status: "success" });
-			})
-			.catch((err) => {
-				Logger.system.error("IndexDBAdapter.delete Error", err, "key=" + combinedKey);
-				cb(err, { status: "failed" });
-			});
+
+		var objectStore = db.transaction(["fsbl"], "readwrite").objectStore("fsbl");
+		var request = objectStore.delete(combinedKey);
+		request.onerror = function(event) {
+			Logger.system.error("IndexDBAdapter.delete key=" + combinedKey + ", Error", event);
+			console.error("IndexDBAdapter.delete key=" + combinedKey + ", Error", event);
+			return cb(event, { status: "failed" });
+		};
+		request.onsuccess = function(event) {
+			cb(null, { status: "success" });
+		};
 	};
 
 	/**
@@ -160,19 +219,20 @@ const IndexDBAdapter = function () {
 	 */
 	this.clearCache = (params, cb) => {
 		const userPreface = this.getUserPreface(this);
-
-		dexie
-			.spawn(function* () {
-				Logger.system.debug("IndexDBAdapter.clearCache for userPreface=" + userPreface);
-				console.debug("IndexDBAdapter.clearCache for userPreface=" + userPreface);
-				yield db.fsbl.where("key").startsWith(userPreface).delete();
-				cb();
-			})
-			.catch((err) => {
-				Logger.system.debug("IndexDBAdapter.clearCache failed Error", err, "userPreface=" + userPreface);
-				console.debug("IndexDBAdapter.clearCache failed Error", err, "userPreface=" + userPreface);
-				cb(err, { status: "failed" });
-			});
+		Logger.system.debug("IndexDBAdapter.clearCache for userPreface=" + userPreface);
+		console.debug("IndexDBAdapter.clearCache for userPreface=" + userPreface);
+				
+		let keyRange = IDBKeyRange.forPrefix(userPreface);
+		var objectStore = db.transaction(["fsbl"], "readwrite").objectStore("fsbl");
+		var request = objectStore.delete(keyRange);
+		request.onerror = function(event) {
+			Logger.system.debug("IndexDBAdapter.clearCache failed Error", event, "userPreface=" + userPreface);
+			console.debug("IndexDBAdapter.clearCache failed Error", event, "userPreface=" + userPreface);
+			return cb(event, { status: "failed" });
+		};
+		request.onsuccess = function(event) {
+			cb();
+		};
 	};
 
 	/**
@@ -180,18 +240,18 @@ const IndexDBAdapter = function () {
 	 * @param {function} cb
 	 */
 	this.empty = (cb) => {
-		dexie
-			.spawn(function* () {
-				yield db.fsbl.clear();
-				Logger.system.debug("IndexDBAdapter.empty");
-				console.debug("IndexDBAdapter.empty");
-				cb(null, { status: "success" });
-			})
-			.catch((err) => {
-				Logger.system.debug("IndexDBAdapter.empty failed Error", err);
-				console.debug("IndexDBAdapter.empty failed Error", err);
-				cb(err, { status: "failed" });
-			});
+		Logger.system.debug("IndexDBAdapter.empty");
+		console.debug("IndexDBAdapter.empty");
+		var objectStore = db.transaction(["fsbl"], "readwrite").objectStore("fsbl");
+		var request = objectStore.clear();
+		request.onerror = function(event) {
+			Logger.system.debug("IndexDBAdapter.empty failed Error", event);
+			console.debug("IndexDBAdapter.empty failed Error", event);
+			return cb(event, { status: "failed" });
+		};
+		request.onsuccess = function(event) {
+			cb();
+		};
 	};
 };
 
