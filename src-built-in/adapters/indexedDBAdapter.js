4a618814a5d0fc124acf65c7e96767f2a149034a
NinjaManatee <andy@chartiq.com>
2018-09-10 16:43:42 -0400

    Collected Interface metods together

--- src-built-in/adapters/indexedDBAdapter.js
+++ src-built-in/adapters/indexedDBAdapter.js
@@ -43,22 +43,24 @@ IDBKeyRange.forPrefix = (prefix) => {
  * our activeWorkspace.
  */
 const IndexedDBAdapter = function () {
+	// #region Initializes a new instance of the IndexedDBAdapter.
 	BaseStorage.call(this, arguments);
-	let db;
+	this.db;
 
 	Logger.system.debug("IndexedDBAdapter init");
 	console.debug("IndexedDBAdapter init");
 
+	// #region Initialize IndexedDB connection.
 	// Open the IndexedDB connection
 	const request = window.indexedDB.open("finsemble");
 
 	// Create the object store if necessary 
 	request.onupgradeneeded = (event) => {
 		// Save the IDBDatabase interface 
-		db = event.target.result;
+		this.db = event.target.result;
 
 		// Create an objectStore for this database
-		const objectStore = db.createObjectStore("fsbl", { keyPath: "key" });
+		const objectStore = this.db.createObjectStore("fsbl", { keyPath: "key" });
 
 		// Use transaction oncomplete to make sure the objectStore creation is 
 		// finished before adding data into it.
@@ -66,52 +68,133 @@ const IndexedDBAdapter = function () {
 			Logger.system.debug("IndexedDBAdapter object store created");
 			console.debug("IndexedDBAdapter object store created");
 		};
-
 	};
 
 	request.onsuccess = (event) => {
 		Logger.system.debug("IndexedDBAdapter initialized successfully");
 		console.debug("IndexedDBAdapter initialized successfully");
 
-		db = event.target.result;
+		this.db = event.target.result;
 	};
 
 	request.onerror = (err) => {
 		Logger.system.error("IndexedDBAdapter DB connection initialization failed, Error: ", err);
 		console.error("IndexedDBAdapter DB connection initialization failed, Error: ", err);
 	};
+	// #endregion
+	// #endregion
 
 	/**
-	 * Save method.
+	 * Get the prefix used to filter keys for particular topics and key prefixes.
+	 * 
+	 * @param {object} params 
+	 * @param {string} params.topic The topic
+	 * @param {string} params.keyPrefix The key prefix (optional).
+	 * @private
+	 */
+	this.getKeyPreface = (params) => {
+		const keyPrefix = "keyPrefix" in params ? params.keyPrefix : "";
+		const preface = `${this.getUserPreface()}:${params.topic}:${keyPrefix}`;
+
+		return preface;
+	}
+
+	/**
+	 * Get prefix for all the users stored data.
+	 * @private
+	 */
+	this.getUserPreface = () => {
+		const preface = `${this.baseName}:${this.userName}`;
+		return preface;
+	}
+
+	// #region Interface Methods
+	/**
+	 * This method should be used very, very judiciously. It's essentially a method designed to wipe the database for a 
+	 * particular user.
+	 */
+	this.clearCache = (params, cb) => {
+		const userPreface = this.getUserPreface(this);
+
+		Logger.system.debug("IndexedDBAdapter.clearCache for userPreface=" + userPreface);
+		console.debug("IndexedDBAdapter.clearCache for userPreface=" + userPreface);
+
+		const keyRange = IDBKeyRange.forPrefix(userPreface);
+		const objectStore = this.db.transaction(["fsbl"], "readwrite").objectStore("fsbl");
+		const request = objectStore.delete(keyRange);
+
+		request.onsuccess = () => {
+			Logger.system.debug("IndexedDBAdapter.clearCache Success: userPreface=" + userPreface);
+			console.debug("IndexedDBAdapter.clearCache Success: userPreface=" + userPreface);
+
+			cb();
+		};
+
+		request.onerror = (err) => {
+			Logger.system.error("IndexedDBAdapter.clearCache failed Error", err, "userPreface=" + userPreface);
+			console.error("IndexedDBAdapter.clearCache failed Error", err, "userPreface=" + userPreface);
+
+			return cb(err, { status: "failed" });
+		};
+	}
+
+	/**
+	 * Delete method.
 	 * 
 	 * @param {object} params
 	 * @param {string} params.topic A topic under which the data should be stored.
-	 * @param {string} params.key The key whose value is being set.
-	 * @param {any} params.value The value being saved.
-	 * @param {function} cb callback to be invoked upon save completion
+	 * @param {string} params.key The key whose value is being deleted.
+	 * @param {function} cb callback to be invoked upon completion
 	 */
-	this.save = (params, cb) => {
-		Logger.system.debug("IndexedDBAdapter.save, params: ", params);
-		console.debug("IndexedDBAdapter.save, params: ", params);
-
+	this.delete = (params, cb) => {
 		const combinedKey = this.getCombinedKey(this, params);
-		const objectStore = db.transaction(["fsbl"], "readwrite").objectStore("fsbl");
-		const request = objectStore.put({ key: combinedKey, value: params.value });
+
+		Logger.system.debug("IndexedDBAdapter.delete for key=" + combinedKey);
+		console.debug("IndexedDBAdapter.delete for key=" + combinedKey);
+
+		const objectStore = this.db.transaction(["fsbl"], "readwrite").objectStore("fsbl");
+		const request = objectStore.delete(combinedKey);
 
 		request.onsuccess = () => {
-			Logger.system.debug("IndexedDBAdapter.save Request Succeeded");
-			console.debug("IndexedDBAdapter.save Request Succeeded");
+			Logger.system.debug("IndexedDBAdapter.delete key=" + combinedKey + ", Success");
+			console.debug("IndexedDBAdapter.delete key=" + combinedKey + ", Success");
 
 			cb(null, { status: "success" });
 		};
 
 		request.onerror = (err) => {
-			Logger.system.error("IndexedDBAdapter.save Request Failed: ", err);
-			console.error("IndexedDBAdapter.save Request Failed: ", err);
+			Logger.system.error("IndexedDBAdapter.delete key=" + combinedKey + ", Error", err);
+			console.error("IndexedDBAdapter.delete key=" + combinedKey + ", Error", err);
 
-			cb(err, { status: "failed" });
+			return cb(err, { status: "failed" });
 		};
-	};
+	}
+
+	/**
+	 * Wipes the storage container.
+	 * @param {function} cb
+	 */
+	this.empty = (cb) => {
+		Logger.system.debug("IndexedDBAdapter.empty");
+		console.debug("IndexedDBAdapter.empty");
+
+		const objectStore = this.db.transaction(["fsbl"], "readwrite").objectStore("fsbl");
+		const request = objectStore.clear();
+
+		request.onerror = (err) => {
+			Logger.system.error("IndexedDBAdapter.empty failed Error", err);
+			console.error("IndexedDBAdapter.empty failed Error", err);
+
+			return cb(err, { status: "failed" });
+		};
+
+		request.onsuccess = () => {
+			Logger.system.debug("IndexedDBAdapter.empty Success");
+			console.debug("IndexedDBAdapter.empty Success");
+
+			cb();
+		};
+	}
 
 	/**
 	 * Get method.
@@ -126,7 +209,7 @@ const IndexedDBAdapter = function () {
 		console.debug("IndexedDBAdapter.get, params: ", params);
 
 		const combinedKey = this.getCombinedKey(this, params);
-		const objectStore = db.transaction(["fsbl"], "readwrite").objectStore("fsbl");
+		const objectStore = this.db.transaction(["fsbl"], "readwrite").objectStore("fsbl");
 		const request = objectStore.get(combinedKey);
 
 		request.onsuccess = (event) => {
@@ -144,29 +227,7 @@ const IndexedDBAdapter = function () {
 
 			cb(err, { status: "failed" });
 		};
-	};
-
-	/**
-	 * Get the prefix used to filter keys for particular topics and key prefixes.
-	 * 
-	 * @param {object} params 
-	 * @param {string} params.topic The topic
-	 * @param {string} params.keyPrefix The key prefix (optional).
-	 */
-	this.getKeyPreface = (params) => {
-		const keyPrefix = "keyPrefix" in params ? params.keyPrefix : "";
-		const preface = `${this.getUserPreface()}:${params.topic}:${keyPrefix}`;
-
-		return preface;
-	};
-
-	/**
-	 * Get prefix for all the users stored data.
-	 */
-	this.getUserPreface = () => {
-		const preface = `${this.baseName}:${this.userName}`;
-		return preface;
-	};
+	}
 
 	/**
 	 * Returns all keys stored in IndexDB.
@@ -177,7 +238,7 @@ const IndexedDBAdapter = function () {
 	this.keys = (params, cb) => {
 		const keyPreface = this.getKeyPreface(params);
 		const keyRange = IDBKeyRange.forPrefix(keyPreface);
-		const objectStore = db.transaction(["fsbl"], "readwrite").objectStore("fsbl");
+		const objectStore = this.db.transaction(["fsbl"], "readwrite").objectStore("fsbl");
 		const request = objectStore.getAllKeys(keyRange);
 
 		request.onsuccess = (event) => {
@@ -195,95 +256,41 @@ const IndexedDBAdapter = function () {
 
 			return cb(err, { status: "failed" });
 		};
-	};
+	}
 
 	/**
-	 * Delete method.
+	 * Save method.
 	 * 
 	 * @param {object} params
 	 * @param {string} params.topic A topic under which the data should be stored.
-	 * @param {string} params.key The key whose value is being deleted.
-	 * @param {function} cb callback to be invoked upon completion
+	 * @param {string} params.key The key whose value is being set.
+	 * @param {any} params.value The value being saved.
+	 * @param {function} cb callback to be invoked upon save completion
 	 */
-	this.delete = (params, cb) => {
+	this.save = (params, cb) => {
+		Logger.system.debug("IndexedDBAdapter.save, params: ", params);
+		console.debug("IndexedDBAdapter.save, params: ", params);
+
 		const combinedKey = this.getCombinedKey(this, params);
-
-		Logger.system.debug("IndexedDBAdapter.delete for key=" + combinedKey);
-		console.debug("IndexedDBAdapter.delete for key=" + combinedKey);
-
-		const objectStore = db.transaction(["fsbl"], "readwrite").objectStore("fsbl");
-		const request = objectStore.delete(combinedKey);
+		const objectStore = this.db.transaction(["fsbl"], "readwrite").objectStore("fsbl");
+		const request = objectStore.put({ key: combinedKey, value: params.value });
 
 		request.onsuccess = () => {
-			Logger.system.debug("IndexedDBAdapter.delete key=" + combinedKey + ", Success");
-			console.debug("IndexedDBAdapter.delete key=" + combinedKey + ", Success");
+			Logger.system.debug("IndexedDBAdapter.save Request Succeeded");
+			console.debug("IndexedDBAdapter.save Request Succeeded");
 
 			cb(null, { status: "success" });
 		};
 
 		request.onerror = (err) => {
-			Logger.system.error("IndexedDBAdapter.delete key=" + combinedKey + ", Error", err);
-			console.error("IndexedDBAdapter.delete key=" + combinedKey + ", Error", err);
+			Logger.system.error("IndexedDBAdapter.save Request Failed: ", err);
+			console.error("IndexedDBAdapter.save Request Failed: ", err);
 
-			return cb(err, { status: "failed" });
+			cb(err, { status: "failed" });
 		};
-	};
-
-	/**
-	 * This method should be used very, very judiciously. It's essentially a method designed to wipe the database for a 
-	 * particular user.
-	 */
-	this.clearCache = (params, cb) => {
-		const userPreface = this.getUserPreface(this);
-
-		Logger.system.debug("IndexedDBAdapter.clearCache for userPreface=" + userPreface);
-		console.debug("IndexedDBAdapter.clearCache for userPreface=" + userPreface);
-
-		const keyRange = IDBKeyRange.forPrefix(userPreface);
-		const objectStore = db.transaction(["fsbl"], "readwrite").objectStore("fsbl");
-		const request = objectStore.delete(keyRange);
-
-		request.onsuccess = () => {
-			Logger.system.debug("IndexedDBAdapter.clearCache Success: userPreface=" + userPreface);
-			console.debug("IndexedDBAdapter.clearCache Success: userPreface=" + userPreface);
-
-			cb();
-		};
-
-		request.onerror = (err) => {
-			Logger.system.error("IndexedDBAdapter.clearCache failed Error", err, "userPreface=" + userPreface);
-			console.error("IndexedDBAdapter.clearCache failed Error", err, "userPreface=" + userPreface);
-
-			return cb(err, { status: "failed" });
-		};
-	};
-
-	/**
-	 * Wipes the storage container.
-	 * @param {function} cb
-	 */
-	this.empty = (cb) => {
-		Logger.system.debug("IndexedDBAdapter.empty");
-		console.debug("IndexedDBAdapter.empty");
-
-		const objectStore = db.transaction(["fsbl"], "readwrite").objectStore("fsbl");
-		const request = objectStore.clear();
-
-		request.onerror = (err) => {
-			Logger.system.error("IndexedDBAdapter.empty failed Error", err);
-			console.error("IndexedDBAdapter.empty failed Error", err);
-
-			return cb(err, { status: "failed" });
-		};
-
-		request.onsuccess = () => {
-			Logger.system.debug("IndexedDBAdapter.empty Success");
-			console.debug("IndexedDBAdapter.empty Success");
-
-			cb();
-		};
-	};
-};
+	}
+	// #endregion
+}
 
 IndexedDBAdapter.prototype = new BaseStorage();
 new IndexedDBAdapter("IndexedDBAdapter");
