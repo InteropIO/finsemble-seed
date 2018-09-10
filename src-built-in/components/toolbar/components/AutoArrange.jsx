90991feb661ed25d04161fbd3040ab2fb82cedd1
sidd <sidd@chartiq.com>
2018-09-10 09:05:19 -0400

    fix initial autoarrange error

--- src-built-in/components/toolbar/components/AutoArrange.jsx
+++ src-built-in/components/toolbar/components/AutoArrange.jsx
@@ -14,7 +14,7 @@ export default class AutoArrange extends React.Component {
 		}, (err, monitorInfo) => {
 			FSBL.Clients.RouterClient.subscribe('DockingService.AutoarrangeStatus', function (err, response) {
 				self.setState({
-					isAutoArranged: response.data.isAutoArranged[monitorInfo.name]
+					isAutoArranged: response.data.isAutoArranged && response.data.isAutoArranged[monitorInfo.name]
 				});
 			});
 		})
