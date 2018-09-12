23c384e9c7a01630d3da4dd110f4dbb2f39fff69
NinjaManatee <andy@chartiq.com>
2018-09-12 15:51:06 -0400

    Minor reformatting

--- src-built-in/adapters/indexedDBAdapter.js
+++ src-built-in/adapters/indexedDBAdapter.js
@@ -14,15 +14,23 @@ Logger.start();
 const SCHEMA_VERSION = 1;
 
 // #region 
-/** PolyFill IDBKeyRange for a key prefix search. In IndexedDB, Primary keys are ordered and a key range is used
- *  is used to selectively retrieve them without having to iterate the whole set and test each. We use string keys 
- *  build up with various prefixes. This polyfill makes it possible to retrieve all keys with a specified prefix 
- *  easily.
+/** 
+ * PolyFill IDBKeyRange for a key prefix search. 
+ * 
+ * In IndexedDB, Primary keys are ordered, and a key range is used is used to selectively retrieve them without having 
+ * to iterate the whole set and test each. We use string keys build up with various prefixes. This polyfill makes it 
+ * possible to retrieve all keys with a specified prefix easily.
+ * 
+ * @param prefix The string by which to filter the primary keys.
  */
 IDBKeyRange.forPrefix = (prefix) => {
-	/** Determines the string that would sort immediately after all strings with the specified prefix and hence 
-	 * can be used as the upper bound for an IDBKeyRange to retreive all keys with a specified prefix (where the 
-	 * lower bound is the prefix itself). */
+	/** 
+	 * Determines the string that would sort immediately after all strings with the specified prefix and hence can be 
+	 * used as the upper bound for an IDBKeyRange to retrieve all keys with a specified prefix (where the lower bound is 
+	 * the prefix itself). 
+	 * 
+	 * @param key 
+	 */
 	const successor = (key) => {
 		let len = key.length;
 		while (len > 0) {
