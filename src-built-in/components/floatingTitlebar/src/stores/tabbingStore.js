35b0b4ffc7a0fe3384ba80c6d086010792d52c21
Brad Carter <brad@chartiq.com>
2018-09-14 14:13:39 -0400

    Fix duplicate declaration

--- src-built-in/components/floatingTitlebar/src/stores/tabbingStore.js
+++ src-built-in/components/floatingTitlebar/src/stores/tabbingStore.js
@@ -14,7 +14,6 @@ var attachedWindow; // = finWindow;
 //theses are constants that are set inside of setupStore. so they're declared as vars and not constantsa.
 
 var windowIdentifier; // = FSBL.Clients.WindowClient.getWindowIdentifier(); <- no idea why this was here. attempts to use FSBL before it is ready.
-let constants = {};
 var Actions = {
 	initialize: function () {
 		var onParentSet = () => {
