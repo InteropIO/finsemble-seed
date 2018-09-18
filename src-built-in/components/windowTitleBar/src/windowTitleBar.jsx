38244ab9cd38b70c3054c34815cb8d6ebe56cf48
sidd <sidd@chartiq.com>
2018-09-18 10:13:32 -0400

    prevent multiple preloads

--- src-built-in/components/windowTitleBar/src/windowTitleBar.jsx
+++ src-built-in/components/windowTitleBar/src/windowTitleBar.jsx
@@ -1,12 +1,17 @@
 // React events were getting attached to shadow dom elements if react was included before hand. so for using react in preload, just require the files after we are ready.
 function init() {
+	// preventing loading multiple times because preload happens more than once??
+	if (window.headerLoaded) return;
+	window.headerLoaded = true;
     console.log("load this")
     require("./windowTitleBar2.jsx")
 }
 
-if (FSBL) {
+if (window.FSBL && FSBL.addEventListener) {
+	console.log("fsbl event")
 	FSBL.addEventListener("onReady", init)
 } else {
+	console.log("window event")
 	window.addEventListener("FSBLReady", init)
 }
 
