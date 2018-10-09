9bb2ab69330c5240bd9ed898599894887a30bacf
Mody <ms@chartiq.com>
2018-10-09 17:36:36 +0300

    Add sort by controls

--- src-built-in/components/appLauncher2/src/components/FilterSort.jsx
+++ src-built-in/components/appLauncher2/src/components/FilterSort.jsx
@@ -11,7 +11,15 @@ export default class FilterSort extends React.Component {
 		const app = this.props.app
 		return (
 			<div className="filter-sort">
-				<input type="text" placeholder="Search.." />
+				<div className="search-box"> 
+					<input type="text" placeholder="Search.." />
+				</div>
+				<div className="sort"> 
+					Sort by: 
+					<span className="first active sort-option pointer">Alphabetical</span>
+					<span className="sort-option pointer">Recent</span>
+					<span className="sort-option pointer">Favorites</span>
+				</div>			
 			</div>
 			)
 	}
