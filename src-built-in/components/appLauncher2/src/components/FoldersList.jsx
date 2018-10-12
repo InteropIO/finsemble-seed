292b525f296d5cb0becb9e1c24d1f2d4bdb5f507
Mody <ms@chartiq.com>
2018-10-13 00:44:13 +0300

    Move folder list to own component and use store

--- src-built-in/components/appLauncher2/src/components/FoldersList.jsx
+++ src-built-in/components/appLauncher2/src/components/FoldersList.jsx
@@ -1,20 +1,59 @@
 import React from  'react'
+import storeActions from '../stores/StoreActions'
+import {getStore} from '../stores/LauncherStore'
 
 export default class FoldersList extends React.Component {
 
 	constructor(props) {
 		super(props)
+		this.state = {
+			folders: storeActions.getFolders(),
+			activeFolder: 'My Apps'
+		}
+
+	}
+	
+	onAppFoldersUpdate() {
+		this.setState({
+			folders: storeActions.getFolders()
+		})
+	}
+
+	onFolderClicked(folder) {
+		getStore().setValue({
+			field: 'activeFolder', 
+			value: folder.name
+		}, () => {
+			this.setState({
+				activeFolder: folder.name
+			})
+		})
+	}
+
+	componentWillMount() {
+		getStore().addListener({field: 'appFolders'}, this.onAppFoldersUpdate)
+	}
+
+	componentWillUnmount() {
+		getStore().removeListener({field: 'appFolders'}, this.onAppFoldersUpdate)
+	}
+
+	renderFoldersList() {
+		return 	this.state.folders.map((folder, index) => {
+			let className = 'complex-menu-section-toggle'
+			if (this.state.activeFolder === folder.name) {
+				className += ' active-section-toggle'
+			}
+			return <div onClick={() => this.onFolderClicked(folder)} 
+				className={className} key={index}>{folder.name}</div>
+		})
 	}
 
 	render() {
 		return (
-			<ul>
-			{
-				this.props.list.map((item, index) => {
-					return <li onClick={() => this.props.onFolderClicked(item)} key={index}>{item.name}</li>
-				})
-			}
-			</ul>
-			)	
+			<div className="top">
+				{this.renderFoldersList()}
+			</div>
+			)
 	}
 }
--- src-built-in/components/appLauncher2/src/components/LeftNav.jsx
+++ src-built-in/components/appLauncher2/src/components/LeftNav.jsx
@@ -1,6 +1,7 @@
 import React from  'react'
 import FoldersList from  './FoldersList'
 import LeftNavBottomLinks from  './LeftNavBottomLinks'
+import {getStore} from '../stores/LauncherStore'
 
 const bottomEntries = [
 	'New folder',
@@ -13,27 +14,11 @@ export default class LeftNav extends React.Component {
 	constructor(props) {
 		super(props)
 	}
-	renderFoldersList() {
-		return (
-				<div className="top">
-				  {
-				  	this.props.folders.map((folder, index) => {
-				  		let className = 'complex-menu-section-toggle'
-				  		return (
-				  			<div onClick={() => this.props.onFolderClicked(folder)} 
-				  			className={className} key={index}>
-				  				{folder.name}
-				  			</div>
-				  			)
-				  	})
-				  }
-			  </div>
-			  )
-	}
+	
 	render() {
 		return (
 			<div className="complex-menu-left-nav">
-			  {this.renderFoldersList()}
+			  <FoldersList />
 			  <LeftNavBottomLinks />
 			 </div>
 
