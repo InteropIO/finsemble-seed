066af370a57c62fed05b959c7d811eaf261a8ff0
Mody <ms@chartiq.com>
2018-10-15 17:20:27 +0300

    Redesign store actions

--- src-built-in/components/appLauncher2/src/stores/StoreActions.js
+++ src-built-in/components/appLauncher2/src/stores/StoreActions.js
@@ -1,24 +1,55 @@
 import {getStore} from './LauncherStore'
 
-const actions = {
-	getFolders: () => {
-		return getStore().getValue({
-			field: 'appFolders'
-		}).folders
-	},
-	getActiveFolder: () => {
-		return getStore().getValue({
-			field: 'appFolders'
-		}).folders.filter((folder) => {
-			return folder.name == getStore().getValue({field: 'activeFolder'})
-		})[0]
-	},
-	getSearchText: () => {
-		return getStore().getValue({field: 'filterText'})
-	},
-	getSortBy: () => {
-		return getStore().getValue({field: 'sortBy'})
-	}
+export default {
+	getFolders,
+	getActiveFolder,
+	getSearchText,
+	getSortBy,
+	getTags,
+	deleteTag,
+	addTag
 }
 
-export default actions
+function getFolders(){
+	return getStore().getValue({
+		field: 'appFolders'
+	}).folders
+}
+
+function getActiveFolder() {
+	return getStore().getValue({
+		field: 'appFolders'
+	}).folders.filter((folder) => {
+		return folder.name == getStore().getValue({field: 'activeFolder'})
+	})[0]
+}
+
+function getSearchText() {
+	return getStore().getValue({field: 'filterText'})
+}
+
+function getSortBy() {
+	return getStore().getValue({field: 'sortBy'})
+}
+
+function getTags() {
+	return getStore().getValue({field: 'tags'}) || []
+}
+
+function addTag(tag) {
+	// Get current list of tags
+	const tags = getTags()
+	// Push new tag to list
+	tags.push(tag)
+	// Update tags in store
+	return getStore().setValue({field: 'tags', value: tags})
+}
+
+function deleteTag(tag) {
+	// Get current list of tags
+	const tags = getTags()
+	// Push new tag to list
+	tags.splice(tags.indexOf(tag), 1)
+	// Update tags in store
+	return getStore().setValue({field: 'tags', value: tags})
+}
