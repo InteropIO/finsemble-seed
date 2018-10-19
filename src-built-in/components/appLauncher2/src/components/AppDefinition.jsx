8e684b8cbdee4397e6607e6b68aa452fe75da53a
Mody <trythrow@gmail.com>
2018-10-19 15:12:25 +0300

    Spawn app on double click

--- src-built-in/components/appLauncher2/src/components/AppDefinition.jsx
+++ src-built-in/components/appLauncher2/src/components/AppDefinition.jsx
@@ -4,11 +4,19 @@ import React from 'react'
 import AppActionsMenu from './AppActionsMenu'
 import AppTagsList from './AppTagsList'
 
+/**
+ * Used to make sure that a user is not waiting for component
+ * to spawn after a double click, helps us prevent multiple
+ * spawns for the same app.
+ */
+let pendingSpawn = false
+
 export default class AppDefinition extends React.Component {
 
 	constructor(props) {
 		super(props)
 		this.onDragToFolder = this.onDragToFolder.bind(this)
+		this.onDoubleClick = this.onDoubleClick.bind(this)
 	}
 
 	/**
@@ -18,11 +26,21 @@ export default class AppDefinition extends React.Component {
 		event.dataTransfer
 			.setData('app', JSON.stringify(this.props.app))
 	}
+	/**
+	 * Spawn a component on double click
+	 */
+	onDoubleClick(app) {
+		if (pendingSpawn) return
+		pendingSpawn = true
+		FSBL.Clients.LauncherClient.spawn(app.name, {}, (error, data) => {
+			pendingSpawn = false
+		})
+	}
 
 	render() {
 		const app = this.props.app
 		return (
-			<div className="app-item" draggable="true" onDragStart={this.onDragToFolder}>
+			<div onDoubleClick={() => this.onDoubleClick(app)} className="app-item" draggable="true" onDragStart={this.onDragToFolder}>
 				<div className="app-item-title">{app.friendlyName}</div>
 				<AppTagsList tags={app.tags} />
 				<AppActionsMenu />
