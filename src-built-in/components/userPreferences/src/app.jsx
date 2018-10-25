b56b0a37fd63daa62ca67df985463ac15a74b6d0
NinjaManatee <andy@chartiq.com>
2018-10-25 11:31:00 -0400

    Moved always on top to config

--- src-built-in/components/userPreferences/src/app.jsx
+++ src-built-in/components/userPreferences/src/app.jsx
@@ -50,7 +50,6 @@ class UserPreferences extends React.Component {
 
 if (window.FSBL && FSBL.addEventListener) { FSBL.addEventListener("onReady", FSBLReady); } else { window.addEventListener("FSBLReady", FSBLReady) }
 function FSBLReady() {
-	FSBL.Clients.WindowClient.finsembleWindow.updateOptions({ alwaysOnTop: true });
 	FSBL.Clients.WindowClient.finsembleWindow.addEventListener("shown", () => {
 		finsembleWindow.bringToFront();
 		FSBL.Clients.DialogManager.showModal();
