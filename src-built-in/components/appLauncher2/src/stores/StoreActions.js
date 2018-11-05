be5d2a4e5e34a1f8ebd3ea19090012df35c1c24e
amaroks <trythrow@gmail.com>
2018-11-06 01:06:25 +0200

    Refactor storeActions, cache and get data synchronously

--- src-built-in/components/appLauncher2/src/stores/StoreActions.js
+++ src-built-in/components/appLauncher2/src/stores/StoreActions.js
@@ -1,7 +1,7 @@
 import { getStore } from './LauncherStore'
 
 export default {
-	getValue,
+	initialize,
 	addNewFolder,
 	addAppToFolder,
 	removeAppFromFolder,
@@ -10,191 +10,147 @@ export default {
 	deleteTag,
 	reorderFolders,
 	getFolders,
+	getActiveFolderName,
 	getActiveFolder,
-	getFavoriteFolder,
+	getSingleFolder,
 	getAllAppsTags,
+	getAllApps,
 	getSearchText,
 	getSortBy,
 	addTag,
 	getTags
 }
 
+const data = {}
+
+function initialize(callback) {
+	const store = getStore()
+	data.folders = store.values.appFolders.folders
+	data.apps = store.values.appDefinitions
+	data.tags = store.values.tags
+	data.activeFolder = store.values.activeFolder
+	data.filterText = store.values.filterText
+	data.sortBy = store.values.sortBy
+
+	// Add listeners to keep our copy up to date
+	store.addListener({ field: 'appFolders.folders' }, (err, dt) => data.folders = dt.value)
+	store.addListener({ field: 'appDefinitions' }, (err, dt) => data.apps = dt.value)
+	store.addListener({ field: 'activeFolder' }, (err, dt) => data.activeFolder = dt.value)
+	store.addListener({ field: 'sortBy' }, (err, dt) => data.sortBy = dt.value)
+	store.addListener({ field: 'tags' }, (err, dt) => data.tags = dt.value)
+	callback && callback()
+}
+
 function _setFolders(folders) {
 	getStore().setValue({
-		field: 'folders',
-		value: folders
+		field: 'appFolders.folders',
+		value: data.folders
 	}, (error, data) => {
 		if (error) {
 			console.log('Failed to save modified folder list.')
 		}
 	})
 }
-/**
- * A wrapper for StoreModel.getValue to return a promise
- * instead of callback to take advantage of the async/await syntax
- * @param {string} field The field we want to get its value
- */
-function getValue(field) {
-	return new Promise((resolve, reject) => {
-		getStore().getValue({field: field}, (error, data) => {
-			if(!error) {
-				resolve(data)
-			} else {
-				reject(error)
-			}
-
-		})
-	})
-}
 
 function getFolders() {
-	return getValue('folders')
+	return data.folders
 }
 
-function getFavoriteFolder() {
-	return new Promise(async (resolve, reject) => {
-		const folders = await getFolders()
-		resolve(folders.find(folder => folder.name === 'Favorites'))
-	})
+function getAllApps() {
+	return data.apps
 }
 
-async function reorderFolders(destIndex, srcIndex) {
-	const folders = await getFolders()
-	// Swap array elements
-	const temp = folders[srcIndex]
-	folders[srcIndex] = folders[destIndex]
-	folders[destIndex] = temp
-	_setFolders(folders)
+function getSingleFolder(folderName) {
+	return data.folders[folderName]
 }
 
-async function addNewFolder(name) {
+function reorderFolders(destIndex, srcIndex) {
+}
+
+function addNewFolder(name) {
 	console.log('adding a folder asyncrounously');
 	// Find folders that have a name of "New folder" or "New folder #"
-	const folders = await getFolders()
-	const newFolders = folders.filter((folder) => {
-		return folder.name.toLowerCase().indexOf('new folder') > -1
+	const newFolders = Object.keys(data.folders).filter((folder) => {
+		return folder.toLowerCase().indexOf('new folder') > -1
 	})
+	const folderName = name || `New folder ${newFolders.length + 1}`
 	const newFolder = {
-		name: name || `New folder ${newFolders.length + 1}`,
-		type: 'folder',
 		disableUserRemove: true,
 		icon: "ff-folder",
-		appDefinitions: []
+		apps: []
 	}
-	folders.push(newFolder)
-	_setFolders(folders)
-	return newFolder
+	data.folders[folderName] = newFolder
+	_setFolders()
 }
 
-async function deleteFolder(name) {
-	const folders = await getFolders();
-	const newFolders = folders.filter((folder) => {
-		if (folder.name !== name) return true;
-	});
-	_setFolders(newFolders);
+function deleteFolder(folderName) {
+	delete data.folders[folderName] && _setFolders()
 }
 
-async function renameFolder(oldName, newName) {
-	const folders = await getFolders();
-	let targetFolder;
-	const newFolders = folders.map((folder) => {
-		if (folder.name === oldName) {
-			folder.name = newName;
-			targetFolder = folder;
-		}
-		return folder;
-	});
-	_setFolders(newFolders);
-	getStore().setValue({
-		field: 'activeFolder',
-		value: newName
-	});
+function renameFolder(oldName, newName) {
+	data.folders[newName] = data.folders[oldName]
+	delete data.folders[oldName]
+	_setFolders()
 }
 
-async function addAppToFolder(folder, app) {
-	const folders = await getFolders()
-	const index = folders.findIndex((item) => {
-		return item.name === folder.name
-	})
-	// Add app to folder & make sure its added once
-	const apps = folders[index].appDefinitions
-	const foundApp = apps.find((item) => {
-		return item.name === app.name
-	})
-
-	if (!foundApp) {
-		apps.push(app)
-		_setFolders(folders)
+function addAppToFolder(folderName, app) {
+	data.folders[folderName].apps[app.appID] = {
+		name: app.name,
+		appID: app.appID
 	}
+	_setFolders()
 }
 
-async function removeAppFromFolder(folder, app) {
-	const folders = await getFolders()
-	const folderIndex = folders.findIndex((item) => {
-		return item.name === folder.name
-	})
-	const apps = folders[folderIndex].appDefinitions
-	const appIndex = apps.findIndex((item) => {
-		return item.name === app.name
-	})
-	// Remove app from apps in folder & Update folders
-	apps.splice(appIndex, 1) && _setFolders(folders)
+function removeAppFromFolder(folderName, appID) {
+	delete data.folders[folderName].apps[appID]
+	_setFolders()
 }
 
 function getActiveFolder() {
-	return new Promise(async (resolve, reject) => {
-		const folders = await getFolders()
-		const activeFolder = await getValue('activeFolder')
-		const folder = folders.find((folder) => {
-			return folder.name == activeFolder
-		})
-		resolve(folder)
+	const folder = data.folders[data.activeFolder]
+	Object.values(folder.apps).map((app) => {
+		app.tags = data.apps[app.appID].tags
 	})
+	return folder
+}
+
+function getActiveFolderName() {
+	return data.activeFolder
 }
 
 function getSearchText() {
-	return getValue('filterText')
+	return data.filterText
 }
 
 function getSortBy() {
-	return getValue('sortBy')
+	return data.sortBy
 }
 
 function getTags() {
-	return getValue('tags')
+	return data.tags
 }
 
 function getAllAppsTags() {
-	return new Promise(async (resolve, reject) => {
-		let tags = []
-		const folders = await getFolders()
-		folders.forEach((folder) => {
-			folder.appDefinitions.forEach((app) => {
-				tags = tags.concat(app.tags)
-			})
-		})
-		// return unique ones only
-		tags = tags.filter((tag, index) => {
-			return tags.indexOf(tag) === index
-		})
-		resolve(tags)
+	let tags = []
+	Object.values(data.apps).forEach((app) => {
+		tags = tags.concat(app.tags)
+	})
+	// return unique ones only
+	return tags.filter((tag, index) => {
+		return tags.indexOf(tag) === index
 	})
 }
 
-async function addTag(tag) {
-	// Get current list of tags
-	const tags = await getTags()
+function addTag(tag) {
 	// Push new tag to list
-	tags.indexOf(tag) < 0 && tags.push(tag)
+	data.tags.indexOf(tag) < 0 && data.tags.push(tag)
 	// Update tags in store
-	getStore().setValue({ field: 'tags', value: tags })
+	getStore().setValue({ field: 'tags', value: data.tags })
 }
 
-async function deleteTag(tag) {
-	// Get current list of tags
-	const tags = await getTags()
+function deleteTag(tag) {
 	// Push new tag to list
-	tags.splice(tags.indexOf(tag), 1)
+	data.tags.splice(data.tags.indexOf(tag), 1)
 	// Update tags in store
-	getStore().setValue({ field: 'tags', value: tags })
+	getStore().setValue({ field: 'tags', value: data.tags })
 }
