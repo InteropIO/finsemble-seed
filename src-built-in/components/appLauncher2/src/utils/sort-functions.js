43c86cae3ffac87944fde40a57cdcc36cef0c277
amaroks <trythrow@gmail.com>
2018-11-07 19:27:25 +0200

    Add comments

--- src-built-in/components/appLauncher2/src/utils/sort-functions.js
+++ src-built-in/components/appLauncher2/src/utils/sort-functions.js
@@ -1,11 +1,18 @@
 import storeActions from '../stores/StoreActions'
 
 export default {
+	/**
+	 * Sorts list alphabetically
+	 */
 	Alphabetical: (list) => {
 		return list.sort((a, b) => {
 			return a.name > b.name
 		})
 	},
+	/**
+	 * @todo We will implement this later once we have a 
+	 * way to detect when was the last time an app was launched
+	 */
 	Recent: (list) => {
 		// dummy sorting
 		// reverse the above
@@ -13,6 +20,13 @@ export default {
 			return a.name < b.name
 		})
 	},
+	/**
+	 * Sorts a list of application by Favorites.
+	 * It gets a list of apps from Favorites folder
+	 * then in the sort function, it checks where a, b apps
+	 * are in favorite folder or not, giving a lower index
+	 * for apps that are in Favorite folder
+	 */
 	Favorites: (list) => {
 		const favorites = storeActions.getSingleFolder('Favorites').apps
 		return list.sort((a, b) => {
