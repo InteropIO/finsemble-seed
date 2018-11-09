cf23d3c2e8a1d7bb78c0d406e6a940b1db27f037
Ryan Sharp <rsharp@chartiq.com>
2018-11-08 23:23:46 -0500

    fix for drag area on the titlebar

--- src-built-in/components/windowTitleBar/src/windowTitleBar.jsx
+++ src-built-in/components/windowTitleBar/src/windowTitleBar.jsx
@@ -23,7 +23,7 @@ import Maximize from "./components/right/MaximizeButton.jsx";
 import Close from "./components/right/CloseButton.jsx";
 import BringSuiteToFront from "./components/right/BringSuiteToFront.jsx";
 import AlwaysOnTop from "./components/right/AlwaysOnTop.jsx";
-import TabRegion from './components/center/TabRegion'
+import TabRegion from "./components/center/TabRegion";
 import "../../../../assets/css/finsemble.css";
 
 /**
@@ -38,11 +38,11 @@ class WindowTitleBar extends React.Component {
 
 		this.setTabBarRef = element => {
 			this.tabBar = element;
-		}
+		};
 
 		this.setToolbarRight = element => {
 			this.toolbarRight = element;
-		}
+		};
 
 		this.bindCorrectContext();
 		windowTitleBarStore.getValue({ field: "Maximize.hide" });
@@ -167,9 +167,9 @@ class WindowTitleBar extends React.Component {
 	 * When tiling start, we want to find the dragHandler and hide it
 	 */
 	onTilingStart() {
-		let dragHandle = document.querySelector('.fsbl-drag-handle');
+		let dragHandle = document.querySelector(".fsbl-drag-handle");
 		if (dragHandle) {
-			dragHandle.classList.add('hidden');
+			dragHandle.classList.add("hidden");
 		}
 	}
 
@@ -177,9 +177,9 @@ class WindowTitleBar extends React.Component {
 	 * When tiling stops, we want to find the dragHandler and reshow it
 	 */
 	onTilingStop() {
-		let dragHandle = document.querySelector('.fsbl-drag-handle.hidden');
+		let dragHandle = document.querySelector(".fsbl-drag-handle.hidden");
 		if (dragHandle) {
-			dragHandle.classList.remove('hidden');
+			dragHandle.classList.remove("hidden");
 		}
 	}
 
@@ -195,63 +195,6 @@ class WindowTitleBar extends React.Component {
 	 * Hide the dragHandle during drop operations, so that it doesn't interfere
 	 */
 	resizeDragHandle() {
-		const fsblHeader = document.querySelector(".fsbl-header");
-		if (!fsblHeader) {
-			// If there isn't an FSBLHeader then there doesn't need to be a drag handle.
-			return;
-		}
-
-		// Create the dragger if it doesn't already exist
-		let dragHandle = document.querySelector(".fsbl-drag-handle");
-		if (!dragHandle) {
-			dragHandle = document.createElement("div");
-			dragHandle.className = "fsbl-drag-handle";
-
-			fsblHeader.insertBefore(dragHandle, fsblHeader.firstChild);
-			var self = this;
-			window.addEventListener("resize", function () {
-				self.resizeDragHandle();
-			});
-		}
-
-		// Set the height of the dragHandle to match the height of the window title bar
-		// Do this every time through the render loop just in case a customer builds a
-		// header bar with dynamic height!
-		let bounds = fsblHeader.getBoundingClientRect();
-		dragHandle.style.height = (bounds.height - 5) + "px"; // Subtract 5 pixels from height in order to make room for resize window cursor at top edge of window
-		dragHandle.style.marginTop = (-bounds.height + 5) + "px"; // Negative margin pulls the drag handle up over the fixed header
-
-		// Start logic for determining where to place our dragHandle
-		if (this.state.showTabs) {
-			// If there is more than one tab, then the drag area is the padding-left of fsbl-header-right
-			// See .fsbl-tabs-multiple in the css
-			if (this.state.tabs.length > 1) {
-				let headerRight = document.querySelector(".fsbl-header-right");
-				let computedStyle = getComputedStyle(headerRight);
-				bounds = headerRight.getBoundingClientRect();
-				// override the bounds.width with the paddingLeft amount
-				bounds = {
-					left: bounds.left,
-					width: parseInt(computedStyle.paddingLeft, 10)
-				};
-			} else {
-				// If tabs are not enabled, then the remained of fsbl-header-center beyond the tabs
-				// is the draggable area. This assumes left aligned tabs.
-				let fsblHeaderCenter = document.querySelector(".fsbl-header-center");
-				bounds = fsblHeaderCenter.getBoundingClientRect();
-				let theTabBounds = fsblHeaderCenter.querySelector(".tab-region-wrapper div").getBoundingClientRect();
-				// Calculate the right portion
-				bounds = {
-					left: bounds.left + theTabBounds.width,
-					width: bounds.width - theTabBounds.width
-				};
-			}
-		} else {
-			// If tabs are not enabled, then the entire center is the drag area
-			bounds = document.querySelector(".fsbl-header-center").getBoundingClientRect();
-		}
-		dragHandle.style.left = bounds.left + "px";
-		dragHandle.style.width = bounds.width + "px";
 
 		//Add an event listener to hide the drag-handler when tiling is started
 		FSBL.Clients.RouterClient.addListener("DockingService.startTilingOrTabbing", this.onTilingStart);
@@ -333,13 +276,13 @@ class WindowTitleBar extends React.Component {
 	onTabsChanged(err, response) {
 		this.setState({
 			tabs: response.value
-		})
+		});
 	}
 
 	onShowTabsChanged(err, response) {
 		this.setState({
 			showTabs: response.value
-		})
+		});
 	}
 
 	onHackScrollbarChanged(err, response) {
@@ -403,8 +346,10 @@ class WindowTitleBar extends React.Component {
 							onTitleUpdated={this.resizeDragHandle}
 						/>}
 
+
 				</div>
 				<div className={rightWrapperClasses} ref={this.setToolbarRight}>
+					<div className="fsbl-drag-handle"></div>
 					{this.state.alwaysOnTopButton && showMinimizeIcon ? <AlwaysOnTop /> : null}
 					<BringSuiteToFront />
 					{this.state.minButton && showMinimizeIcon ? <Minimize /> : null}
@@ -422,7 +367,7 @@ class WindowTitleBar extends React.Component {
 // it is pub/sub, if the event had fired in the past then it will still be fired.
 // window.addEventListener("FSBLReady", function () {
 
-if (window.FSBL && FSBL.addEventListener) { FSBL.addEventListener("onReady", FSBLReady); } else { window.addEventListener("FSBLReady", FSBLReady) }
+if (window.FSBL && FSBL.addEventListener) { FSBL.addEventListener("onReady", FSBLReady); } else { window.addEventListener("FSBLReady", FSBLReady); }
 function FSBLReady() {
 	if (FSBL.titleBarInserted) return;
 	FSBL.titleBarInserted = true;
