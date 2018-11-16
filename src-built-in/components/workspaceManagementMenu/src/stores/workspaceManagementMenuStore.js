eda47aad649ddc03190a02ef70f5f8b7daf0b80f
thorsent <terry@chartiq.com>
2018-11-16 11:46:36 -0500

    updated config bad component 10637 Pressing cancel locks the user out from switching workspaces. (#244)
    * 10637 Pressing cancel locks the user out from switching workspaces.
    
    Now we set the `switching` boolean to false inside of `onAsyncComplete`,
    which happens after the workspace is switched _or_ the action is cancelled.
    
    * No need to check for errors. We never want to lock the user out.
    
    * Fix missing callback

--- src-built-in/components/workspaceManagementMenu/src/stores/workspaceManagementMenuStore.js
+++ src-built-in/components/workspaceManagementMenu/src/stores/workspaceManagementMenuStore.js
@@ -338,12 +338,10 @@ Actions = {
 		 * Actually perform the switch. Happens after we ask the user what they want.
 		 *
 		 */
-		function switchIt() {
+		function switchIt(callback) {
 			FSBL.Clients.WorkspaceClient.switchTo({
 				name: name
-			}, (err) => {
-				switching = false;
-			});
+			}, callback);
 		}
 		/**
 		 * Make sure the user wants to do what they say that they want to do.
@@ -384,7 +382,9 @@ Actions = {
 
 			async.waterfall(tasks, Actions.onAsyncComplete);
 		} else {
-			switchIt();
+			switchIt(() => {
+				switching = false;
+			});
 		}
 	},
 
@@ -406,6 +406,9 @@ Actions = {
 			//handle error.
 			Logger.system.error(err);
 		}
+
+		//Unlock the UI.
+		switching = false;
 	},
 	/**
 	 * NOTE: Leaving this function here until we figure out notifications.
