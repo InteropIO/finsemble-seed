44eaea2dde785fe277da5239f6767cc94581c6ca
Joe Claus <joseph@chartiq.com>
2018-11-20 15:47:39 -0500

    10326 Fix drag handler when window scrolls before titlebar load

--- src-built-in/components/windowTitleBar/src/windowTitleBar.jsx
+++ src-built-in/components/windowTitleBar/src/windowTitleBar.jsx
@@ -219,7 +219,7 @@ class WindowTitleBar extends React.Component {
 		// header bar with dynamic height!
 		let bounds = fsblHeader.getBoundingClientRect();
 		dragHandle.style.height = (bounds.height - 5) + "px"; // Subtract 5 pixels from height in order to make room for resize window cursor at top edge of window
-		dragHandle.style.marginTop = (-bounds.height + 5) + "px"; // Negative margin pulls the drag handle up over the fixed header
+		dragHandle.style.setProperty("margin-top", (-bounds.height) + "px", "important"); // Negative margin pulls the drag handle up over the fixed header
 
 		// Start logic for determining where to place our dragHandle
 		if (this.state.showTabs) {
