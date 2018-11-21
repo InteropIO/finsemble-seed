4662c08258013e626b512e0db62ffd8a0762cf23
Brad Carter <brad@chartiq.com>
2018-11-21 09:44:47 -0500

    10351 Add isClosable:false to prevent default row from closing.
    Without this prop the default row closes and prevents the user from creating anything once all elements in the row are closed.

--- src/components/dragDrop/Drag.jsx
+++ src/components/dragDrop/Drag.jsx
@@ -3,68 +3,66 @@
 ***************************/
 
 import React, { Component } from 'react'
+const DEFAULT_CONFIG = {
+    content: [
+        {
+            type: 'row',
+            //isClosable prevents the default row from closing, which allows us to add components when the layout has nothing in it.
+            isClosable: false,
+            content: [
+            ]
+        }
+    ]
+};
 
 export default class Drag extends Component {
     constructor(props) {
         super(props)
         this.state = {
-            config: {
-                content: [{
-                    type: 'row',
-                    content: [
-                    ]
-                }]
-            },
+            config: DEFAULT_CONFIG,
             myLayout: null,
             contentItems: null
-        }
+        };
+        this.onDrop = this.onDrop.bind(this);
     }
+
     componentDidMount = () => {
-        var config = {
-            content: [{
-                type: 'row',
-                content: []
-            }]
-        }
         try {
             var myLayout = new GoldenLayout(this.state.config)
             myLayout.init();
         } catch (e) {
             console.log('error right here:', e)
         }
-        // myLayout.root.childElementContainer[0].onDrop = this.onDrop
-        // myLayout.root.childeElementContainer[0].onDrop = this.onDrop
-        // myLayout.childeNodes = this.onDrop
+
         this.setState({ myLayout })
     }
+
     onDrop = (ev) => {
         console.log('preAdd:', this.state.myLayout.root)
-        let id = ev.dataTransfer.getData('text')
         var RandomNumber = Math.random() * Math.floor(10000)
-        let contentItems = this.state.myLayout.root.contentItems[0]
-        let rerun = true
+        //@todo make sure there's some text here.
+        const componentType = ev.dataTransfer.getData('text');
+        const id = componentType + RandomNumber;
+
         var newItemConfig = {
             type: 'component',
-            componentName: id + RandomNumber + "",
-            componentState: { text: id }
+            componentName: id,
+            componentState: { text: componentType }
         };
-        // try {
 
-        this.state.myLayout.registerComponent(id + RandomNumber + "", function (container, state) {
+        this.state.myLayout.registerComponent(id, function (container, state) {
             container.getElement().html('<h2>' + state.text + '</h2>');
         })
-        console.log(this.state.myLayout.root)
         this.state.myLayout.root.contentItems[0].addChild(newItemConfig)
         console.log('poastAdd:', this.state.myLayout.root.contentItems[0])
-        console.log('\n\n\n\n\n')
+
     }
 
     render() {
         return (
-            <div onDrop={e => this.onDrop(e)}>
+            <div onDrop={this.onDrop}>
                 hello
-
-            </div>
+            </div >
         )
     }
 }
