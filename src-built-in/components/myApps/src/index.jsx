de4061956d8929ce3aaa9f1edefbcca990a5f7a6
Joseph Claus <joseph@chartiq.com>
2018-11-26 15:50:05 -0500

    [App Launcher] [App Catalog] Resolves naming issues (#251)
    * [App Launcher] [App Catalog] Resolves some naming issues
    
    - AppCatalog2 -> App Catalog (original app catalog removed).
    - AppLauncher2 -> MyApps
    - Removes old references to "AppMarket".
    
    * Changes to address naming changes in App Launcher catalog launching

--- src-built-in/components/myApps/src/index.jsx
+++ src-built-in/components/myApps/src/index.jsx
@@ -30,7 +30,7 @@ class AppLauncher extends React.Component {
 	openAppMarket() {
 		FSBL.Clients.LauncherClient.showWindow(
 			{
-				componentType: "App Catalog 2"
+				componentType: "App Catalog"
 			},
 			{
 				monitor: "mine",
