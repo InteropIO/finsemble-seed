96082b9ea329a81194625721714a7f66cb046cdd
Joseph Claus <joseph@chartiq.com>
2018-11-26 15:58:56 -0500

    [AppLauncher] 10754 ability to pin adhoc components (#255)
    * Changes to upgrade assimilation
    
    **Couldn't start project without these changes**
    
    * Adds adhoc components to the FSBL component list and, fixing pinning for adhoc components
    
    * Revert "Changes to upgrade assimilation"
    
    This reverts commit 02b11ad2f40cacb6f7c5053f26db236c1b193789.
    
    * Remove pins associated with app being deleted

--- src-built-in/components/appLauncher2/src/stores/StoreActions.js
+++ src-built-in/components/appLauncher2/src/stores/StoreActions.js
@@ -141,6 +141,7 @@ function addPin(pin) {
 			ToolbarStore.setValue({ field: 'pins.' + pin.name.replace(/[.]/g, "^DOT^"), value: thePin });
 		}
 	});
+
 }
 
 function removePin(pin) {
@@ -191,26 +192,40 @@ function addApp(app = {}, cb) {
 	}
 	data.folders[MY_APPS].apps[appID] = data.apps[appID]
 	data.folders[folder].apps[appID] = data.apps[appID]
-	// Save appDefinitions and then folders
-	_setValue('appDefinitions', data.apps, () => {
-		_setFolders()
-		cb && cb()
-	})
+	FSBL.Clients.LauncherClient.addUserDefinedComponent(data.apps[appID], (compAddErr) => {
+		if (compAddErr) {
+			//TODO: We need to handle the error here. If the component failed to add, we should probably fall back and not add to launcher
+			console.warn("Failed to add new app");
+			return;
+		}
+		// Save appDefinitions and then folders
+		_setValue('appDefinitions', data.apps, () => {
+			_setFolders()
+			cb && cb()
+		})
+	});
 }
 
 function deleteApp(appID) {
-	// Delete app from any folder that has it
-	for(const key in data.folders) {
-		if(data.folders[key].apps[appID]) {
-			delete data.folders[key].apps[appID]
+	ToolbarStore.removeValue({ field: 'pins.' + data.apps[appID].name.replace(/[.]/g, "^DOT^") }, (err, res) => {
+		if (err) {
+			//TODO: Need to gracefully handle this error. If the pin can't be removed, the app shouldn't either
+			console.warn("Error removing pin for deleted app");
+			return
 		}
-	}
-	// Delete app from the apps list
-	delete data.apps[appID]
-	// Save appDefinitions and then folders
-	_setValue('appDefinitions', data.apps, () => {
-		_setFolders()
-	})
+		// Delete app from any folder that has it
+		for(const key in data.folders) {
+			if(data.folders[key].apps[appID]) {
+				delete data.folders[key].apps[appID]
+			}
+		}
+		// Delete app from the apps list
+		delete data.apps[appID]
+		// Save appDefinitions and then folders
+		_setValue('appDefinitions', data.apps, () => {
+			_setFolders()
+		})
+	});
 }
 
 function addNewFolder(name) {
