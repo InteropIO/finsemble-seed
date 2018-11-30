05738e7260983c501fcd2106d972c56f39fa2732
chalangiq <chalangiq@chartiq.com>
2018-11-29 20:34:33 -0500

    10351, have been looking at the multiple dropzone issue all day, issue with the layouts disappearing once you make it into an array. will ask for brads help tomorrow.

--- src/components/dashboardCanvas/Canvas.jsx
+++ src/components/dashboardCanvas/Canvas.jsx
@@ -20,28 +20,59 @@ export default class Canvas extends Component {
             myLayout: null,
             GLState: null,
             name: null,
-            storageClient: null
+            storageClient: null,
+            divide: false,
+            testing: true,
+            layouts: [],
+            divs: []
         };
         this.onDrop = this.onDrop.bind(this);
     }
-    componentDidMount = () => {
-        var myLayout = new GoldenLayout(this.state.config, document.getElementById('layoutContainer'))
-        this.setState({ myLayout })
-        myLayout.init();
-        myLayout.on('stateChanged', () => {
-            let GLState = myLayout.toConfig();
-            this.setState({ GLState });
-        });
-
-        this.setState({ myLayout })
-        FSBL.Clients.RouterClient.addListener("Save", function (error, response) {
-            if (error) {
-                console.log("Save Error: " + (error));
-            } else {
-                FSBL.Clients.WindowClient.setWindowTitle(`${response.data['DName']}`);
+    setLayouts = () => {
+        let layouts = [];
+        if (this.state.divide) {
+            let numLayouts = this.state.divide ? 4 : 1
+            for (var ii = 0; ii < this.state.divs.length; ii++) {
+                console.log('divs:', this.state.divs[ii])
+                let myLayout = new GoldenLayout(this.state.config, document.getElementById(ii))
+                myLayout.init();
+                myLayout.on('stateChanged', () => {
+                    let GLState = myLayout.toConfig();
+                    this.setState({ GLState });
+                });
+                FSBL.Clients.RouterClient.addListener("Save", function (error, response) {
+                    if (error) {
+                        console.log("Save Error: " + (error));
+                    } else {
+                        FSBL.Clients.WindowClient.setWindowTitle(`${response.data['DName']}`);
+                    }
+                });
+                layouts.push(myLayout)
             }
-        });
+            this.setState({ layouts })
+        }
+        if (!this.state.divide) {
+            var myLayout = new GoldenLayout(this.state.config, document.getElementById('layoutContainer'))
+            myLayout.init();
+            myLayout.on('stateChanged', () => {
+                let GLState = myLayout.toConfig();
+                this.setState({ GLState });
+            });
 
+            this.setState({ myLayout })
+            FSBL.Clients.RouterClient.addListener("Save", function (error, response) {
+                if (error) {
+                    console.log("Save Error: " + (error));
+                } else {
+                    FSBL.Clients.WindowClient.setWindowTitle(`${response.data['DName']}`);
+                }
+            });
+        }
+        return layouts
+    }
+    componentDidMount = () => {
+
+        this.setState({ divs: this.calculateDivs() }, () => this.setState({ layouts: this.setLayouts() }))
     }
     getJSON = () => {
         let name = this.state.name
@@ -51,27 +82,57 @@ export default class Canvas extends Component {
         }
     }
     onDrop = (ev) => {
+        console.log(ev.target.id)
+        console.log(this.state.layouts)
         //need this because you have to have unique names for registered components
         var RandomNumber = Math.random() * Math.floor(10000)
         //@todo make sure there's some text here.
         const componentType = ev.dataTransfer.getData('text');
-        const id = componentType + RandomNumber;
+        const id = `${componentType} ${RandomNumber}`;
 
         var newItemConfig = {
             type: 'component',
             componentName: id,
             componentState: { text: componentType }
         };
-
-        this.state.myLayout.registerComponent(id, function (container, state) {
-            container.getElement().html('<h2>' + state.text + '</h2>');
-        })
-        this.state.myLayout.root.contentItems[0].addChild(newItemConfig)
+        if (this.state.divide) {
+            this.state.layouts[ev.target.id].registerComponent(id, function (container, state) {
+                container.getElement().html('<h2>' + state.text + '</h2>');
+            })
+            this.state.layouts[ev.target.id].root.contentItems[0].addChild(newItemConfig)
+        }
+        if (!this.state.divide) {
+            this.state.myLayout.registerComponent(id, function (container, state) {
+                container.getElement().html('<h2>' + state.text + '</h2>');
+            })
+            this.state.myLayout.root.contentItems[0].addChild(newItemConfig)
+        }
+    }
+    handleClick = (ev) => {
+        if (ev.target.id = "divide") {
+            this.setState({ divide: !this.state.divide }, () => this.setState({ divs: this.calculateDivs() }))
+        }
+    }
+    calculateDivs = () => {
+        var divs = [];
+        if (this.state.divide) {
+            for (var ii = 0; this.state.divide ? ii < 4 : ii < 1; ii++) {
+                divs.push(<div onDrop={this.onDrop} key={ii} id={ii} className="dropzones">                </div>)
+            }
+        }
+        if (!this.state.divide) {
+            for (var ii = 0; this.state.divide ? ii < 4 : ii < 1; ii++) {
+                divs.push(<div onDrop={this.onDrop} key={ii} id="layoutContainer"></div>)
+            }
+        }
+        return divs
     }
     render() {
         return (
-            <div onDrop={this.onDrop} id="layoutContainer" >
-            </div >
+            <div>
+                <button className="divide" onClick={this.handleClick} id="divide">{this.state.divide ? 'Join' : 'Divide'}</button>
+                {this.state.divs}
+            </div>
         )
     }
 }
