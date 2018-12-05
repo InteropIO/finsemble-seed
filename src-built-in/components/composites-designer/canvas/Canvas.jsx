4a2921de9700e6fd7f69dd19029c911a1d9cc194
amaroks <trythrow@gmail.com>
2018-12-05 03:29:21 +0200

    Implement goldenlayout

--- src-built-in/components/composites-designer/canvas/Canvas.jsx
+++ src-built-in/components/composites-designer/canvas/Canvas.jsx
@@ -1,5 +1,10 @@
 import React from 'react'
+import GoldenLayout from 'golden-layout'
+import 'golden-layout/src/css/goldenlayout-base.css'
+import 'golden-layout/src/css/goldenlayout-dark-theme.css'
 
+// Golden layout instance
+let layout
 /**
  * The canvas components, its where you drop apps in
  */
@@ -8,11 +13,17 @@ export default class Canvas extends React.Component {
     constructor(props) {
         super(props)
         this.state = {
+            // The name to be displayed in window title
             compositeName: '',
-            components: [],
+            // Tells us whether we should display the instructions or not
+            items: 0,
         }
         // Bind correct context
+        this.onWindowResize = this.onWindowResize.bind(this)
+        this.layoutStateChanged = this.layoutStateChanged.bind(this)
         this.closeWindow = this.closeWindow.bind(this)
+        this.onDrop = this.onDrop.bind(this)
+        this.onDrop = this.onDrop.bind(this)
     }
     componentWillMount() {
         // Subscribe to composites name input
@@ -21,6 +32,32 @@ export default class Canvas extends React.Component {
     componentWillUnmount() {
         // Unsubscribe to composites name input
         FSBL.Clients.RouterClient.removeListener("composites:name", this.setTitle)
+        // Remove resize listener
+        window.removeEventListener("resize", this.onWindowResize)
+    }
+    componentDidMount() {
+        const tilesElement = document.getElementById("tiles")
+        layout = new GoldenLayout({
+            dimensions: {
+                // The gap between tiles
+                borderWidth: 2
+            },
+            content: [{
+                type: 'row',
+                // content contains the grid tiles
+                content: []
+            }]
+        }, tilesElement)
+        // Keep updated about the layout state
+        layout.on("stateChanged", this.layoutStateChanged)
+        // Register our dummy component
+        layout.registerComponent('tile', function () { })
+        // Initialize layout, for some reason a delay is needed
+        setTimeout(() => {
+            layout.init()
+        }, 100)
+        // Resize layout when user resizes window
+        window.addEventListener("resize", this.onWindowResize)
     }
     /**
      * Sets this window's title
@@ -29,7 +66,7 @@ export default class Canvas extends React.Component {
         const data = message.data
         // Update the window titler whenever the name input is updated
         // in the composites designer component
-        FSBL.Clients.WindowClient.setWindowTitle(!data.name ? 
+        FSBL.Clients.WindowClient.setWindowTitle(!data.name ?
             this.state.compositeName : data.name)
     }
     /**
@@ -38,10 +75,46 @@ export default class Canvas extends React.Component {
     closeWindow() {
         FSBL.Clients.WindowClient.close()
     }
-
+    /**
+     * Triggered when app is dropped into canvas
+     * @param {object} event The drop event object
+     */
+    onDrop(event) {
+        event.preventDefault()
+        // Add component to layout grid
+        layout.root.contentItems[0].addChild({
+            title: event.dataTransfer.getData("component"),
+            type: 'component',
+            componentName: 'tile',
+        })
+        // Update layout size
+        this.onWindowResize()
+    }
+    /**
+     * Shows a copy cursor when mouse is over the drop area
+     * @param {object} event dragOver event
+     */
+    onDragOver(event) {
+        event.dataTransfer.dropEffect = "copy"
+    }
+    /**
+     * Called when canvas is resized
+     */
+    onWindowResize() {
+        layout.updateSize()
+    }
+    /**
+     * Called when a change was made to layout
+     */
+    layoutStateChanged() {
+        this.setState({
+            items: layout.root.contentItems.length
+        })
+    }
     render() {
-        return <div id="canvas">
-           {!this.state.components.length && <h1>Drag apps from the left into this area</h1>}
+        return <div id="canvas" onDrop={this.onDrop} onDragOver={this.onDragOver}>
+            <div id="tiles"></div>
+            {!this.state.items && <h1>Drag apps from the left into this area</h1>}
         </div>
     }
 }
