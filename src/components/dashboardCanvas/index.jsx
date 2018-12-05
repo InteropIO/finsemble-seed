3e141c0d7d07269d8403c05c987f0919c0a0bf4e
amaroks <trythrow@gmail.com>
2018-12-05 03:56:11 +0200

    Delete old components

--- src/components/dashboardCanvas/index.jsx
+++
@@ -1,7 +0,0 @@
-import React from 'react'
-import ReactDOM from 'react-dom'
-import Canvas from './Canvas'
-
-FSBL.addEventListener("onReady", function () {
-	ReactDOM.render(<Canvas />, document.getElementById('root'))
-})
