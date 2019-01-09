e976ee6e497cc0cd1141e9f1db2b32cc38fe03b1
Brad Carter <brad@chartiq.com>
2019-01-09 13:03:02 -0500

    fix reordering

--- src-built-in/components/myApps/src/components/LeftNav.jsx
+++ src-built-in/components/myApps/src/components/LeftNav.jsx
@@ -1,7 +1,7 @@
-import React from  "react";
-import FoldersList from  "./FoldersList";
-import LeftNavBottomLinks from  "./LeftNavBottomLinks";
-import {getStore} from "../stores/LauncherStore";
+import React from "react";
+import FoldersList from "./FoldersList";
+import LeftNavBottomLinks from "./LeftNavBottomLinks";
+import { getStore } from "../stores/LauncherStore";
 import storeActions from "../stores/StoreActions";
 
 import {
@@ -23,18 +23,9 @@ export default class LeftNav extends React.Component {
 	}
 
 	onDragEnd(event = {}) {
-		console.log("on drag end");
-		// storeActions.reorderFolders(
-		// 	event.destination.index,
-		// 	event.source.index)
-
-		// Don't allow the destination to be my apps or favorites
-		// This is a quick and easy and clean way, avoiding big change
-		// for now
-		let newIndex = event.destination.index;
-		if(event.destination && newIndex <= 1) {
-			newIndex = 2;
-		}
+		if (!event.destination) return;
+		//There are two items above the 0th item in the list. They aren't reorderable. We add 2 to the index so that it matches with reality. The source comes in properly but the destination needs to be offset.
+		let newIndex = event.destination.index + 2;
 
 		storeActions.reorderFolders(event.source.index, newIndex);
 
@@ -45,8 +36,8 @@ export default class LeftNav extends React.Component {
 			<div className="complex-menu-left-nav">
 				<FinsembleDnDContext onDragStart={this.onDragStart} onDragEnd={this.onDragEnd}>
 					<FinsembleDroppable direction="vertical" droppableId="folderList">
-				  		<FoldersList />
-				  	</FinsembleDroppable>
+						<FoldersList />
+					</FinsembleDroppable>
 				</FinsembleDnDContext>
 				<LeftNavBottomLinks {...this.props} />
 			</div>
