b3b53eeb72cf52d3e2bf29e1ee7745093a21dcf0
meredith-ciq <meredith@chartiq.com>
2019-02-01 17:47:54 -0500

    11837 current progress on this bug on branch 11837_toolbar_preferences
    -Setting fixed or floating for toolbar state now works
    -"All" for only fixed is there, but it only changes when an item is selected - needs to be improved
    -Monitor code is broken, it's setting state to a single digit number, but I don't think that's connected to anything related to our current monitors
    -various html improvements needed.

--- src-built-in/components/userPreferences/src/components/general/Toolbar.jsx
+++ src-built-in/components/userPreferences/src/components/general/Toolbar.jsx
@@ -7,20 +7,52 @@ export default class Toolbar extends React.Component {
 			monitor: "all"
 		};
 		this.setMonitor = this.setMonitor.bind(this);
+		this.setToolbarState = this.setToolbarState.bind(this);
 		this.restartApplication = this.restartApplication.bind(this);
 	}
 
 	/**
 	 * Sets the monitor by calling the setPreferences API. This will override the config for the component.
+	 * Using all works, but nothing else works from existing code. Likely a mismatch between values used for monitor. Need to pull current monitors in.
 	 * @param {event} e
 	 */
 	setMonitor(e) {
 		let monitor = e.target.value;
+		console.log("MONITOR: ", e.target.value);
 		this.setState({
 			monitor: monitor
 		});
 		FSBL.Clients.ConfigClient.setPreference({ field: "finsemble.components.Toolbar.window.monitor", value: monitor });
 		// There's no need to override spawnOnAllMonitors. Setting monitor to anything other than undefined will automatically override that value. Setting monitor="all" is equivalent.
+	}
+
+	/**
+	 * Sets the toolbar as fixed or floating by calling the setPreferences API. This will override the config for the component.
+	 * @param {event} e
+	 */
+	setToolbarState(e) {
+		let toolbarType = e.target.value;
+		this.setState({
+			toolbarType: toolbarType
+		});
+		if (toolbarType === "Fixed") {
+			FSBL.Clients.ConfigClient.setPreference({ field: "finsemble.components.Toolbar.window.dockable", value: null });
+		} else if (toolbarType === "Floating") {
+			FSBL.Clients.ConfigClient.setPreference({ field: "finsemble.components.Toolbar.window.dockable", value: ["top", "bottom"] });
+		}
+	}
+
+	//Create a list of monitors available to move toolbar to.
+	//Not including all monitors because we have no current way to tell the users which monitor is which
+	createSelectItems() {
+		let items = [];
+		if (this.state.toolbarType === "Fixed") {
+			items.push(<option value={"all"}>All</option>);
+		}
+		items.push(<option value={"0"}>Primary</option>);
+		items.push(<option value={this.state.monitor}>Current Location</option>);
+		console.log(items);
+		return items;
 	}
 
 	restartApplication() {
@@ -38,6 +70,19 @@ export default class Toolbar extends React.Component {
 				monitor: value
 			});
 		});
+		//Unless we want to add another config variable tracking dockable state we need to determine state
+		//using the set values.
+		FSBL.Clients.ConfigClient.getValue("finsemble.components.Toolbar.window.dockable", (err, value) => {
+			console.log("GETVALUE INIT: " , JSON.stringify(value));
+			if (!value) { 
+				value = "Fixed";
+			} else if (JSON.stringify(value) === JSON.stringify(["top", "bottom"])) {
+				value = "Floating";
+			}
+			this.setState({
+				toolbarType: value
+			});
+		});
 	}
 
 	componentWillUnmount() {
@@ -45,16 +90,13 @@ export default class Toolbar extends React.Component {
 
 	render() {
 		return <div className="complex-menu-content-row">
-			<div>
-				<span>Display toolbar on monitor</span>
+			<div>			    
+				<span>Toolbar Type</span>
+				<p>Floating: <input type="radio" name="toolbarState" value="Floating" onChange={this.setToolbarState} checked={this.state.toolbarType === "Floating"}/></p>
+				<p>Fixed: <input type="radio" name="toolbarState" value="Fixed" onChange={this.setToolbarState} checked={this.state.toolbarType === "Fixed"} /></p>
+				<span>Display toolbar on monitor</span>				
 				<select style={{ margin: "0px 10px" }} onChange={this.setMonitor} value={this.state.monitor}>
-					<option value={"all"}>All</option>
-					<option value={"0"}>Primary</option>
-					<option value={"1"}>2</option>
-					<option value={"2"}>3</option>
-					<option value={"3"}>4</option>
-					<option value={"4"}>5</option>
-					<option value={"5"}>6</option>
+					{this.createSelectItems()}					
 				</select>
 				<span> </span>
 				<button onClick={this.restartApplication}>Restart Application</button>
