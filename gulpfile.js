29699d12e6aa245e98827b30dd3a9e76672bb7f7
NinjaManatee <andy@chartiq.com>
2019-02-08 16:06:32 -0500

    Changes to improve readability based on review

--- gulpfile.js
+++ gulpfile.js
@@ -268,12 +268,16 @@
 			const CLI_VERSION = require(path.join(CLI_PATH, "package.json")).version;
 			const CONTROLS_PATH = path.join(__dirname, "node_modules", "@chartiq", "finsemble-react-controls");
 			const CONTROLS_VERSION = require(path.join(CONTROLS_PATH, "package.json")).version;
+			
+			// Check e2o version
 			const E2O_PATH = path.join(__dirname, "node_modules", "@chartiq", "e2o");
-			const E2O_VERSION = channelAdapter === "e2o" && fs.existsSync(E2O_PATH) ? require(path.join(E2O_PATH, "package.json")).version : undefined ;
-
-			if (channelAdapter === "e2o" && !fs.existsSync(E2O_PATH)) {
+			const E2O_PATH_EXISTS = fs.existsSync(E2O_PATH);
+			const USING_E2O = channelAdapter === "e2o";
+			if (USING_E2O && !E2O_PATH_EXISTS) {
 				throw "Cannot use e2o channelAdapter unless e2o optional dependency is installed. Please run npm i @chartiq/e2o";
 			}
+
+			const E2O_VERSION = require(path.join(E2O_PATH, "package.json")).version;
 
 			function checkLink(params, cb) {
 				let { path, name, version } = params;
