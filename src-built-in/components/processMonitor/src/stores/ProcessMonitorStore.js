af9b13efb91800f8efb7f7695ee4b3c0b5284fff
Brad Carter <brad@chartiq.com>
2019-02-28 15:04:52 -0500

    Update ProcessMonitorStore.js
    bsLint

--- src-built-in/components/processMonitor/src/stores/ProcessMonitorStore.js
+++ src-built-in/components/processMonitor/src/stores/ProcessMonitorStore.js
@@ -341,7 +341,7 @@ var Actions = {
 				FSBL.Clients.DialogManager.open("yesNo", {
 					title: "Terminate Process?",
 					question: "The app that you are attempting to close is unresponsive. Would you like to terminate the process? Terminating the process may close other apps.",
-					affirmativeResponseLabel:"Terminate",
+					affirmativeResponseLabel: "Terminate",
 					showNegativeButton: false
 				}, (err, response) => {
 					if (err || response.choice === "affirmative") {
