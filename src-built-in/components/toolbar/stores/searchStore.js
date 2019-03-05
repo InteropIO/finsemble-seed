1845cbf9c2ccef35dbd2def280313a4759a4a8a5
mhmcclung <mm@chartiq.com>
2019-03-05 15:47:13 -0500

    save

--- src-built-in/components/toolbar/stores/searchStore.js
+++ src-built-in/components/toolbar/stores/searchStore.js
@@ -247,7 +247,7 @@ function initialize(cb) {
 	], function (err) {
 		if (err) {
 			console.error(err);
-			}
+		}
 		FSBL.SystemManagerAPI.publishCheckpointState("Toolbar", "searchStoreInit", "completed");
 		cb(menuStore);
 	});
