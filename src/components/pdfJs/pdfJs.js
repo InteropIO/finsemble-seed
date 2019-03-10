7b4673ef58e8e2026b0f758c0c2c50a97a15bae6
Kris West <kris@chartiq.com>
2019-03-10 11:11:23 +0000

    Set window title on PDF load and adjust linker/DnD support

--- src/components/pdfJs/pdfJs.js
+++ src/components/pdfJs/pdfJs.js
@@ -3,25 +3,66 @@
 
 const FSBLReady = () => {
 	try {
-		FSBL.Clients.WindowClient.setWindowTitle(document.title);
-		// create an observer instance
-		var observer = new MutationObserver(function(mutations) {
-			// We need only first event and only new value of the title
-			FSBL.Clients.WindowClient.setWindowTitle(document.title);
-		});
-		// configuration of the observer:
-		var config = { subtree: true, characterData: true };
-		// pass in the target node, as well as the observer options
-		observer.observe(document.title, config);
-		
-		//Check for a URL to load
-		let data = FSBL.Clients.WindowClient.getSpawnData();
-		if (data && data.url) {
-			console.log("Got data URL: " + data.url);
-			PDFViewerApplication.open(data.url);
-		} else {
-			console.log("No PDF URL to load");
+		//observe page title to set window title on PDF load
+		const target = document.querySelector('title');
+		const config = {
+			attributes: true,
+			attributeOldValue: true,
+			characterData: true,
+			characterDataOldValue: true,
+			childList: true,
+			subtree: true
+		};
+		function subscriber(mutations) {
+			mutations.forEach((mutation) => {
+				FSBL.Clients.WindowClient.setWindowTitle(mutation.target.textContent);
+			});
 		}
+		const observer = new MutationObserver(subscriber);
+		observer.observe(target, config);
+		
+		//Check for a URL to load in spawn data
+		let spawndata = FSBL.Clients.WindowClient.getSpawnData();
+		if (spawndata && spawndata.url) {
+			FSBL.Clients.Logger.log("Got spawn data URL: " + spawndata.url);
+			PDFViewerApplication.open(spawndata.url);
+			FSBL.Clients.WindowClient.setWindowTitle(document.title);
+		} else {
+			console.log("No PDF URL to load from spawn data");
+		}
+		
+		//Support linker messages on the url and pdf channels
+		let linkerHandler = function(linkerdata){
+			if (linkerdata && (linkerdata.url || linkerdata.pdf)) {
+				let url = linkerdata.url ? linkerdata.url : linkerdata.pdf;
+				FSBL.Clients.Logger.log("Got linker data URL: " + url);
+				PDFViewerApplication.open(url);
+				FSBL.Clients.WindowClient.setWindowTitle(document.title);
+			} else {
+				console.log("No PDF URL to load from linker");
+			}
+		};
+		FSBL.Clients.LinkerClient.subscribe("url", linkerHandler);
+		FSBL.Clients.LinkerClient.subscribe("pdf", linkerHandler);
+	
+		//support Drag and Drop
+		FSBL.Clients.DragAndDropClient.addReceivers({
+			receivers: [
+				{
+					type: 'pdf',
+					handler: function (err, response) {
+						if (!err) { linkerHandler(response.data); }
+					}
+				},
+				{
+					type: 'url',
+					handler: function (err, response) {
+						if (!err) { linkerHandler(response.data); }
+					}
+				}
+			]
+		});
+
 	} catch (e) {
 		FSBL.Clients.Logger.error(e);
 	}
