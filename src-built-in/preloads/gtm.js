00716a3ccbe8541a7d80132d1b982d6016c76507
WatsonCIQ <chris.watson@chartiq.com>
2019-03-19 16:18:43 +0000

    Instruction comments added. How to use the preload and GTM.

--- src-built-in/preloads/gtm.js
+++ src-built-in/preloads/gtm.js
@@ -1,6 +1,18 @@
 /*
 	Google Tag Manager Preload
+	==========================
+	Google Tag Manager can track:
+	- page views (these are the component views / windows)
+	- element interactions - button clicks
+	- javascript errros
+	and more
+
+	The three actions listed above need no extra code and can be done via GTM directly.
+
+	Once integrated with GTM it is best to use Google Analatics but alternatives can be used.
 */
+
+const gtmID = "GTM-55JT28T";
 
 const startGoogleTagManager = () => {
 	// includes the GTM script dynamically
@@ -21,9 +33,10 @@ const startGoogleTagManager = () => {
 		j.async = true;
 		j.src = "https://www.googletagmanager.com/gtm.js?id=" + i + dl;
 		f.parentNode.insertBefore(j, f);
-	})(window, document, "script", "dataLayer", "GTM-55JT28T");
+	})(window, document, "script", "dataLayer", gtmID);
 };
 
+// add gtm as early as possible
 window.addEventListener("DOMContentLoaded", event => {
 	startGoogleTagManager();
 });
