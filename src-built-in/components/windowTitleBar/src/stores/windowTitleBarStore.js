3425335f22ddeb7724ced74a9af178bed82f20cb
Siddharth Dalal <sgd2z@users.noreply.github.com>
2019-05-23 11:05:55 -0400

    fix: #id 13489 revert change to workspace format (#385)
    fix: #id 13489
    
    **Description of change**
    * This fix preserves backward compatibility with 3.8.x for workspace format. Related to https://github.com/ChartIQ/finsemble/pull/1288
    
    **Description of testing**
    * See https://github.com/ChartIQ/finsemble/pull/1288

--- src-built-in/components/windowTitleBar/src/stores/windowTitleBarStore.js
+++ src-built-in/components/windowTitleBar/src/stores/windowTitleBarStore.js
@@ -564,12 +564,12 @@ var Actions = {
 		if (Actions.parentWrapper) {
 			cb();
 		} else {
-			FSBL.Clients.WindowClient.getStackedWindow(params, (err, response) => {
+			FSBL.Clients.WindowClient.getStackedWindow(params, (err, wrap) => {
 				if (err) {
 					Actions.parentWrapper = null;
 					Actions._setTabs(null)
 				} else {
-					Actions.parentWrapper = FSBL.Clients.WindowClient.finsembleWindow.getParent();
+					Actions.parentWrapper = wrap;
 					Actions.setupStore(cb);
 				}
 			})
