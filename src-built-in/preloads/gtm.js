e130368baae8218640d8a8ec08f9d99deb1bb53b
WatsonCIQ <chris.watson@chartiq.com>
2019-06-18 10:49:15 +0100

    Added video links to the code comments.

--- src-built-in/preloads/gtm.js
+++ src-built-in/preloads/gtm.js
@@ -4,17 +4,24 @@
 	Google Tag Manager can track:
 	- page views (these are the component views / windows)
 	- element interactions - button clicks
-	- javascript errros
+	- javascript errors
 	and more
 
 	The three actions listed above need no extra code and can be done via GTM directly.
 
-	Once integrated with GTM it is best to use Google Analatics but alternatives can be used.
+	Once integrated with GTM it is best to use Google Analytics but alternatives can be used.
 
 
 	Instructions for use:
 	Add to each component you would like to track via the config file i.e.
-		presentationalComponents.json / conponents.json
+		presentationalComponents.json / components.json
+
+	Videos on how to use with Google Analytics:
+	1) Create Account - https://drive.google.com/open?id=1-j9dJiy_DKO_2CI29ie4n8HFSQRP1ykU
+	2) Intro to interactions - https://drive.google.com/open?id=1-h8vqwGnZpZhJJ-mJdSrFUOg5FUNSaGA
+	3) Save your updates ! - https://drive.google.com/open?id=1-e3GP3V_NmM9239N6McUtxLyZfzHGtdz
+	4) CSS element clicks / interactions - https://drive.google.com/open?id=1-kdFVrHtksDFun_UE0RUPGDBrjojpy2-
+	5) Javascript error logging - https://drive.google.com/open?id=1-XXoFyWDc-BBXuSccd6UyF8WmPKV8QMe
 */
 
 const gtmID = "GTM-55JT28T";
@@ -26,7 +33,7 @@ const startGoogleTagManager = () => {
 
 	window.dataLayer = window.dataLayer || [];
 
-	(function(w, d, s, l, i) {
+	(function (w, d, s, l, i) {
 		w[l] = w[l] || [];
 		w[l].push({
 			"gtm.start": new Date().getTime(),
