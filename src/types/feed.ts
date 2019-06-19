669147460adaa66486cd70ba005091fcba71724a
Joel Carras <joel@chartiq.com>
2019-06-19 12:52:16 +0100

    more changes.

--- src/types/feed.ts
+++ src/types/feed.ts
@@ -2,28 +2,27 @@ import { INotification } from './notification';
 
 interface IFeedService {
     /*
-      Called whenever a new notification is delivered from a remote source. The send function needs to be
-      called whenever a notification needs to be sent to the notification service.
+      Called from FeedService.processNotifications whenever a new notification is delivered from a remote source
+      and need to be pushed into system. Registers a responder which to send notification to NotificationService.
     */
     handleNotification: (notification: INotification) => void;
 }
 
 interface IFeedDataService {
     /*
-        Most likely will query a remote source for notifications at a point in time. Used to pull in
+        Queries a remote source for notifications at a point in time. Used to pull in
         notifications which occurred when Finsemble was offline.
     */
     query(lastUpdatedTimestamp: string): INotification[];
     /*
-        This function will be implemented by Finsemble developer and needs to call FeedService.handleNotification
-        each time it wants to send a notification through the system.
+        Needs to call FeedService.handleNotification each time it wants to send a notification through the system.
     */
     processNotifications(): void;
     /*
         Called whenever a new notification is received successfully in Finsemble. Useful for logging,
         error handleing, or reporting back to the remote service, etc...
     */    
-   notificationReceived?(receivedTime: string, notification: INotification): void;
+    notificationReceived?(receivedTime: string, notification: INotification): void;
 }
 
 interface IFeedClient {
@@ -32,11 +31,6 @@ interface IFeedClient {
        This will call FeedService.query which will do the actual heavy lifting.
     */
     fetchSnapshot(lastUpdatedTimestamp: string): INotification[];
-    /* 
-       Helper method for NotificationsClient. This will call FeedService.handleNotification
-       with a call back which is triggered whenever a notification needs to be delivered.
-    */
-    listenForNotifications(responder: Function): void;
 }
 
 export { IFeedService, IFeedDataService, IFeedClient };
