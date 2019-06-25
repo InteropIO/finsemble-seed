31f5d8d6db90ae9b787ffb8d64d5b34e9365443b
WatsonCIQ <chris.watson@chartiq.com>
2019-06-25 14:58:53 +0100

    Fixed font loading Open Sans.
    Modified font size - toolbar and menus

--- build/webpack/defaultWebpackConfig.js
+++ build/webpack/defaultWebpackConfig.js
@@ -61,23 +61,23 @@ module.exports = class WebpackDefaults {
 					},
 					{
 						test: /\.svg$/,
-						loader: 'url-loader?limit=65000&mimetype=image/svg+xml&name=public/fonts/[name].[ext]'
+						loader: 'url-loader?limit=65000&mimetype=image/svg+xml&name=/public/fonts/[name].[ext]'
 					},
 					{
 						test: /\.woff$/,
-						loader: 'url-loader?limit=65000&mimetype=application/font-woff&name=public/fonts/[name].[ext]'
+						loader: 'url-loader?limit=65000&mimetype=application/font-woff&name=/public/fonts/[name].[ext]'
 					},
 					{
 						test: /\.woff2$/,
-						loader: 'url-loader?limit=65000&mimetype=application/font-woff2&name=public/fonts/[name].[ext]'
+						loader: 'url-loader?limit=65000&mimetype=application/font-woff2&name=/public/fonts/[name].[ext]'
 					},
 					{
 						test: /\.[ot]tf$/,
-						loader: 'url-loader?limit=65000&mimetype=application/octet-stream&name=public/fonts/[name].[ext]'
+						loader: 'url-loader?limit=65000&mimetype=application/octet-stream&name=/public/fonts/[name].[ext]'
 					},
 					{
 						test: /\.eot$/,
-						loader: 'url-loader?limit=65000&mimetype=application/vnd.ms-fontobject&name=public/fonts/[name].[ext]'
+						loader: 'url-loader?limit=65000&mimetype=application/vnd.ms-fontobject&name=/public/fonts/[name].[ext]'
 					},
 					{
 						test: /semver\.browser\.js/,
