4951722e53daf43689a6fd4319aca1746f1b1f1e
Erik <erik.mellum@chartiq.com>
2019-07-19 12:04:45 -0700

    Fix injectB

--- src-built-in/components/welcome/transmitInjectB.js
+++ src-built-in/components/welcome/transmitInjectB.js
@@ -1,2 +1,2 @@
 fin.desktop.logger.error('Transmit inject B success');
-FSBL.Clients.RouterClient.transmit("SomeChannelName", event);
+FSBL.Clients.RouterClient.transmit("SomeChannelName", { foo: 'bar' });
