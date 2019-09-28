996a0e3d6741538136bc4f4207e8cc148be29c23
WatsonCIQ <chris.watson@chartiq.com>
2019-09-28 23:43:22 +0100

    re-organized code to a better structure.

--- src/services/shutdown/shutdownService.js
+++ src/services/shutdown/shutdownService.js
@@ -11,7 +11,7 @@ Logger.start();
 Logger.log("shutdown Service starting up");
 
 // Add and initialize any other clients you need to use
-//   (services are initialised by the system, clients are not)
+// (services are initialized by the system, clients are not)
 WorkspaceClient.initialize();
 ConfigClient.initialize();
 DialogManager.initialize();
@@ -23,43 +23,7 @@ DialogManager.initialize();
 function shutdownService() {
 	let shutDownTimer = null;
 
-	this.shutdownFunction = function() {
-		// Get the shutdown day, hour and minutes from the config
-		ConfigClient.getValue(
-			{ field: "finsemble.scheduledShutdown" },
-			(err, config) => {
-				if (config) {
-					Logger.system.log(
-						"APPLICATION LIFECYCLE:SCHEDULED SHUTDOWN TIME:",
-						config
-					);
-				} else {
-					Logger.system.log(
-						"APPLICATION LIFECYCLE:SCHEDULED SHUTDOWN NOT CONFIGURED."
-					);
-				}
-				scheduleShutdown(err, config);
-			}
-		);
-
-		// If dynamically changing the schedule shutdown, log it out, and schedule the shutdown.
-		ConfigClient.addListener(
-			{ field: "finsemble.scheduledShutdown" },
-			(err, config) => {
-				if ("value" in config && config.value) {
-					Logger.system.log(
-						"APPLICATION LIFECYCLE:SCHEDULED SHUTDOWN TIME CHANGED. NEW TIME:",
-						config.value
-					);
-				} else {
-					Logger.system.log(
-						"APPLICATION LIFECYCLE:SCHEDULED SHUTDOWN DISABLED."
-					);
-				}
-				scheduleShutdown(err, config.value);
-			}
-		);
-
+	this.shutdownFunction = function () {
 		/**
 		 * Saves workspace and if it is dirty then  fires off a message shutting down the application.
 		 */
@@ -106,62 +70,68 @@ function shutdownService() {
 			});
 		};
 
-		const shutdownFinsemble = async () => {
-			// if the user cancels the event then don't shutdown
-			if ((await saveWorkspace()) === "cancel") return;
-			// code to shutdown Finsemble
-			RouterClient.transmit("Application.shutdown");
+		/**
+		 * set the timer to shutdown Finsemble
+		 * @param {Object} config
+		 * @returns {number} timeInMs - milliseconds
+		 */
+		const setShutdownTimer = config => {
+			const daysUntilShutdown = (restartDay, today) => {
+				const shutdownIsToday = restartDay - today === 0;
+				const shutdownIsNextWeek = restartDay - today < 0;
+
+				if (shutdownIsToday) {
+					return 0;
+				} else if (shutdownIsNextWeek) {
+					// 6 is how many full days until the same day next week
+					return restartDay - today + 6;
+				} else {
+					// shutdown day is coming up soon this week
+					return restartDay - today;
+				}
+			};
+
+			const timeInMsToShutdown = (shutdownTime, now) => {
+				// ensure the time has not passed
+				if (shutdownTime - now < 0) {
+					//if the time has passed then set the day to next week (same day)
+					shutdownTime.setDate(shutdownTime.getDate() + 7);
+				}
+				return shutdownTime - now;
+			};
+
+			const now = new Date();
+
+			const shutdownTime = new Date();
+			// using days to set the date for shutdown
+			shutdownTime.setDate(
+				now.getDate() + daysUntilShutdown(config.day, now.getDay())
+			);
+			shutdownTime.setHours(config.hour);
+			shutdownTime.setMinutes(config.minute);
+
+			return timeInMsToShutdown(shutdownTime, now);
 		};
 
 		/**
-		 * Schedule the timer and fire the shutdown function
-		 * @param {*} err
-		 * @param {*} config
+		 * The function that shuts down Finsemble
+		 * @param {error} err
+		 * @param {Object} config
 		 */
-		function scheduleShutdown(err, config) {
-			if (err) Logger.log(err);
-
-			if (config) {
-				const daysUntilShutdown = (restartDay, today) => {
-					const shutdownIsToday = restartDay - today === 0;
-					const shutdownIsNextWeek = restartDay - today < 0;
-
-					if (shutdownIsToday) {
-						return 0;
-					} else if (shutdownIsNextWeek) {
-						// 6 is how many full days until the same day next week
-						return restartDay - today + 6;
-					} else {
-						// shutdown day is coming up soon this week
-						return restartDay - today;
-					}
-				};
-
-				const timeInMsToShutdown = (shutdownTime, now) => {
-					// ensure the time has not passed
-					if (shutdownTime - now < 0) {
-						//if the time has passed then set the day to next week (same day)
-						shutdownTime.setDate(shutdownTime.getDate() + 7);
-					}
-					return shutdownTime - now;
-				};
-
-				const now = new Date();
-
-				const shutdownTime = new Date();
-				// using days to set the date for shutdown
-				shutdownTime.setDate(
-					now.getDate() + daysUntilShutdown(config.day, now.getDay())
-				);
-				shutdownTime.setHours(config.hour);
-				shutdownTime.setMinutes(config.minute);
-
-				const countdownTillShutdown = timeInMsToShutdown(shutdownTime, now);
-
+		const shutdownFinsemble = async (err, config) => {
+			if (err) {
+				Logger.log(err);
+				return err
+			}
+			else if (config) {
+				const shutdownTimer = setShutdownTimer(config);
 				// countdown timer until the shutdown
 				shutDownTimer = setTimeout(() => {
-					shutdownFinsemble();
-				}, countdownTillShutdown);
+					// if the user cancels the event then don't shutdown
+					if ((await saveWorkspace()) === "cancel") return;
+					// code to shutdown Finsemble
+					RouterClient.transmit("Application.shutdown");
+				}, shutdownTimer);
 				shutDownTimer;
 			} else {
 				if (shutDownTimer) {
@@ -169,10 +139,48 @@ function shutdownService() {
 					clearTimeout(shutDownTimer);
 				}
 			}
-		}
-	};
 
-	return this;
+		};
+
+
+		// Get the shutdown day, hour and minutes from the config
+		ConfigClient.getValue(
+			{ field: "finsemble.scheduledShutdown" },
+			(err, config) => {
+				if (config) {
+					Logger.system.log(
+						"APPLICATION LIFECYCLE:SCHEDULED SHUTDOWN TIME:",
+						config
+					);
+				} else {
+					Logger.system.log(
+						"APPLICATION LIFECYCLE:SCHEDULED SHUTDOWN NOT CONFIGURED."
+					);
+				}
+				shutdownFinsemble(err, config);
+			}
+		);
+
+		// If dynamically changing the schedule shutdown, log it out, and schedule the shutdown.
+		ConfigClient.addListener(
+			{ field: "finsemble.scheduledShutdown" },
+			(err, config) => {
+				if ("value" in config && config.value) {
+					Logger.system.log(
+						"APPLICATION LIFECYCLE:SCHEDULED SHUTDOWN TIME CHANGED. NEW TIME:",
+						config.value
+					);
+				} else {
+					Logger.system.log(
+						"APPLICATION LIFECYCLE:SCHEDULED SHUTDOWN DISABLED."
+					);
+				}
+				shutdownFinsemble(err, config.value);
+			}
+		);
+
+		return this;
+	}
 }
 
 shutdownService.prototype = new Finsemble.baseService({
