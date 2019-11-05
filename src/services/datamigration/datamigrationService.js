f36be27ffa5570ef09184168ff0eaf9d1360a81d
sonyl-ciq <sonyl@chartiq.com>
2019-11-04 22:37:35 -0500

    Logging cleanup

--- src/services/datamigration/datamigrationService.js
+++ src/services/datamigration/datamigrationService.js
@@ -35,18 +35,11 @@ class DataMigrationService extends BaseService {
 					}					
 
 					keysLength = data.length - 1;
-					Logger.log('*** datamigration keylength', data[0], `Data length ${keysLength}`);
-
 					data.forEach((finsembleKey) => {
-						Logger.log('*** datamigration datum', finsembleKey);
 						StorageClient.get({ topic: topic, key: finsembleKey }, (err, response) => {
-							Logger.log('*** datamigration datum', response);
-
 							if (err) {
 								reject(new Error(err));
 							}
-							Logger.log(`*** datamigration keys ${fetchedKeyCount}`)
-							//Logger.log('*** datamigration datum get response', finsembleKey,err, response, allData);
 							this.allData.push({ topic: topic, key: finsembleKey, value: response } );
 							fetchedKeyCount++;
 
@@ -68,7 +61,6 @@ class DataMigrationService extends BaseService {
 			this.allData.forEach((datum) => {
 				StorageClient.setStore({ topic: datum.topic, dataStore: "LocalStorageAdapter" }, (err, data) => {
 					StorageClient.save({ topic: datum.topic, key: datum.key, value: datum.value }, (err, response) => {
-						Logger.log(`*** datamigration Saved ${datum.key}`);
 						migratedKeysLength++;
 						if (migratedKeysLength == this.allData.length - 1) {
 							resolve()
@@ -78,6 +70,7 @@ class DataMigrationService extends BaseService {
 			});
 		});
 	}
+	
 	/**
 	 * fetchUserStatus
 	 * 
@@ -88,18 +81,13 @@ class DataMigrationService extends BaseService {
 	fetchUserStatus() {
 		// @TODO determine if the user was in DB from which to migration to begin with
 		StorageClient.get( { topic: "finsemble", key: `migrated_from_${this.adapter}` }, (err, data) => {
-			Logger.log(`*** datamigration data from storageclient ${data}`)
-
 			if (!data) {
-				Logger.log('*** datamigration spawning migration component');
-
 				LauncherClient.spawn("migration", {}, (err, response) => {
 					RouterClient.transmit('Migration', 'needed');
 					RouterClient.addListener('Migration', (err, event) => {
 						if (event.data == 'begin') {
 							const dataPromise = this.getAllData('finsemble');
 							dataPromise.then((res) => {
-								Logger.log('*** datamigration alldata', this.allData);
 								const dataPromise2 = this.getAllData('finsemble.workspace');
 								dataPromise2.then((res) => {
 									const dataPromise3 = this.getAllData('finsemble.workspace.cache');
@@ -116,8 +104,6 @@ class DataMigrationService extends BaseService {
 						}
 					});
 				});
-			} else {
-				Logger.log('*** datamigration already completed');
 			}
 		});
 	};
@@ -130,15 +116,12 @@ const dms = new DataMigrationService({
 });
 
 dms.onBaseServiceReady((callback) => {
-	Logger.log('*** datamigration service ready')
 	let ranMigrationCheck = false; //to make sure we only do this once, technically auth can be run multiple times
 	
 	RouterClient.subscribe("AuthorizationState", function(err,notify) {
 		if (err) {
 			Logger.log('*** datamigration failed to auth')
 		} else {
-			Logger.log('*** datamigration authed', notify)
-
 			if (!ranMigrationCheck && notify.data.state == "done") {
 				ranMigrationCheck = true;
 				dms.fetchUserStatus();
