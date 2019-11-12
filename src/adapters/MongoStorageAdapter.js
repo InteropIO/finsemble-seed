5b9beb4b96542db6e8dff7a0cf59b194c81e5477
Sonyl Nagale <55208294+sonyl-ciq@users.noreply.github.com>
2019-11-12 04:45:09 -0500

    [16253] MongoDB Storage Adapter Recipe README and Refinements. (#491)
    * Updating syntax to use class and extends
    
    * Additional refinements
    
    * README
    
    * Update src/adapters/README.md
    
    Co-Authored-By: Kris West <kris@chartiq.com>
    
    * Update src/adapters/README.md
    
    Co-Authored-By: Kris West <kris@chartiq.com>
    
    * Update src/adapters/README.md
    
    Co-Authored-By: Kris West <kris@chartiq.com>
    
    * Update src/adapters/README.md
    
    Co-Authored-By: Kris West <kris@chartiq.com>
    
    * Update src/adapters/README.md
    
    Co-Authored-By: Kris West <kris@chartiq.com>
    
    * Update src/adapters/MongoStorageAdapter.js
    
    Co-Authored-By: Kris West <kris@chartiq.com>
    
    * Update src/adapters/MongoStorageAdapter.js
    
    Co-Authored-By: Kris West <kris@chartiq.com>
    
    * Update src/adapters/MongoStorageAdapter.js
    
    Co-Authored-By: Kris West <kris@chartiq.com>
    
    * Update src/adapters/MongoStorageAdapter.js
    
    Co-Authored-By: Kris West <kris@chartiq.com>
    
    * Update src/adapters/MongoStorageAdapter.js
    
    Co-Authored-By: Kris West <kris@chartiq.com>
    
    * Update src/adapters/MongoStorageAdapter.js
    
    Co-Authored-By: Kris West <kris@chartiq.com>
    
    * Revision to refer to the file in the repository instead of inline.
    
    * Update src/adapters/MongoStorageAdapter.js
    
    Co-Authored-By: Kris West <kris@chartiq.com>
    
    * Update src/adapters/MongoStorageAdapter.js
    
    Co-Authored-By: Kris West <kris@chartiq.com>
    
    * Update src/adapters/MongoStorageAdapter.js
    
    Co-Authored-By: Kris West <kris@chartiq.com>

--- src/adapters/MongoStorageAdapter.js
+++ src/adapters/MongoStorageAdapter.js
@@ -16,13 +16,43 @@ const Logger = require("@chartiq/finsemble").Clients.Logger;
 // Because calls to this storage adapter will likely come from many different windows, we will log successes and failures in the central logger.
 Logger.start();
 
-const MongoStorageAdapter = function (uuid) {
-	BaseStorage.call(this, arguments);
+class MongoStorageAdapter extends BaseStorage {
+	constructor(uuid) {
+		super(arguments);
 
-	Logger.system.log("MongoStorageAdapter init");
+		Logger.system.log("MongoStorageAdapter init");
+		this.baseURL = "http://localhost:3001";
 
-	// This baseURL is the middleware to allow communication with MongoDB.
-	this.baseURL = "http://localhost:3001"; 
+		this.ROUTES = {
+			SAVE: `${this.baseURL}/save`,
+			GET: `${this.baseURL}/get`,
+			KEYS: `${this.baseURL}/keys`,
+			DELETE: `${this.baseURL}/delete`
+		};
+	}
+
+	/**
+	 * Get method.
+	 * @param {object} params
+	 * @param {string} params.topic A topic under which the data should be stored.
+	 * @param {string} params.key The key whose value is being set.
+	 * @param {function} cb Callback to be invoked upon completion.
+	 */
+	get = function(params, cb) {
+		console.log('get')
+		let combinedKey = this.getCombinedKey(this, params);
+		
+		fetch(`${this.ROUTES.GET}?key=${combinedKey}`)
+			.then(response => response.json())
+			.then(data => {
+				Logger.system.debug("Mongo.get for key=" + combinedKey + " data=", data.value);
+				cb(null, data.value);
+			})
+			.catch(err => {
+				Logger.system.error("Mongo.get key=" + combinedKey + ", Error", err);
+				cb(err, { status: "failed" });
+			});
+	};
 
 	/**
 	 * Save method.
@@ -32,16 +62,16 @@ const MongoStorageAdapter = function (uuid) {
 	 * @param {any} params.value The value being saved.
 	 * @param {function} cb Callback to be invoked upon save completion.
 	 */
-	this.save = function (params, cb) {
+	save = function(params, cb) {
 		let combinedKey = this.getCombinedKey(this, params);
 
 		Logger.system.debug("Mongo.save for key=" + combinedKey + " with data=" + params.value);
 		
-		fetch(`${this.baseURL}/save`, {
-			method: 'POST',
+		fetch(`${this.ROUTES.SAVE}`, {
+			method: "POST",
 			body: JSON.stringify({ key: combinedKey, value: params.value }),
 			headers: {
-				'Content-Type': 'application/json'
+				"Content-Type": "application/json"
 			}
 		})
 		.then(data => {
@@ -54,52 +84,43 @@ const MongoStorageAdapter = function (uuid) {
 	};
 
 	/**
-	 * Get method.
+	 * Returns all keys stored in MongoDB for the user and an optional topic and keyPrefix.
+	 *
 	 * @param {object} params
-	 * @param {string} params.topic A topic under which the data should be stored.
-	 * @param {string} params.key The key whose value is being set.
-	 * @param {function} cb Callback to be invoked upon completion.
+	 * @param {string} params.topic The topic (optional).
+	 * @param {string} params.keyPrefix The key prefix (optional).
+	 * @param {function} cb
 	 */
-	this.get = function (params, cb) {
-		let combinedKey = this.getCombinedKey(this, params);
-		
-		fetch(`${this.baseURL}/get?key=${combinedKey}`)
+	keys = function(params, cb) {
+		const keyPreface = this.getKeyPreface(params);
+
+		fetch(`${this.ROUTES.KEYS}?param=${keyPreface}`)
 			.then(response => response.json())
 			.then(data => {
-				Logger.system.debug("Mongo.get for key=" + combinedKey + " data=", data.value);
+				Logger.system.debug("Mongo.get for keys" + keyPreface + " data=", data.value);
 				cb(null, data.value);
 			})
 			.catch(err => {
-				Logger.system.error("Mongo.get key=" + combinedKey + ", Error", err);
+				Logger.system.error("Mongo.get keys" + keyPreface + ", Error", err);
 				cb(err, { status: "failed" });
 			});
 	};
 
-	// Return prefix used to filter keys.
-	this.getKeyPreface = function (self, params) {
-		let preface = self.baseName + ":" + self.userName + ":" + params.topic + ":";
-		if ("keyPrefix" in params) {
-			preface = preface + params.keyPrefix;
-		}
-		return preface;
-	};
-
-
 	/**
 	 * Delete method.
 	 * @param {object} params
-	 * @param {string} params.topic A topic under which the data should be stored.
+	 * @param {string} params.topic The topic under which the data is stored.
 	 * @param {string} params.key The key whose value is being deleted.
 	 * @param {function} cb Callback to be invoked upon completion.
 	 */
-	this.delete = function (params, cb) {
+	delete = function(params, cb) {
 		let combinedKey = this.getCombinedKey(this, params);
 
 		Logger.system.debug("Mongo.delete for key=" + combinedKey);
 
 
-		fetch(`${this.baseURL}/delete`, {
-			method: 'DELETE',
+		fetch(`${this.ROUTES.DELETE}`, {
+			method: "DELETE",
 			body: JSON.stringify({ key: combinedKey }),
 			headers: {
 				'Content-Type': 'application/json'
@@ -113,9 +134,52 @@ const MongoStorageAdapter = function (uuid) {
 			cb(err, { status: "failed" });
 		});
 	};
-};
 
+	/**
+	 * Get the prefix used to filter keys with an optional topic and keyPrefix.
+	 * Note that if topic is not provided, keyPrefix is also ignored.
+	 *
+	 * @param {object} params
+	 * @param {string} params.topic The topic (optional).
+	 * @param {string} params.keyPrefix The key prefix (optional).
+	 * @private
+	 */
+	getKeyPreface = function(params) {
+		const keyPrefix = "keyPrefix" in params ? params.keyPrefix : "";
+		if (params && params.topic){
+			return `${this.getUserPreface()}${params.topic}:${keyPrefix}`;
+		} else {
+			return `${this.getUserPreface()}`;
+		}
 
-MongoStorageAdapter.prototype = new BaseStorage();
-new MongoStorageAdapter("MongoStorageAdapter");
-module.exports = MongoStorageAdapter;//Allows us to get access to the uninitialized object
+		return preface;
+	};
+
+	/**
+	 * Get prefix for all the users stored data.
+	 * @private
+	 */
+	getUserPreface = function() {
+		const preface = `${this.baseName}:${this.userName}:`;
+		return preface;
+	};
+
+	/**
+	 * This method should be used very, very judiciously. It's essentially a method designed to wipe the database for a
+	 * particular user.
+	 */
+	clearCache = function(params, cb) {
+		Logger.system.error("clearCache is not implemented for the MongoStorageAdapter");
+	};
+
+	/**
+	 * Wipes the storage container.
+	 * @param {function} cb
+	 */
+	empty = function(cb) {
+		Logger.system.error("empty is not implemented for the MongoStorageAdapter");
+	};
+}
+
+let mongoAdapter = new MongoStorageAdapter("MongoStorageAdapter")
+module.exports = mongoAdapter; // Allows us to get access to the uninitialized object
