4d3cd3e30b22149db316461b175991b8d02d4f36
sonyl-ciq <sonyl@chartiq.com>
2019-11-13 10:59:26 -0500

    Removing unneeded parts as per KW

--- src/components/migration/migration.js
+++ src/components/migration/migration.js
@@ -16,28 +16,6 @@ const FSBLReady = () => {
 		 * 
 		 */
 		FSBL.Clients.RouterClient.addListener("Migration", (err, event) => {
-			if (event.data == "needed") {
-				// The Migration component isn't intended to be visible to users in a menu, but just in case, there is a default message.
-				// However, if migration is needed, that message will be hidden and migration activities begun.
-				document.querySelector("#migrationCheck").classList.add("hidden");
-				
-				// Warn the user that migration is needed.
-				document.querySelector("#migrationWarning").classList.remove("hidden");
-
-				// Set a 30 second timer.
-				let timeout = 30000;
-
-				migrationTimer = setInterval(() => {
-					timeout -= 1000;
-
-					document.querySelector("#countdown").innerHTML = (timeout / 1000);
-
-					if (timeout === 0) {
-						clearInterval(migrationTimer);
-						FSBL.Clients.RouterClient.transmit("Migration", "begin");
-					}
-				}, 1000);	
-			}
 
 			if (event.data == "end") {
 				document.querySelector("#migrationWarning").classList.add("hidden");
