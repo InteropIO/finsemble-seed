740947484c8def55d6013116c99ee92c79c98d87
Kris West <kris@chartiq.com>
2019-11-22 13:26:44 +0000

    Recipes/data migration (#505)
    Merge sonyl-ciq:recipes/DataMigration into final recipe branch

---
+++ src/services/datamigration/README.md
@@ -0,0 +1,88 @@
+[![Finsemble Logo](https://documentation.chartiq.com/finsemble/styles/img/Finsemble_Logo_Dark.svg)](https://documentation.chartiq.com/finsemble/)
+
+# Finsemble Recipe: Data Migration
+
+In some cases, users have been deployed using Chromium's IndexedDB as the default storage adapter, as this is the default for Finsemble's seed project. For more sustainable usage across multiple machines and centralized handling of data, migration to a remote database is often times needed.
+
+In other cases, migration from one remote datastore to another is needed. This recipe, with appropriate modifications, can handle both cases.
+
+For the purposes of this example, the **current** datasource will be **IndexedDB**. _N.b._: this recipe does not utilize a remote data adapter. It utilizes **LocalStorage** as a mimic for data migration. It is up to you to create the storage adapter you'd like to use and register it by comparing the recipe to your extant code. [This tutorial](https://documentation.chartiq.com/finsemble/tutorial-storingData.html) has more information on storage adapter fundamentals.
+
+## Ingredients
+
+This recipe contains a service and a component to assist in setting up a migration workflow. Here are the provided files in the `src` directory of this branch:
+
+```
+src
+├── adapters
+├── clients
+├── components
+│   └── datamigration
+│       ├── config.json
+│       ├── finsemble.webpack.json
+│       ├── datamigration.css
+│       ├── datamigration.html
+│       └── datamigration.js
+├── services
+│   └── datamigration
+│       ├── README.md
+│       ├── config.json
+│       ├── datamigration.html
+│       ├── datamigration.png
+│       └── datamigrationService.js
+└── thirdParty
+```
+
+
+### Service: `datamigration`
+
+**Flowchart**
+
+![Data Migration Flowchart](./datamigration.png)
+
+
+1. If the user has no data (is a new user) or has already been migrated, the Migration Assistant component should not spawn.
+1. If the user has data in the current storage adapter, a simple message displays with warning copy and action buttons.
+1. Upon completion, Finsemble should be restarted.
+
+### Data Migration Assistant Component: `datamigration`
+
+The Migration Assistant component is a basic HTML5 component designed to communicate to the user the state of their data. This component is a sample and is designed to be customized to your needs.   
+
+## Directions
+
+1. Copy the `src/services/datamigration` directory from this branch in your Finsemble `src/services` directory.
+1. Copy the `src/components/datamigration` directory from this branch to your Finsemble `src/components` directory.
+1. Modify `configs/application/config.json:servicesConfig.storage` topics to use `LocalStorageAdapter` or your custom storage adapter that you've already created as per [the documentation](https://documentation.chartiq.com/finsemble/tutorial-storingData.html).
+    ```json
+    "storage": {
+			"defaultStorage": "LocalStorageAdapter",
+			"topicToDataStoreAdapters": {
+				"finsemble": "LocalStorageAdapter",
+				"finsemble.workspace": "LocalStorageAdapter",
+				"finsemble.workspace.cache": "IndexedDBAdapter"
+			},
+			"dataStoreAdapters": {
+				"LocalStorageAdapter": "$applicationRoot/adapters/localStorageAdapter.js",
+				"IndexedDBAdapter": "$applicationRoot/adapters/indexedDBAdapter.js"
+			}
+        }
+    }
+    ```
+
+    Note: as `finsemble.workspace.cache` is a topic that is very frequently read from and written to, it is not advisable to use an external storage adapter for it.
+
+1. In the  `finsemble.importConfig` array of your [manifest](https://documentation.chartiq.com/finsemble/tutorial-Configuration.html), include references to the component and service `config.json`:
+    
+    ```json
+    "importConfig": [
+        ...
+        "$applicationRoot/components/datamigration/config.json",
+        "$applicationRoot/services/datamigration/config.json",
+        ...
+    ],
+    ```
+1. Install if needed and start Finsemble: `npm install; npm run dev`.
+1. After the user is authenticated, the Data Migration Assistant component will or will not display as per the logic from the service, as above.
+
+Note that this recipe is a non-destructive migration in that it does not clear the prior datastore. 
---
+++ src/services/datamigration/datamigrationService.js
@@ -0,0 +1,238 @@
+const Finsemble = require("@chartiq/finsemble");
+const BaseService = Finsemble.baseService;
+const {
+	RouterClient,
+	Logger,
+	StorageClient,
+	LauncherClient
+} = Finsemble.Clients;
+
+Logger.start();
+StorageClient.initialize();
+LauncherClient.initialize();
+
+/**
+ * class DataMigrationService
+ * 
+ * This class is configurable to migrate data from one storage adapter to another.
+ * In this example, IndexedDB data is being migrated to LocalStorage for all internal Finsemble data.
+ * 
+ */
+class DataMigrationService extends BaseService {
+	constructor(args) {
+		super(args);
+		this.adapter = "IndexedDBAdapter";
+		this.newAdapter = "LocalStorageAdapter";
+	}
+
+	/**
+	 * retrieveDatabase
+	 * 
+	 * Takes all Storage Adapter topics provided and creates an array of objects for all topics
+	 * Invokes saveAllData afterwards.
+	 * 
+	 * @param {Array} topics The storage topics to be migrated as specified in TOPICS
+	 */
+	retrieveDatabase(topics) {
+		const promiseSet = [];
+
+		topics.forEach((topic) => {
+			promiseSet.push(new Promise((resolve, reject) => {
+				// Reset to use old storage adapter to retrieve all the user's data
+				StorageClient.setStore({ topic: topic, dataStore: this.adapter }, (err, data) => {
+					if (err) {
+						reject(new Error(err));
+					}
+					resolve();
+				});
+			}).catch((error) => {
+				Logger.error("*** datamigration error", error);
+			}));
+		});
+
+		Promise.all(promiseSet).then(() => {
+			const keysPromises = [];
+
+			topics.forEach((topic) => {
+				keysPromises.push(new Promise((resolve, reject) => {
+					StorageClient.keys({ topic: topic }, (err, keys) => {
+						if (err) {
+							reject(err);
+						}
+						
+						const topickeypairs = [];
+
+						keys.forEach((key) => {
+							topickeypairs.push({ topic: topic, key: key });
+						});
+
+						resolve(topickeypairs);
+					});
+				}).catch((error) => {
+					Logger.error("*** datamigration error", error);
+				}));
+
+				Promise.all(keysPromises).then((allpairs) => {
+					const pairs = allpairs.reduce((acc, curr) => {
+						return [...acc, ...curr];
+					});
+		
+					const valuesPromises = [];
+		
+					pairs.forEach((pair) => {
+						valuesPromises.push(new Promise((resolve, reject) => {
+							StorageClient.get({ topic: pair.topic, key: pair.key }, (err, value) => {
+								if (err) {
+									reject(err);
+								}
+								resolve({topic: pair.topic, key: pair.key, value: value});
+							});
+						}).catch((error) => {
+							Logger.error("*** datamigration error", error);
+						}));
+					});
+		
+					Promise.all(valuesPromises).then((data) => {
+						this.saveAllData(topics, data);
+					});
+				});
+			});
+		});
+	}
+
+	/**
+	 * saveAllData
+	 * 
+	 * Saves data from the old storage adapter into the new one.
+	 * 
+	 * Invoked when all data has been retrieved from the old adapter.
+	 * @param {Array} topics Array of all topics retrieved.
+	 * @param {Array} data Array of all data received from retrieveDatabase
+	 */
+	saveAllData(topics, dataset) {
+		const promiseSet = [];
+
+		topics.forEach((topic) => {
+			promiseSet.push(new Promise((resolve, reject) => {
+				// Reset to use new storage adapter
+				StorageClient.setStore({ topic: topic, dataStore: this.newAdapter }, (err, data) => {
+					if (err) {
+						reject(new Error(err));
+					}
+					resolve(data);
+				});
+			}).catch((error) => {
+				Logger.error("*** datamigration error", error);
+			}));
+		});
+
+		Promise.all(promiseSet).then(() => {
+			const saveSet = [];
+			
+			saveSet.push(new Promise((resolve, reject) => {
+
+				dataset.forEach((datum) => {
+					StorageClient.save({ topic: datum.topic, key: datum.key, value: datum.value }, (err, response) => {
+						if (err) {
+							reject(new Error(err));
+						}
+						resolve(response);
+					});
+				});
+			}).catch((error) => {
+				Logger.error("*** datamigration error", error);
+			}));
+
+			Promise.all(saveSet).then(() => {
+				StorageClient.save({ topic: "finsemble", key: `migrated_from_${this.adapter}`, value: Date.now() });
+				RouterClient.publish("Migration", "end");
+			});
+		});
+	}
+
+	/**
+	 * fetchUserStatus
+	 * 
+	 * Determines if the user has been migrated from IndexedDBAdapter (or another storage adapter)
+	 * by checking for a key in the new storage service of "migrated_from_<adapter to migrate from>", 
+	 * which in this example is "migrated_from_IndexedDBAdapter".
+	 */
+	fetchUserStatus() {
+		StorageClient.get({ topic: "finsemble", key: `migrated_from_${this.adapter}` }, (err, data) => {
+			if (err) {
+				Logger.log(`Error fetching the user migration status for migration from ${this.adapter}.`);
+			}
+
+			if (!data) {
+				// The user does not have a migration record.
+
+				StorageClient.setStore({ topic: "finsemble", dataStore: this.adapter }, (err) => {
+					if (err) {
+						Logger.error("*** datamigration failed to set store", err);
+					}
+					StorageClient.keys({ topic: "finsemble" }, (err, keys) => {
+						if (err) {
+							Logger.error("*** datamigration failed to fetch", err);
+						}
+	
+						if (keys) {
+							// The user has Finsemble records and thus should be migrated.
+							RouterClient.addPubSubResponder("Migration", { "State" : "start"}, {
+								publishCallback: (err, publish) => {
+									if (err) {
+										Logger.error("*** datamigration failed to publish callback", err);
+									}
+
+									if (publish) {
+										publish.sendNotifyToAllSubscribers(null, publish.data);
+
+										if (publish.data == "begin") {
+											Logger.log("*** datamigration begin")
+											this.retrieveDatabase(TOPICS);
+										}
+									}
+								}
+							});
+
+							LauncherClient.spawn("datamigration", {}, () => {
+								RouterClient.publish("Migration", "needed");
+
+							});
+						} else {
+							// This is a new user. Set their migration record as 0 so they're not checked again.
+							StorageClient.save({ topic: "finsemble", key: `migrated_from_${this.adapter}`, value: 0 });
+							RouterClient.publish("Migration", "not needed");
+						}
+					});
+				});
+			}
+		});
+	}
+}
+
+const TOPICS = ["finsemble", "finsemble.workspace"];
+
+const dms = new DataMigrationService({ 
+	startupDependencies: {
+		services: ["authenticationService", "configService", "storageService"]
+	}
+});
+
+dms.onBaseServiceReady((callback) => {
+	let ranMigrationCheck = false; //to make sure we only do this once, as technically auth can be run multiple times
+	
+	RouterClient.subscribe("AuthorizationState", function(err,notify) {
+		if (err) {
+			Logger.error("*** datamigration failed to auth");
+		} else {
+			if (!ranMigrationCheck && notify.data.state == "done") {
+				ranMigrationCheck = true;
+				dms.fetchUserStatus();
+			}	
+		}
+	})
+	callback();
+});
+
+dms.start();
+module.exports = dms;
