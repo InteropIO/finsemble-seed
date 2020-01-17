187be9e3770f83e9aeca2f5e649045b8cb9292b5
zona <zona@chartiq.com>
2020-01-17 14:53:12 -0500

    refactor the linker code to use TypeScript

--- src-built-in/components/UIAPI/reducers/index.js
+++
@@ -1,4 +0,0 @@
-import { combineReducers } from "redux-loop";
-import linker from "./linker";
-
-export default combineReducers({ linker });
--- src-built-in/components/UIAPI/store.js
+++
@@ -1,5 +0,0 @@
-import { createStore } from "redux";
-import rootReducer from "./reducers";
-import { install } from "redux-loop";
-
-export default createStore(rootReducer, undefined, install());
