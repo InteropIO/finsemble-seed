a1caa04ea6d196ab1265f33298afefd9c6c71361
Kris West <kris@chartiq.com>
2020-01-27 09:31:47 +0000

    adding visibility change to autoHide button to match autoshow

--- src-built-in/components/windowTitleBar/src/components/right/AutoHide.jsx
+++ src-built-in/components/windowTitleBar/src/components/right/AutoHide.jsx
@@ -70,7 +70,11 @@ export default class AutoHide extends React.Component {
 		if (this.state && this.state.autoHide) wrapClasses += "fsbl-icon-highlighted ";
 		const tooltip = "Auto-hide window chrome";
 
-		return (<div className={wrapClasses} id="fsbl-window-autohide" title={tooltip} data-hover={this.state.hoverState} onClick={this.changeAutoHide}>
+		return (<div className={wrapClasses} 
+					id="fsbl-window-autohide" title={tooltip} 
+					data-hover={this.state.hoverState} 
+					onClick={this.changeAutoHide} 
+					style={this.state.visible ? {} : { display: "none" }}>
 				<FinsembleHoverDetector edge="top" hoverAction={this.hoverAction} />
 			<i className={iconClasses}></i>
 		</div>);
