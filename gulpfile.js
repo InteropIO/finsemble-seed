7d46d8f62b380b06a9a8c946416c2450c21f3ab8
Matt Swartz <matt@twocloud.net>
2020-01-27 10:24:08 -0500

    First pass with installer variables

--- gulpfile.js
+++ gulpfile.js
@@ -470,10 +470,19 @@ const { launch, connect } = require('hadouken-js-adapter');
 			// Inline require because this file is so large, it reduces the amount of scrolling the user has to do.
 			let installerConfig = require("./configs/other/installer.json");
 
+			let packageJson = require("./package.json");
+			// Overrides from package.json and environment variables
+			installerConfig.version = packageJson.version; 
+			installerConfig.name = process.env.installer_Name || installerConfig.name;
+			installerConfig.outputDirectory = process.env.installer_outputDirectory || installerConfig.outputDirectory;
+			installerConfig.certificatefile = process.env.installer_certificatefile || installerConfig.certificatefile;
+			installerConfig.certificatePassword = process.env.installer_certificatePassword || installerConfig.certificatePassword;
+			installerConfig.signWithParams = process.env.installer_signWithParams || installerConfig.signWithParams;
+		
 			// need absolute paths for certain installer configs
 			installerConfig = resolveRelativePaths(installerConfig, ['icon'], './');
-
-			const manifestUrl = taskMethods.startupConfig[env.NODE_ENV].serverConfig;
+			const manifestUrl = process.env.manifestUrl || taskMethods.startupConfig[env.NODE_ENV].serverConfig;
+			console.log("Manifest is:" + manifestUrl);			
 			let updateUrl = taskMethods.startupConfig[env.NODE_ENV].updateUrl;
 			const chromiumFlags = taskMethods.startupConfig[env.NODE_ENV].chromiumFlags;
 
