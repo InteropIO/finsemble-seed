c2fff15a599eafe87abbf93689cede93b4aaf062
Brad Carter <brad@chartiq.com>
2020-02-05 16:06:38 -0500

    comment out the old toolbar by default

--- src-built-in/components/toolbar/src/app.jsx
+++ src-built-in/components/toolbar/src/app.jsx
@@ -5,7 +5,7 @@
 
 // Static vs Dynamic Toolbar
 // import Toolbar from "./staticToolbar";
-import Toolbar from "./dynamicToolbar";
+// import Toolbar from "./dynamicToolbar";
 import ToolbarRefactored from "../../toolbarRefactored/app.tsx";
 import onReady from "../../UIAPI/effects/index";
 import ReactDOM from "react-dom";
