a1653f217ecd789295100b117849491284ba18f5
Brad Carter <brad@chartiq.com>
2020-02-05 16:51:46 -0500

    feat: #id 21676 create toolbar shell (#584)
    Add toolbar shell

--- src-built-in/components/toolbar/src/app.jsx
+++ src-built-in/components/toolbar/src/app.jsx
@@ -4,5 +4,28 @@
 */
 
 // Static vs Dynamic Toolbar
-import Toolbar from "./dynamicToolbar";
 // import Toolbar from "./staticToolbar";
+import Toolbar from "./dynamicToolbar";
+import ToolbarRefactored from "../../toolbarRefactored/app.tsx";
+import onReady from "../../UIAPI/effects/index";
+import ReactDOM from "react-dom";
+import React from "react";
+
+const ToolbarShell = () => {
+	return (
+		<ToolbarRefactored>
+			New Toolbar. Delete this line when the first component is added.
+			{/* Drag Handle */}
+			{/* Workspace Management Menu */}
+			{/* App Menu */}
+			{/* Favorites Section */}
+			{/* Minimize All */}
+			{/* Reveal All */}
+			{/* Auto Arrange */}
+		</ToolbarRefactored>
+	)
+}
+onReady(() => ReactDOM.render(
+	<ToolbarShell />,
+	document.getElementById("toolbar_refactored"))
+);
---
+++ src-built-in/components/toolbarRefactored/app.tsx
@@ -0,0 +1,23 @@
+/*!
+* Copyright 2017 by ChartIQ, Inc.
+* All rights reserved.
+*/
+import * as React from "react";
+import * as ReactDOM from "react-dom";
+import { Provider } from 'react-redux';
+
+import "../../../assets/css/font-finance.css";
+import "../../../assets/css/finsemble.css";
+import store from '../UIAPI/store';
+
+const Toolbar: React.FunctionComponent = (props) => {
+	return (
+		<Provider store={store}>
+			<div className="finsemble-toolbar">
+				{props.children}
+			</div>
+		</Provider>
+	);
+}
+
+export default Toolbar;
