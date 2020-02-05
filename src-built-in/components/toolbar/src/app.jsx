4d7f967519a3d77e6851881dfaf704bc8eb1f20a
Brad Carter <brad@chartiq.com>
2020-02-05 16:31:08 -0500

    Consume changes, add comment

--- src-built-in/components/toolbar/src/app.jsx
+++ src-built-in/components/toolbar/src/app.jsx
@@ -5,6 +5,7 @@
 
 // Static vs Dynamic Toolbar
 // import Toolbar from "./staticToolbar";
+// Uncomment below if you want to show the dynamic toolbar on top of the refactored toolbar.
 // import Toolbar from "./dynamicToolbar";
 import ToolbarRefactored from "../../toolbarRefactored/app.tsx";
 import onReady from "../../UIAPI/effects/index";
