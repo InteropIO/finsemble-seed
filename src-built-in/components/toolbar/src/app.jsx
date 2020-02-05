fa1384b447a1cfc4c413dae70850ec1c11ce6932
Brad Carter <brad@chartiq.com>
2020-02-05 15:16:13 -0500

    Add shell for toolbar refactored.

--- src-built-in/components/toolbar/src/app.jsx
+++ src-built-in/components/toolbar/src/app.jsx
@@ -4,5 +4,11 @@
 */
 
 // Static vs Dynamic Toolbar
-import Toolbar from "./dynamicToolbar";
 // import Toolbar from "./staticToolbar";
+import Toolbar from "./dynamicToolbar";
+import ToolbarRefactored from "../../toolbarRefactored/app.tsx";
+import onReady from "../../UIAPI/effects/index";
+import ReactDOM from "react-dom";
+import React from "react";
+
+onReady(() => ReactDOM.render(<ToolbarRefactored />, document.getElementById("toolbar_refactored")));
