3faeb4f226fb0ef285dcb3af9ce0da1489e8e40b
Brad Carter <brad@chartiq.com>
2020-02-05 15:16:13 -0500

    Add shell for toolbar refactored.

---
+++ src-built-in/components/toolbarRefactored/app.tsx
@@ -0,0 +1,35 @@
+/*!
+* Copyright 2017 by ChartIQ, Inc.
+* All rights reserved.
+*/
+import * as React from "react";
+import * as ReactDOM from "react-dom";
+import { Provider } from 'react-redux';
+
+import "../../../assets/css/font-finance.css";
+import "../../../assets/css/finsemble.css";
+import store from '../UIAPI/store';
+
+const ToolbarRefactored = () => {
+	return (<div style={{
+		backgroundColor: 'red'
+	}} className="finsemble-toolbar">
+		{/* Drag Handle */}
+		{/* Workspace Management Menu */}
+		{/* App Menu */}
+		{/* Favorites Section */}
+		{/* Minimize All */}
+		{/* Reveal All */}
+		{/* Auto Arrange */}
+	</div>)
+}
+
+const Toolbar = () => {
+	return (
+		<Provider store={store}>
+			<ToolbarRefactored />
+		</Provider>
+	);
+}
+
+export default Toolbar;
