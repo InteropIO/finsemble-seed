0f407713777a7db691426e430c74fd7f1baea06d
Ian German Mesner <mesner@gmail.com>
2020-02-06 19:33:45 -0500

    resize window in group

--- src-built-in/components/welcome/welcome.js
+++ src-built-in/components/welcome/welcome.js
@@ -18,3 +18,47 @@ function init() {
 		});
 	}
 }
+
+window.resizeWindowInGroup = async function(height) {
+	// Determine if this window is in a group
+	const windowGroups = FSBL.Clients.WindowClient.getWindowGroups();
+	let boundsResponse = await finsembleWindow.getBounds();
+	if (!(boundsResponse && boundsResponse.data)) {
+		console.error(`Error fetching bounds ${boundsResponse.err}`);
+		return;
+	}
+	finsembleWindow.bounds = boundsResponse.data;
+	const deltaHeight = height - finsembleWindow.bounds.height;
+	if(windowGroups.length > 0){
+		// Fetch all wraps of windows in this group
+		for(const windowName of windowGroups[0].windowNames) {
+			const windowWrap = await FSBL.FinsembleWindow.getInstance({name:windowName})
+			if(!(windowWrap && windowWrap.wrap)) {
+				console.error(`Error fetching wrap ${windowName}`);
+				continue;
+			}
+			boundsResponse = await windowWrap.wrap.getBounds();
+			if (!(boundsResponse && boundsResponse.data)) {
+				console.error(`Error fetching bounds ${boundsResponse.err}`);
+				return;
+			}
+			windowWrap.wrap.bounds = boundsResponse.data;
+			// Move any window adjacent to bottom edge
+			if(windowWrap.wrap.bounds.top == finsembleWindow.bounds.bottom) {
+				windowWrap.wrap.bounds.top += deltaHeight;
+				windowWrap.wrap.setBounds(windowWrap.wrap.bounds);
+				FSBL.Clients.RouterClient.transmit("DockingService.updateWindowLocation", 
+					{ windowName: windowWrap.wrap.name, location: windowWrap.wrap.bounds });
+			}
+		}
+	}
+	// Finally, adjust this window's height
+	finsembleWindow.setBounds({height:height});
+	boundsResponse = await finsembleWindow.getBounds();
+	if (!(boundsResponse && boundsResponse.data)) {
+		console.error(`Error fetching bounds ${boundsResponse.err}`);
+		return;
+	}
+	FSBL.Clients.RouterClient.transmit("DockingService.updateWindowLocation", 
+		{ windowName: finsembleWindow.name, location: boundsResponse.data });
+}
