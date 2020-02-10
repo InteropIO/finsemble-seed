d79063f1a85679d4d74a8b2e0a9f85f0fa8a74ff
Brad Carter <brad@chartiq.com>
2020-02-10 13:33:44 -0500

    Channels now initialize

--- src-built-in/components/UIAPI/reducers/linker.ts
+++ src-built-in/components/UIAPI/reducers/linker.ts
@@ -30,9 +30,10 @@ const reducer = (state = initialState, action: ACTION_TYPES) => {
                 });
             },
             UPDATE_ACTIVE_CHANNELS: ({ channelNames, windowIdentifier }) => {
+                const channelArray = Object.values(channelNames).map(obj => obj.name);
                 draft.windowIdentifier = windowIdentifier;
                 for (const channel of Object.values(draft.channels)) {
-                    channel.active = channelNames.includes(channel.name);
+                    channel.active = channelArray.includes(channel.name);
                 }
             },
             SET_ACCESSIBILITY: ({ isAccessibleLinker }) => {
