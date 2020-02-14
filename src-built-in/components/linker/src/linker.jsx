888fc0a914ae57cb05ee933a9686dd650e12742e
Daniel Hines <d4hines@gmail.com>
2020-02-14 10:01:51 -0500

    updated to use new repo structure

--- src-built-in/components/linker/src/linker.jsx
+++ src-built-in/components/linker/src/linker.jsx
@@ -8,4 +8,9 @@ import ReactDOM from "react-dom";
 import Linker from "../../linkerRefactored/linker";
 import onReady from "../../UIAPI/effects/index";
 
-onReady(() => ReactDOM.render(<Linker />, document.getElementById("main")));
+import { LinkerMenu } from "@chartiq/finsemble-ui/lib/components/linker/linkerMenu";
+import "../../../../assets/css/font-finance.css";
+import "../../../../assets/css/finsemble.css";
+import "../css/linkerWindow.css"
+
+ReactDOM.render(<LinkerMenu />, document.getElementById("main"));
--- src-built-in/components/toolbar/src/app.jsx
+++ src-built-in/components/toolbar/src/app.jsx
@@ -2,33 +2,23 @@
 * Copyright 2017 by ChartIQ, Inc.
 * All rights reserved.
 */
-
-// Static vs Dynamic Toolbar
-// import Toolbar from "./staticToolbar";
-// Uncomment below if you want to show the dynamic toolbar on top of the refactored toolbar.
-// import Toolbar from "./dynamicToolbar";
-import ToolbarRefactored from "../../toolbarRefactored/app.tsx";
-import onReady from "../../UIAPI/effects/index";
 import ReactDOM from "react-dom";
 import React from "react";
-import DragHandle from '../../toolbarRefactored/DragHandle';
+import { ToolbarShell, DragHandle } from "@chartiq/finsemble-ui/lib/components";
 
-const ToolbarShell = () => {
+import '../toolbar.css';
+const Toolbar = () => {
 	return (
-		<ToolbarRefactored>
-			<DragHandle />
-			{/* Drag Handle */}
+		<ToolbarShell>
+			<DragHandle/>
 			{/* Workspace Management Menu */}
 			{/* App Menu */}
 			{/* Favorites Section */}
 			{/* Minimize All */}
 			{/* Reveal All */}
 			{/* Auto Arrange */}
-		</ToolbarRefactored>
+		</ToolbarShell>
 	)
 }
 
-onReady(() => ReactDOM.render(
-	<ToolbarShell />,
-	document.getElementById("toolbar_refactored"))
-);
+ReactDOM.render(<Toolbar />, document.getElementById("toolbar_refactored"));
