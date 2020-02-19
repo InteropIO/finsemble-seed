128ff77e745637b94e23adcab742a5766114a015
amaroks <trythrow@gmail.com>
2020-02-19 20:36:18 +0200

    Add menuhotkey example

--- src-built-in/components/toolbar/src/exampleMenu.jsx
+++ src-built-in/components/toolbar/src/exampleMenu.jsx
@@ -3,11 +3,14 @@ import { MenuShell } from '@chartiq/finsemble-ui/lib/components/menu/menuShell'
 import { MenuActivator } from '@chartiq/finsemble-ui/lib/components/menu/menuActivator'
 import { Menu } from '@chartiq/finsemble-ui/lib/components/menu/menu'
 import { MenuItem } from '@chartiq/finsemble-ui/lib/components/menu/menuItem'
+import { MenuHotKey } from '@chartiq/finsemble-ui/lib/components/menu/menuHotKey'
 
 export const ExampleMenu = () => {
 	const onClick = () => alert('You clicked');
 	return (
 		<MenuShell id="mymenu">
+			<MenuHotKey open={["shift", "down arrow"]}/>
+			<MenuHotKey close={["shift", "up arrow"]}/>
 			<MenuActivator>
 				My menu
 			</MenuActivator>
