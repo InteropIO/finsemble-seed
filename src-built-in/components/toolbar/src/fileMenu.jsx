ced63f600811fd8a3a1f2214e7755a1325f2ecbd
Daniel Hines <d4hines@gmail.com>
2020-02-19 16:08:10 -0500

    update to prepackaged components

--- src-built-in/components/toolbar/src/fileMenu.jsx
+++ src-built-in/components/toolbar/src/fileMenu.jsx
@@ -1,60 +1,22 @@
 import * as React from "react";
-import { MenuShell } from "@chartiq/finsemble-ui/lib/components/menu/menuShell";
-import { MenuActivator } from "@chartiq/finsemble-ui/lib/components/menu/menuActivator";
-import { Menu } from "@chartiq/finsemble-ui/lib/components/menu/menu";
-import { MenuItem } from "@chartiq/finsemble-ui/lib/components/menu/menuItem";
+import { MenuShell, MenuActivator,Menu, Preferences, SystemLog, CentralLogger, Documentation, Restart, Reset, Shutdown } from "@chartiq/finsemble-ui/lib/components";
 
 export const FileMenu = () => {
 	return (
 		<MenuShell id="mymenu">
 			<MenuActivator>File Menu</MenuActivator>
 			<Menu>
-				<MenuItem
-					onClick={() => {
-						FSBL.Clients.LauncherClient.showWindow(
-							{
-								componentType: "UserPreferences"
-							},
-							{
-								monitor: "mine",
-								left: "center",
-								top: "center"
-							}
-						);
-					}}
-				>
-					Preferences
-				</MenuItem>
-				<MenuItem onClick={FSBL.SystemManagerClient.showSystemLog}>
-					System Log
-				</MenuItem>
-				<MenuItem
-					onClick={() => {
-						FSBL.Clients.RouterClient.transmit("CentralConsole-Show", true);
-					}}
-				>
-					Central Logger
-				</MenuItem>
-				<MenuItem
-					onClick={() => {
-						FSBL.System.openUrlWithBrowser(
-							"https://www.chartiq.com/tutorials/?slug=finsemble-seed-project"
-						);
-					}}
-				>
-					Documentation
-				</MenuItem>
-				<MenuItem onClick={FSBL.restartApplication}>Restart</MenuItem>
-				<MenuItem
-					onClick={() => {
-						FSBL.Clients.StorageClient.clearCache(() => {
-							FSBL.restartApplication({ forceRestart: true });
-						});
-					}}
-				>
-					Reset
-				</MenuItem>
-				<MenuItem onClick={FSBL.shutdownApplication}>Shut Down</MenuItem>
+				<Preferences>Preferences</Preferences>
+				<SystemLog>System Log</SystemLog>
+				<CentralLogger>Central Logger</CentralLogger>
+				<Documentation>Documentation</Documentation>
+				<Restart>Restart</Restart>
+				<Reset>Reset</Reset>
+				<Shutdown>Shut Down</Shutdown>
+				{/* To add your own items to the menu, import MenuItem from
+				  * "@chartiq/finsemble-ui/lib/components" and add the following:
+				  * <MenuItem onClick={...}>Your Item</MenuItem>
+				  */}
 			</Menu>
 		</MenuShell>
 	);
