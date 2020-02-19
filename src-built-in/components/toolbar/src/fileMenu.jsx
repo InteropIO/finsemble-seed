c8f1ebcd65ab61045f791441a7689eb044c26ce8
Daniel Hines <d4hines@gmail.com>
2020-02-19 14:50:05 -0500

    extract anonymous functions

--- src-built-in/components/toolbar/src/fileMenu.jsx
+++ src-built-in/components/toolbar/src/fileMenu.jsx
@@ -4,56 +4,47 @@ import { MenuActivator } from "@chartiq/finsemble-ui/lib/components/menu/menuAct
 import { Menu } from "@chartiq/finsemble-ui/lib/components/menu/menu";
 import { MenuItem } from "@chartiq/finsemble-ui/lib/components/menu/menuItem";
 
+function showPreferences() {
+	FSBL.Clients.LauncherClient.showWindow(
+		{
+			componentType: "UserPreferences"
+		},
+		{
+			monitor: "mine",
+			left: "center",
+			top: "center"
+		}
+	);
+}
+
+function showLonger() {
+	FSBL.Clients.RouterClient.transmit("CentralConsole-Show", true);
+}
+function showDOcumentation() {
+	FSBL.System.openUrlWithBrowser(
+		"https://www.chartiq.com/tutorials/?slug=finsemble-seed-project"
+	);
+}
+
+function resetFinsemble() {
+	FSBL.Clients.StorageClient.clearCache(() => {
+		FSBL.restartApplication({ forceRestart: true });
+	});
+}
+
 export const FileMenu = () => {
 	return (
 		<MenuShell id="mymenu">
 			<MenuActivator>File Menu</MenuActivator>
 			<Menu>
-				<MenuItem
-					onClick={() => {
-						FSBL.Clients.LauncherClient.showWindow(
-							{
-								componentType: "UserPreferences"
-							},
-							{
-								monitor: "mine",
-								left: "center",
-								top: "center"
-							}
-						);
-					}}
-				>
-					Preferences
-				</MenuItem>
+				<MenuItem onClick={showPreferences}>Preferences</MenuItem>
 				<MenuItem onClick={FSBL.SystemManagerClient.showSystemLog}>
 					System Log
 				</MenuItem>
-				<MenuItem
-					onClick={() => {
-						FSBL.Clients.RouterClient.transmit("CentralConsole-Show", true);
-					}}
-				>
-					Central Logger
-				</MenuItem>
-				<MenuItem
-					onClick={() => {
-						FSBL.System.openUrlWithBrowser(
-							"https://www.chartiq.com/tutorials/?slug=finsemble-seed-project"
-						);
-					}}
-				>
-					Documentation
-				</MenuItem>
+				<MenuItem onClick={showLonger}>Central Logger</MenuItem>
+				<MenuItem onClick={showDOcumentation}>Documentation</MenuItem>
 				<MenuItem onClick={FSBL.restartApplication}>Restart</MenuItem>
-				<MenuItem
-					onClick={() => {
-						FSBL.Clients.StorageClient.clearCache(() => {
-							FSBL.restartApplication({ forceRestart: true });
-						});
-					}}
-				>
-					Reset
-				</MenuItem>
+				<MenuItem onClick={resetFinsemble}>Reset</MenuItem>
 				<MenuItem onClick={FSBL.shutdownApplication}>Shut Down</MenuItem>
 			</Menu>
 		</MenuShell>
