998483ae330759c554f125b26a92a332faa9b1ca
Brad Carter <brad@chartiq.com>
2020-02-24 12:59:57 -0500

    Import search

--- src-built-in/components/toolbar/src/app.jsx
+++ src-built-in/components/toolbar/src/app.jsx
@@ -8,14 +8,14 @@ import { ToolbarShell, DragHandle } from "@chartiq/finsemble-ui/lib/components";
 
 import '../toolbar.css';
 import { ExampleMenu, ExampleMenu2 } from "./exampleMenu";
-
+import { Search } from '@chartiq/finsemble-ui/lib/components/toolbar/search'
 const Toolbar = () => {
 	return (
 		<ToolbarShell>
 			<DragHandle/>
 			<ExampleMenu />
 			<ExampleMenu2 />
-
+			<Search/>
 			{/* Drag Handle */}
 			{/* Workspace Management Menu */}
 			{/* App Menu */}
