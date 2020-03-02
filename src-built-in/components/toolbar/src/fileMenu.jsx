bef745a7d51114ce0b92b7b93c95554575a7c671
Daniel Hines <d4hines@gmail.com>
2020-03-02 15:27:47 -0500

    Remove props.children

--- src-built-in/components/toolbar/src/fileMenu.jsx
+++ src-built-in/components/toolbar/src/fileMenu.jsx
@@ -8,13 +8,13 @@ export const FileMenu = () => {
 				<img className="finsemble-toolbar-brand-logo" src="../../../assets/img/Finsemble_Taskbar_Icon.png" />
 			</MenuActivator>
 			<Menu>
-				<Preferences>Preferences</Preferences>
-				<SystemLog>System Log</SystemLog>
-				<CentralLogger>Central Logger</CentralLogger>
-				<Documentation>Documentation</Documentation>
-				<Restart>Restart</Restart>
-				<Reset>Reset</Reset>
-				<Quit>Quit</Quit>
+				<Preferences/>
+				<SystemLog/>
+				<CentralLogger/>
+				<Documentation/>
+				<Restart/>
+				<Reset/>
+				<Quit/>
 				{/* To add your own items to the menu, import MenuItem from
 				  * "@chartiq/finsemble-ui/lib/components" and add the following:
 				  * <MenuItem onClick={...}>Your Item</MenuItem>
