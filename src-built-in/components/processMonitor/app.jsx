81ea3c5968f69b930fbcd534f286ee5aba082fff
zona <zona@chartiq.com>
2020-03-03 09:35:15 -0500

    move process monitor code to finsemble UI

---
+++ src-built-in/components/processMonitor/app.jsx
@@ -0,0 +1,10 @@
+/*!
+* Copyright 2017 by ChartIQ, Inc.
+* All rights reserved.
+*/
+import React from "react";
+import ReactDOM from "react-dom";
+
+import ProcessMonitor from "@chartiq/finsemble-ui/lib/components/processMonitor/index";
+
+ReactDOM.render(<ProcessMonitor />, document.getElementById("ProcessMonitor-component-wrapper"));
