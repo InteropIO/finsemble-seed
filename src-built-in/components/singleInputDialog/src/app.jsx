7a70889c6936d021948f7e5f9bf376e89d7cf6e4
Daniel Hines <d4hines@gmail.com>
2020-03-05 14:29:24 -0500

    use wrapped component

--- src-built-in/components/singleInputDialog/src/app.jsx
+++ src-built-in/components/singleInputDialog/src/app.jsx
@@ -8,13 +8,6 @@ import "../../../../assets/css/finsemble.css";
 
 import { SingleInputDialog } from "@chartiq/finsemble-ui/lib/components/singleInputDialog/singleInputDialog";
 
-//render component when FSBL is ready.
-
-function FSBLReady() {
-	ReactDOM.render(
-		<SingleInputDialog />
-		, document.getElementById("singleInputDialog-component-wrapper"));
-}
-
-if (window.FSBL && FSBL.addEventListener) { FSBL.addEventListener("onReady", FSBLReady); } else { window.addEventListener("FSBLReady", FSBLReady) }
-
+ReactDOM.render(
+		<SingleInputDialog />,
+		document.getElementById("singleInputDialog-component-wrapper"));
