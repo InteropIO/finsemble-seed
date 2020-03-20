ed252eabc2b04f73093ff4cba66a261ecd904d9f
zona <zona@chartiq.com>
2020-03-20 07:48:22 -0400

    delete unused assets, change advancedAppCatalog folder to app catalog, update webpack build

--- src-built-in/components/appCatalog/app.jsx
+++ src-built-in/components/appCatalog/app.jsx
@@ -5,7 +5,7 @@
 import React from "react";
 import ReactDOM from "react-dom";
 //style
-import "../appCatalog.css";
+import "./appCatalog.css";
 import { AppCatalog } from "@chartiq/finsemble-ui/lib/components";
 
 ReactDOM.render(
