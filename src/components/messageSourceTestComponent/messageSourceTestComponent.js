26642a62216137a18de1daf2ee52679a2daf55a5
Kris West <kris@chartiq.com>
2020-03-20 20:39:28 +0000

    improving tool layout

--- src/components/messageSourceTestComponent/messageSourceTestComponent.js
+++ src/components/messageSourceTestComponent/messageSourceTestComponent.js
@@ -1,6 +1,28 @@
+
+let elements = {};
+
 const FSBLReady = () => {
 	try {
-		// Do things with FSBL in here.
+		
+		elements.ltHeader = document.getElementById("listenTransmitHeading");
+		elements.ltColumn = document.getElementById("listenTransmitCol");
+		elements.qrHeader = document.getElementById("queryResponseHeading");
+		elements.qrColumn = document.getElementById("queryResponseCol");
+
+		elements.headers = [elements.ltHeader, elements.qrHeader];
+		elements.columns = [elements.ltColumn, elements.qrColumn];
+
+		//Listen tranmit elements
+		elements.listenersList = document.getElementById("listeners");
+		elements.receivedMessage = document.getElementById("receivedMessage");
+		elements.messageEnvelope = document.getElementById("messageEnvelope");
+		elements.senderDetails = document.getElementById("senderDetails");
+		elements.listenChannel = document.getElementById("channel");
+
+		elements.transmitChannel = document.getElementById("transmitChannel");
+		elements.transmitContent = document.getElementById("transmitContent");
+
+		window.elements = elements;
 	} catch (e) {
 		FSBL.Clients.Logger.error(e);
 	}
@@ -9,16 +31,12 @@ const FSBLReady = () => {
 let channels = {};
 
 const listenHandlerFn = (err, response) => {
-	let receivedMessage = document.getElementById("receivedMessage");
-	let messageEnvelope = document.getElementById("messageEnvelope");
-	let senderDetails = document.getElementById("senderDetails");
-	
 	if (err) {
-		receivedMessage.value = "Error: " + JSON.stringify(err, null, 4);
+		elements.receivedMessage.value = "Error: " + JSON.stringify(err, null, 4);
 	} else {
-		receivedMessage.value = JSON.stringify(response.data, null, 4);
-		messageEnvelope.value = JSON.stringify(response.header, null, 4);
-		senderDetails.value = 
+		elements.receivedMessage.value = JSON.stringify(response.data, null, 4);
+		elements.messageEnvelope.value = JSON.stringify(response.header, null, 4);
+		elements.senderDetails.value = 
 `origin:          ${response.header.origin}
 originated here: ${response.header.origin == FSBL.Clients.RouterClient.getClientName()}
 last client:     ${response.header.lastClient}
@@ -27,12 +45,11 @@ last client:     ${response.header.lastClient}
 }
 
 window.listen = () => {
-	let channel = document.getElementById("channel").value;
-	
+	let channel = elements.listenChannel.value;
 	if (!channels[channel]) {
 		FSBL.Clients.RouterClient.addListener(channel, listenHandlerFn);
 		channels[channel] = listenHandlerFn;
-		let listenersList = document.getElementById("listeners");
+		
 		let li = document.createElement("li");
 		li.id = "li_" + channel;
 		li.appendChild(document.createTextNode(channel));
@@ -47,7 +64,7 @@ window.listen = () => {
 
 		li.appendChild(removeButton);
 
-		listenersList.appendChild(li);
+		elements.listenersList.appendChild(li);
 
 	} else {
 		console.warn(`Already listening to channel '${channel}', ignoring...`);
@@ -57,20 +74,42 @@ window.listen = () => {
 window.removeListener = (channel) => {
 	if (channels[channel]) { 
 		FSBL.Clients.RouterClient.removeListener(channel, channels[channel]);
-		let listenersList = document.getElementById("listeners");
+		
 		let element = document.getElementById("li_" + channel);
 		if (element) {
-			listenersList.removeChild(element);
+			elements.listenersList.removeChild(element);
 		}
 		delete channels[channel];
 	}
 }
 
 window.transmit = () => {
-	let channel = document.getElementById("transmitChannel").value;
-	let messageContent = document.getElementById("transmitContent").value;
-	FSBL.Clients.RouterClient.transmit(channel, messageContent);
-	console.log(`Transmitted to channel: ${channel}\nMessage: ${messageContent}`);
+	FSBL.Clients.RouterClient.transmit(transmitChannel.value, transmitContent.value);
+	console.log(`Transmitted to channel: ${transmitChannel.value}\nMessage: ${transmitContent.value}`);
+}
+
+const displayType = (heading, column) => {
+	elements.headers.forEach(aHeader => {
+		if (aHeader != heading) {
+			aHeader.className = "heading";
+		}
+	});
+	heading.className = "heading active";
+	
+	elements.columns.forEach(aColumn => {
+		if (aColumn != column) {
+			aColumn.className = "column hidden";
+		}
+	});
+	column.className = "column";
+}
+
+window.displayListenTransmit = () => {
+	displayType(elements.ltHeader,elements.ltColumn);
+}
+
+window.displayQueryResponse = () => {
+	displayType(elements.qrHeader,elements.qrColumn);
 }
 
 if (window.FSBL && FSBL.addEventListener) {
