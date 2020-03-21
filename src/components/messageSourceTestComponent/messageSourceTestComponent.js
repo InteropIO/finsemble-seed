a02f58a223499e958d243efe139d13045c4675bd
Kris West <kris@chartiq.com>
2020-03-21 02:39:36 +0000

    retrieving details of sender

--- src/components/messageSourceTestComponent/messageSourceTestComponent.js
+++ src/components/messageSourceTestComponent/messageSourceTestComponent.js
@@ -30,17 +30,70 @@ const FSBLReady = () => {
 
 let channels = {};
 
-const listenHandlerFn = (err, response) => {
+const  listenHandlerFn = async (err, response) => {
 	if (err) {
 		elements.receivedMessage.value = "Error: " + JSON.stringify(err, null, 4);
 	} else {
 		elements.receivedMessage.value = JSON.stringify(response.data, null, 4);
 		elements.messageEnvelope.value = JSON.stringify(response.header, null, 4);
+		let myClientName = FSBL.Clients.RouterClient.getClientName();
+		let origin = response.header.origin;
+		let windowName = clientNameToWindowName(origin);
+		let windowDescriptor = await windowNameToDescriptor(windowName);
+		let componentType = null, componentConfig = null, configuredUrlOrPath = null, currentUrl = null;
+		if (windowDescriptor) {
+			componentType = windowDescriptor.componentType;
+			currentUrl = windowDescriptor.url;
+			componentConfig = componentType ? await componentTypeToConfiguration(componentType) : null;
+			if (componentConfig) {
+				configuredUrlOrPath =  componentConfig.window.url ? componentConfig.window.url : componentConfig.window.path;
+			} else { 
+				configuredUrlOrPath = null; 
+			}
+		}
+
 		elements.senderDetails.value = 
-`origin:          ${response.header.origin}
-originated here: ${response.header.origin == FSBL.Clients.RouterClient.getClientName()}
-last client:     ${response.header.lastClient}
+`last client:         ${response.header.lastClient}
+origin:              ${response.header.origin == myClientName ? "here" : origin }
+windowName:          ${windowName}
+componentType:       ${componentType}
+configuredUrlOrPath: ${configuredUrlOrPath}
+currentUrl:          ${currentUrl}
 `
+	}
+}
+
+const clientNameToWindowName = (clientName) => {
+	let index = clientName.indexOf(".");
+	if (index > -1) {
+		return clientName.substring(index+1);
+	} else {
+		return clientName;
+	}
+}
+
+const  windowNameToDescriptor = async (windowName) => {
+	let resp = await FSBL.Clients.LauncherClient.getActiveDescriptors();
+	if (resp.err) {
+		console.error("Failed to retrieve active descriptors! Error: ", resp.err);
+		return null;
+	} else { 
+		if (resp.data[windowName]){
+			return resp.data[windowName];
+		} else {
+			console.error(`No active descriptor for windowName '${windowName}'`, resp.err);
+			return null;
+		}
+	}
+}
+
+const componentTypeToConfiguration = async (componentType) => {
+	let resp = await FSBL.Clients.LauncherClient.getComponentDefaultConfig(componentType);
+	if (resp.err) {
+		console.error(`Failed to retrieve config for component type '${componentType}'! Error: `, resp.err);
+		return null;
+	} else { 
+		return resp.data;
 	}
 }
 
