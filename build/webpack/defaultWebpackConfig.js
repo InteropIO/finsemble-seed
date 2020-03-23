7cdb5dd952d2d062db0c05686566f599cfa90976
Brad Carter <brad@chartiq.com>
2020-03-23 10:10:21 -0400

    fix: #id 23307 Fix build issues. Fix paths to ui-components. (#631)
    Moves paths from lib to react.
    Fixes build issues in some components.

--- build/webpack/defaultWebpackConfig.js
+++ build/webpack/defaultWebpackConfig.js
@@ -107,11 +107,11 @@ module.exports = class WebpackDefaults {
 						loader: 'ts-loader',
 						exclude: /node_modules/
 					},
-					  // All output '.js' files will have any sourcemaps re-processed by 'source-map-loader'.
-            {
-							enforce: "pre",
-							test: /\.js$/,
-							loader: "source-map-loader"
+					// All output '.js' files will have any sourcemaps re-processed by 'source-map-loader'.
+					{
+						enforce: "pre",
+						test: /\.js$/,
+						loader: "source-map-loader"
 					}
 				]
 			},
