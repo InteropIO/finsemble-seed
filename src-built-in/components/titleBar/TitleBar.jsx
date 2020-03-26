ee0034ff3e5de43eacc19f8e97be96b97f2883ea
Daniel Hines <d4hines@gmail.com>
2020-03-26 11:26:34 -0400

    add explanatory comment

--- src-built-in/components/titleBar/TitleBar.jsx
+++ src-built-in/components/titleBar/TitleBar.jsx
@@ -2,12 +2,23 @@
 * Copyright 2017 - 2020 by ChartIQ, Inc.
 * All rights reserved.
 */
-import React from "./node_modules/react";
-import { useTitleBar } from "./node_modules/@chartiq/finsemble-ui/react/hooks";
-import { AlwaysOnTopButton, DockingButton, LinkerButton, ShareButton, initializeTitleBar, TabRegion, CloseButton, MaximizeButton, MinimizeButton,  } from "./node_modules/@chartiq/finsemble-ui/react/components";
+import React from "react";
+import { useTitleBar } from "@chartiq/finsemble-ui/react/hooks";
+import { AlwaysOnTopButton, DockingButton, LinkerButton, ShareButton, initializeTitleBar, TabRegion, CloseButton, MaximizeButton, MinimizeButton,  } from "@chartiq/finsemble-ui/react/components";
 import "../../../assets/css/finsemble.css";
 import "../../../assets/css/_windowTitleBar.css";
 
+/**
+ * This is the Title Bar component, which is rendered at
+ * the top of every Finsemble-controlled HTML window.
+ * You can customize this template by adding or removing
+ * elements and sytling as you see fit.
+ * 
+ * The visibility of each of the controls is controlled by
+ * config. For example, setting the
+ * "foreign.components.Window Manager.showLinker" property
+ * to false will hide the <LinkerButton>.
+ */
 const TitleBar = () => {
 	const {
 		showLinkerButton,
