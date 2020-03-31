7e550d8f6a2fb890153f6204db92336815470f0e
zona <zona@chartiq.com>
2020-03-31 16:02:36 -0400

    make userPreference a template

--- src/components/userPreferences/UserPreferences.jsx
+++ src/components/userPreferences/UserPreferences.jsx
@@ -2,11 +2,42 @@
 * Copyright 2017 - 2020 by ChartIQ, Inc.
 * All rights reserved.
 */
-import React from "react";
+import React, { useState } from "react";
 import ReactDOM from "react-dom"
 import "../../../assets/css/font-finance.css";
 import "../../../assets/css/finsemble.css";
 import "./userPreferences.css";
-import { UserPreferences } from "@chartiq/finsemble-ui/react/components";
+import { UserPreferencesShell, LeftNav, ContentSection } from "@chartiq/finsemble-ui/react/components";
+import { Sample } from "./sample";
+
+
+const entries = [
+    {
+        name: "general",
+        label: "General"
+    },
+    {
+        name: "workspaces",
+        label: "Workspaces"
+    },
+    {
+        name: "sample",
+        label: "Sample"
+    }
+];
+
+const customSections = {
+    sample: Sample
+};
+
+const UserPreferences = () => {
+    const [activeSection, setActiveSection] = useState("general");
+    return (
+        <UserPreferencesShell>
+            <LeftNav entries={entries} setActiveEntry={setActiveSection} />
+            <ContentSection activeSection={activeSection} customSections={customSections} />
+        </UserPreferencesShell>
+    )
+}
 
 ReactDOM.render(<UserPreferences />, document.getElementById("UserPreferences-component-wrapper"));
---
+++ src/components/userPreferences/sample.jsx
@@ -0,0 +1,5 @@
+import React from "react";
+
+export const Sample = () => (
+	<p>hi</p>
+)
