9e6d10f317b6ece94f4564a0b738523f1cc72311
zona <zona@chartiq.com>
2020-04-01 14:06:21 -0400

    cleanup and comments

--- src/components/userPreferences/UserPreferences.jsx
+++ src/components/userPreferences/UserPreferences.jsx
@@ -8,30 +8,25 @@ import "../../../assets/css/font-finance.css";
 import "../../../assets/css/finsemble.css";
 import "./userPreferences.css";
 import { UserPreferencesShell, LeftNav, ContentSection } from "@chartiq/finsemble-ui/react/components";
-import { Sample } from "./sample";
 
+// The preference menu entries. It is passed as a prop to the LeftNav component to generate the preference menu section names.
+const entries = ["General", "Workspaces", "My section"];
 
-const entries = [
-    {
-        name: "general",
-        label: "General"
-    },
-    {
-        name: "workspaces",
-        label: "Workspaces"
-    },
-    {
-        name: "sample",
-        label: "Sample"
-    }
-];
-
-const customSections = {
-    sample: Sample
-};
+/*
+ * Put your customized preference menu components here and pass this object as a prop to the ContentSection component.
+ * The key should be the customized component's entry name and the value should be the customized React component.
+ * For example, if you have a customized menu component called "Sample" with entry name "My Section", after you import the component to this file, i.e. import { Sample } from "./sample";
+ * include this component in this object like so:
+ * 
+ * const customSections = {
+ *  "My section": Sample
+ * };
+*/
+const customSections = {};
 
 const UserPreferences = () => {
-    const [activeSection, setActiveSection] = useState("general");
+    // The active section is the section that is currently selected and displayed in the preference menu.
+    const [activeSection, setActiveSection] = useState("General");
     return (
         <UserPreferencesShell>
             <LeftNav entries={entries} setActiveEntry={setActiveSection} />
--- src/components/userPreferences/sample.jsx
+++
@@ -1,5 +0,0 @@
-import React from "react";
-
-export const Sample = () => (
-	<p>hi</p>
-)
