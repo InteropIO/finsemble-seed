91bb9fc711f4645fe70a68af57110421acb5c670
Daniel Hines <d4hines@gmail.com>
2020-04-01 15:38:05 -0400

    move titlebar to src

--- src/components/welcome/welcome.js
+++ src/components/welcome/welcome.js
@@ -17,4 +17,16 @@ function init() {
 			//console.log("failed to launch docs");
 		});
 	}
+	FSBL.Clients.DragAndDropClient.setEmitters({
+		emitters: [
+			{
+				type: "symbol",
+				data: {}
+			},
+			{
+				type: "chartiq.chart",
+				data: {}
+			}
+		]
+	});
 }
