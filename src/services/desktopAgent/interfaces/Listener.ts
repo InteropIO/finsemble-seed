864376993c5a4fa5f1e5cfcadaa5342b80c5fcf2
WatsonCIQ <chris.watson@chartiq.com>
2020-05-06 16:45:33 +0100

    add new schemas and tpes from FDC3 1.1

--- src/services/desktopAgent/interfaces/Listener.ts
+++ src/services/desktopAgent/interfaces/Listener.ts
@@ -1,3 +1,11 @@
+/**
+ * SPDX-License-Identifier: Apache-2.0
+ * Copyright 2019 FINOS FDC3 contributors - see NOTICE file
+ */
+
 interface Listener {
-    unsubscribe();
-  }
+    /**
+     * Unsubscribe the listener object.
+     */
+    unsubscribe(): void;
+}
