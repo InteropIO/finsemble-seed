e267f3d1699503bcc9b4bc4745f7b69d8e8dbaf2
WatsonCIQ <chris.watson@chartiq.com>
2020-05-08 12:39:01 +0100

    added constructor to the Channel Class

--- src/services/desktopAgent/channel.ts
+++ src/services/desktopAgent/channel.ts
@@ -1,18 +1,25 @@
-
 export default class C implements Channel {
-    id: string;
-    type: string;
-    displayMetadata?: DisplayMetadata;
-    broadcast(context: object): void {
-        throw new Error("Method not implemented.");
-    }
-    getCurrentContext(contextType?: string): Promise<object> {
-        throw new Error("Method not implemented.");
-    }
-    //addContextListener(handler: ContextHandler): Listener;
-    //addContextListener(contextType: string, handler: ContextHandler): Listener;
-    addContextListener(contextType: any, handler?: any): Listener {
-        throw new Error("Method not implemented.");
-    }
+	id: string;
+	type: string;
+	displayMetadata?: DisplayMetadata;
 
+	constructor(id: string, type: string, displayMetadata: DisplayMetadata) {
+		this.id = id;
+		this.type = type;
+		this.displayMetadata = displayMetadata;
+	}
+
+	broadcast(context: object): void {
+		//FSBL.Clients.LinkerClient.Publish(context)
+		// They may not have joined this channel yet?
+		throw new Error("Method not implemented.");
+	}
+	getCurrentContext(contextType?: string): Promise<Context | null> {
+		throw new Error("Method not implemented.");
+	}
+	//addContextListener(handler: ContextHandler): Listener;
+	//addContextListener(contextType: string, handler: ContextHandler): Listener;
+	addContextListener(contextType: any, handler?: any): Listener {
+		throw new Error("Method not implemented.");
+	}
 }
