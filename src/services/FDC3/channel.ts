6e1025e58ef8e22fa00ef658f1973bdd85a9b86d
WatsonCIQ <chris.watson@chartiq.com>
2020-05-11 11:03:45 +0100

    Formatting and added Param types for constructor.

--- src/services/FDC3/channel.ts
+++ src/services/FDC3/channel.ts
@@ -1,12 +1,19 @@
+interface Params {
+	id: string;
+	type: string;
+	displayMetadata: {};
+	FSBL: any;
+}
+
 export default class C implements Channel {
 	id: string;
 	type: string;
 	displayMetadata?: DisplayMetadata;
-	private contexts: {[contextType: string]: Context} = {};
+	private contexts: { [contextType: string]: Context } = {};
 	private currentContext: Context;
 	private FSBL: any;
 
-	constructor(params: any) {
+	constructor(params: Params) {
 		this.id = params.id;
 		this.type = params.type;
 		this.displayMetadata = params.displayMetadata;
@@ -16,11 +23,14 @@ export default class C implements Channel {
 	broadcast(context: Context): void {
 		this.currentContext = context;
 		this.contexts[(context as any).type] = context;
-		this.FSBL.Clients.LinkerClient.publish({
-			dataType: `FDC3.broadcast.${(context as any).type}`, 
-			data: context,
-			channels: [this.id]
-		}, ()=> {});
+		this.FSBL.Clients.LinkerClient.publish(
+			{
+				dataType: `FDC3.broadcast.${(context as any).type}`,
+				data: context,
+				channels: [this.id],
+			},
+			() => {}
+		);
 	}
 
 	async getCurrentContext(contextType?: string): Promise<Context | null> {
@@ -30,6 +40,8 @@ export default class C implements Channel {
 	//addContextListener(handler: ContextHandler): Listener;
 	//addContextListener(contextType: string, handler: ContextHandler): Listener;
 	addContextListener(contextType: any, handler?: any): Listener {
-		throw new Error("Method not implemented in service. You must use the client for this.");
+		throw new Error(
+			"Method not implemented in service. You must use the client for this."
+		);
 	}
 }
