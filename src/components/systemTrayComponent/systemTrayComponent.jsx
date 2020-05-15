795b242a6bc5ee9a16dfc8b29ffad46b99132837
Brad Carter <carterbs89@gmail.com>
2020-05-15 05:35:45 -0400

    Use shell

--- src/components/systemTrayComponent/systemTrayComponent.jsx
+++ src/components/systemTrayComponent/systemTrayComponent.jsx
@@ -6,16 +6,16 @@ import React from "react";
 import ReactDOM from "react-dom"
 import "@chartiq/finsemble-ui/react/assets/css/finsemble.css";
 import "../../../assets/css/_themeWhitelabel.css";
-import { SystemTrayComponentWrapper, Preferences, SystemLog, CentralLogger, Documentation, Restart, Reset, Quit } from "@chartiq/finsemble-ui/react/components";
+import { SystemTrayComponentShell, Preferences, SystemLog, CentralLogger, Documentation, Restart, Reset, Quit } from "@chartiq/finsemble-ui/react/components";
 
 /**
  * This component will be rendered whenever the user right clicks on your application's system tray icon.
- * Feel free to add/remove anything in here. The SystemTrayComponentWrapper handles setting the height/width
+ * Feel free to add/remove anything in here. The SystemTrayComponentShell handles setting the height/width
  * of the window. It will also hide the window when it loses focus.
  */
 const SystemTrayComponent = () => {
     return (
-        <SystemTrayComponentWrapper padding={{width: 80}}>
+        <SystemTrayComponentShell padding={{ width: 80 }}>
             <div className="menu menu-primary">
                 <Preferences />
                 <SystemLog />
@@ -25,8 +25,8 @@ const SystemTrayComponent = () => {
                 <Reset />
                 <Quit />
             </div>
-        </SystemTrayComponentWrapper>
-        
+        </SystemTrayComponentShell>
+
     )
 }
 
