f47d5c45ae007dec79054c8987cacb3827d65a7a
jclaus <joseph@chartiq.com>
2020-05-27 17:56:49 -0400

    Remove store from helper

--- src-built-in/components/advancedAppCatalog/src/utils/helpers.js
+++ src-built-in/components/advancedAppCatalog/src/utils/helpers.js
@@ -1,5 +1,4 @@
 import { findIndex } from 'lodash';
-import { getStore } from '../stores/appStore';
 
 export const findAppIndexInFolder = (appID, folderName, folders) => {
     return findIndex(folders[folderName].apps, app => app.appID === appID);
