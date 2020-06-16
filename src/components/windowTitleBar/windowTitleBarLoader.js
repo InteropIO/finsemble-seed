3e3d2b7d035af77d19687b10901e699203d65a2c
meredith-ciq <46169395+meredith-ciq@users.noreply.github.com>
2020-06-16 15:42:17 -0400

    24473 Spawn windows with browserview (#7)
    https://chartiq.kanbanize.com/ctrl_board/106/cards/24473/details/
    
    Pulls in and modifies code from experimental/browserview branches to get browserview running on planned/5.0.

--- src/components/windowTitleBar/windowTitleBarLoader.js
+++ src/components/windowTitleBar/windowTitleBarLoader.js
@@ -4,9 +4,9 @@
  */
 
 function init() {
-	if (window.headerLoaded) return;
-	window.headerLoaded = true;
-	require("./WindowTitleBar.jsx");
+	// Temporary hack for browserview parent and contents both triggering FSBL ready. This can cause titlebar listeners to not get setup
+	// Will be removed with the work for only parent windows to send the spawn complete callback.
+	setTimeout(() => require("./WindowTitleBar.jsx"), 100);
 }
 
 if (window.FSBL && FSBL.addEventListener) {
