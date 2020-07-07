f03855aaa78ef6e716d8d981f706242538a37e5e
Terry Thorsen <terry@chartiq.com>
2020-07-07 16:35:38 -0400

    Refactor of seed build files (#78)
    - Clean up and modularize defaultWebpackConfig
    - DRY up all the other files
    - Add plenty of commenting
    - Tried not to go crazy, this is just an interim step towards a more modular approach

--- build/webpack/webpack.titleBar.js
+++ build/webpack/webpack.titleBar.js
@@ -1,112 +1,19 @@
-//The title bar can be injected into any component or page - that component or page may not be aware of finsemble, and may not include the vendor.bundle.js file that's required to make the DLL plugin work. This webpack config is to build that component independent of any code splitting done for the rest of the presentation components.
-const path = require("path");
-const UglifyJsPlugin = require("uglifyjs-webpack-plugin");
-const { DefinePlugin } = require("webpack");
-const HardSourceWebpackPlugin = require("hard-source-webpack-plugin");
-const env = process.env.NODE_ENV ? process.env.NODE_ENV : "development";
-let plugins = [
-	new HardSourceWebpackPlugin({
-		info: {
-			level: "warn",
-		},
-		cacheDirectory: "../.webpack-file-cache/[confighash]",
-	}),
-	new DefinePlugin({
-		"process.env": {
-			NODE_ENV: JSON.stringify(env),
-		},
-	}),
-];
+/**
+ * The webpacking for WindowTitleBar differs from other components because it does not make use of the "vendor bundle".
+ * The vendor bundle allows common libraries such as react, react-dom to be consolidated into a single js file.
+ * Finsemble's built-in UI components make use of this vendor bundle but this cannot be counted on for external
+ * components, particularly QuickComponents.
+ *
+ * Note: this will be unnecessary once WindowTitleBar is separated from app content via a BrowserView.
+ */
+const { generateDefaultConfig } = require("./defaultWebpackConfig");
 
-if (env === "production") {
-	// When building the production environment, minify the code.
-	plugins.push(new UglifyJsPlugin());
-}
-
-const windowTitleBarPath =
-	"./src/components/WindowTitleBar/windowTitleBarLoader.js";
-module.exports = {
-	devtool: env === "production" ? "source-map" : "eval-source-map",
-	entry: {
-		"components/WindowTitleBar/WindowTitleBar": windowTitleBarPath,
-	},
-	stats: "minimal",
-	module: {
-		rules: [
-			{
-				test: /\.s?css$/i,
-				use: [
-					"style-loader",
-					{
-						loader: "css-loader",
-						options: {
-							sourceMap: env !== "production",
-						},
-					},
-				],
-			},
-			{
-				test: /\.(png|img|ttf|ottf|eot|woff|woff2|svg)$/,
-				loader: "url-loader",
-			},
-			{
-				test: /semver\.browser\.js/,
-				use: ["imports?define=>undefined"],
-			},
-			{
-				test: /\.js(x)?$/,
-				exclude: /node_modules/,
-				use: {
-					loader: "babel-loader",
-					options: {
-						cacheDirectory: ".webpack-file-cache",
-						presets: [
-							[
-								"@babel/preset-env",
-								{
-									targets: {
-										browsers: "Chrome 70",
-									},
-									modules: "commonjs",
-								},
-							],
-							"@babel/preset-react",
-						],
-						plugins: [
-							"babel-plugin-add-module-exports",
-							"@babel/plugin-proposal-export-default-from",
-							"@babel/plugin-transform-modules-commonjs",
-							"@babel/plugin-proposal-class-properties",
-							[
-								"@babel/plugin-proposal-decorators",
-								{ decoratorsBeforeExport: false },
-							],
-							["@babel/plugin-transform-runtime", { regenerator: true }],
-						],
-					},
-				},
-			},
-			{
-				test: /\.ts(x)?$/,
-				loader: "ts-loader",
-				exclude: /node_modules/,
-			},
-		],
-	},
-	mode: env,
-	plugins: plugins,
-	output: {
-		filename: "[name].js",
-		sourceMapFilename: "[name].map.js",
-		path: path.resolve(__dirname, "../../dist/"),
-	},
-	resolve: {
-		extensions: [".ts", ".tsx", ".js", ".jsx", ".json", "scss", "html"],
-		alias: {
-			react: path.resolve("./node_modules/react"),
-			"react-dom": path.resolve("./node_modules/react-dom"),
-			"@babel/runtime": path.resolve("./node_modules/@babel/runtime"),
-			async: path.resolve("./node_modules/async"),
-		},
-	},
+const entries = {
+	"components/WindowTitleBar/WindowTitleBar":
+		"./src/components/WindowTitleBar/windowTitleBarLoader.js",
 };
+
+let webpackConfig = generateDefaultConfig();
+webpackConfig.entry = entries;
+
+module.exports = webpackConfig;
