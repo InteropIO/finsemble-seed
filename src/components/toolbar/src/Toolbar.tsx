6ce6d0e1505f4c79dd58a41166cbdf9ee6aeb83d
Kris West <kris@chartiq.com>
2020-10-08 18:58:30 +0100

    Rebuild of the dynamicAuth recipe for 5.0 where dynamically configured menus have to implemented by the client

--- src/components/toolbar/src/Toolbar.tsx
+++ src/components/toolbar/src/Toolbar.tsx
@@ -3,7 +3,7 @@
  * All rights reserved.
  */
 
-import React from "react";
+import React, { useState, useEffect, FunctionComponent } from "react";
 import ReactDOM from "react-dom";
 import { FinsembleProvider } from "@finsemble/finsemble-ui/react/components/FinsembleProvider";
 import {
@@ -24,6 +24,98 @@ import { useHotkey } from "@finsemble/finsemble-ui/react/hooks/useHotkey";
 import "@finsemble/finsemble-ui/react/assets/css/finsemble.css";
 import "../../../../assets/css/theme.css";
 
+/* #region Customisation for dynamically configurable menus */
+type MenuConfig = {
+	label?: string;
+	menuType: string;
+	align: string;
+	customData: {
+		list?: any[];
+		mode?: string;
+	};
+	icon?: string;
+	fontIcon?: string;
+};
+type AppLaunchersProps = {
+	align: string;
+	dynamicMenus: MenuConfig[];
+	components: any;
+};
+const AppLaunchers: FunctionComponent<AppLaunchersProps> = (props) => {
+	const { dynamicMenus, components, align } = props;
+
+	if (dynamicMenus.length === 0) {
+		return (
+			<>
+				<AppLauncherMenu enableQuickComponents={true} />
+			</>
+		);
+	} else {
+		return (
+			<>
+				{dynamicMenus.map((config, i) => {
+					if (align == config.align) {
+						let theTitle: JSX.Element | string = "Apps";
+						if (config.icon) {
+							theTitle = <span>
+								<img src={config.icon} style={{verticalAlign: "middle", marginRight: "5px"}}/>
+								{config.label ? config.label : ""}
+							</span>
+						} else if (config.fontIcon) {
+							theTitle = <span>
+								<i className={config.fontIcon} style={{verticalAlign: "middle", marginRight: "5px"}}/>
+								{config.label ? config.label : ""}
+							</span>
+						} else if (config.label) {
+							theTitle = config.label;
+						}
+						
+						if (config.menuType === "Advanced App Launcher"){
+							return (
+								<AdvancedAppLauncherMenu 
+									title={theTitle}
+									id={`menu-${i}`}
+									enableQuickComponents={true} 
+								/>
+							);
+						} else {
+							if (config.customData.mode) {
+								let allComps = Object.keys(components);
+								let list: string[] = [];
+								allComps.forEach(key => {
+									if (components[key].component && components[key].component.mode == config.customData.mode){
+										list.push(key);
+									}
+								});
+
+								return (
+									<AppLauncherMenu
+										title={theTitle}
+										id={`menu-${i}`}
+										componentFilter={list}
+										enableQuickComponents={false}
+									/>
+								);
+							} else {
+								return (
+									<AppLauncherMenu
+										title={theTitle}
+										id={`menu-${i}`}
+										componentFilter={config.customData.list}
+										enableQuickComponents={false}
+									/>
+								);
+							}
+						}
+					}
+				})}
+			</>
+		);
+	}
+};
+
+/* #endregion */
+
 /**
  * Note: Set `FSBL.debug = true` if you need to reload the toolbar during development.
  * By default, it prevents the system from closing it so that users aren't lost without
@@ -38,6 +130,25 @@ const Toolbar = () => {
 		window.FSBL.Clients.WorkspaceClient.minimizeAll()
 	);
 
+	/* #region retrieve Finsemble config for dynamically configured menus */
+	const [dynamicMenus, setDynamicMenus] = useState([] as any[]);
+	const [components, setComponents] = useState([] as any[]);
+	useEffect(() => {
+		FSBL.Clients.ConfigClient.getValue("finsemble", (err: any, config: any) => {
+			const menuConfig = config.menus;
+			const components = config.components;
+			if (Array.isArray(menuConfig)) {
+				setDynamicMenus(
+					menuConfig.filter((menu) => menu.menuType === "App Launcher" || menu.menuType === "Advanced App Launcher"),
+				);
+				setComponents(
+					components
+				);
+			}
+		});
+	}, []);
+	/* endregion */
+
 	return (
 		<ToolbarShell
 			hotkeyShow={["ctrl", "alt", "t"]}
@@ -48,9 +159,8 @@ const Toolbar = () => {
 				<FileMenu />
 				<Search openHotkey={["ctrl", "alt", "f"]} />
 				<WorkspaceManagementMenu />
-				{/* Uncomment the following to enable the AdvancedAppLauncherMenu*/}
-				{/* <AdvancedAppLauncherMenu enableQuickComponents={true} /> */}
-				<AppLauncherMenu enableQuickComponents={true} />
+				{/* Component that generates dynamically configured menus */}
+				<AppLaunchers dynamicMenus={dynamicMenus} components={components} align="left"/>
 			</ToolbarSection>
 			<ToolbarSection className="center" hideBelowWidth={115}>
 				<div className="divider" />
@@ -58,6 +168,7 @@ const Toolbar = () => {
 			</ToolbarSection>
 			<ToolbarSection className="right">
 				<div className="divider"></div>
+				<AppLaunchers dynamicMenus={dynamicMenus} components={components} align="right"/>
 				<AutoArrange />
 				<MinimizeAll />
 				<RevealAll />
