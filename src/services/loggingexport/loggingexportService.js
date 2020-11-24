db792f00aabde5c9834adff9914cf5d2deded4ad
WatsonCIQ <chris.watson@chartiq.com>
2020-11-24 11:32:12 +0000

    Uodate service name and class

--- src/services/loggingexport/loggingexportService.js
+++ src/services/loggingexport/loggingexportService.js
@@ -17,25 +17,28 @@ Finsemble.Clients.Logger.start();
 Finsemble.Clients.Logger.log("customService Service starting up");
 
 
-// #region Settings
+
+class LoggingExportService extends Finsemble.baseService {
+
+	// #region Settings
 /** Transmit queued messages when their number reaches or exceeds this amount. */
-const BATCH_SIZE = 100;
+	BATCH_SIZE = 100;
 
 /** Wait at most this number of milliseconds after first message in batch is received before transmitting.*/
-const TIMEOUT_MILLISECS = 30 * 1000;
+	TIMEOUT_MILLISECS = 30 * 1000;
 
 /** Wait at most this number of milliseconds for batch transmission to complete */
-const TRANSMIT_TIMEOUT_MILLISECS = 10 * 1000;
+	TRANSMIT_TIMEOUT_MILLISECS = 10 * 1000;
 
 /** Log levels of messsages to capture. Valid values: Error, Warning, Log, Info, Debug, Verbose */
-const CAPTURE_LOG_LEVELS = {
+	CAPTURE_LOG_LEVELS = {
   "Log": true,
   "Warn": true,
   "Error": true
 };
 
 /** Log categories of messages to capture. Valid values: system, dev or perf */
-const CAPTURE_LOG_CATEGORIES = {
+	CAPTURE_LOG_CATEGORIES = {
   "system": true,
   "dev": true
 };
@@ -46,81 +49,21 @@ const CAPTURE_LOG_CATEGORIES = {
  * Note that, due to the async nature of logging, you might still send a later batch containing messages with a lower
  * logTimestamp. Hence, it is best to avoid sorting messages in this service if messages will be sorted elsewhere.
  */
-const SORT_MESSAGES = false;
-
-/**
- * Message formatting function which converts each log message from Finsemble's format to the format you wish to transmit.
- * Incoming log message format:
- *		[
- *			{
- *				"category": "system",				//Log message type: system, dev or perf
- *				"logClientName": "Finsemble",		//The registered name of the logger instance
- *				"logType": "Log",					//Log level: Error, Warning, Log, Info, Debug, Verbose
- *				"logData": "[\"SERVICE LIFECYCLE: STATE CHANGE: Service initializing\",\"windowService\"]",
- *													//JSON encoded array of message and data components of the log message
- *													//N.B. maybe be prefixed by string "*** Logging Error: ""
- *				"logTimestamp": 1544090028391.6226	//Log message timestamp for ordering use
- *			},
- *			{...},
- *			...
- *		]
- */
-const FORMAT_MESSAGE = function (log_message) {
-	//TODO: add any necessary message format changes here
-
-	return log_message;
-};
+	SORT_MESSAGES = false;
 
 /** Where to transmit the logs to. */
 //TODO: Update to your logging endpoint
-const LOGGING_ENDPOINT = "http://somedomain.com/loggingendpoint";
+	LOGGING_ENDPOINT = "http://somedomain.com/loggingendpoint";
 
-// Add and initialize any other clients you need to use (services are initialized by the system, clients are not):
-// Finsemble.Clients.AuthenticationClient.initialize();
-// Finsemble.Clients.ConfigClient.initialize();
-// Finsemble.Clients.DialogManager.initialize();
-// Finsemble.Clients.DistributedStoreClient.initialize();
-// Finsemble.Clients.DragAndDropClient.initialize();
-// Finsemble.Clients.LauncherClient.initialize();
-// Finsemble.Clients.LinkerClient.initialize();
-// Finsemble.Clients.HotkeyClient.initialize();
-// Finsemble.Clients.SearchClient.initialize();
-// Finsemble.Clients.StorageClient.initialize();
-// Finsemble.Clients.WindowClient.initialize();
-// Finsemble.Clients.WorkspaceClient.initialize();
-
-/**
- * Add service description here
- */
-class customService extends Finsemble.baseService {
-	/**
-	 * Initializes a new instance of the customServiceService class.
-	 */
 	constructor() {
 		super({
 			// Declare any client dependencies that must be available before your service starts up.
 			startupDependencies: {
-				// When ever you use a client API with in the service, it should be listed as a client startup
-				// dependency. Any clients listed as a dependency must be initialized at the top of this file for your
-				// service to startup.
-				clients: [
-					// "authenticationClient",
-					// "configClient",
-					// "dialogManager",
-					// "distributedStoreClient",
-					// "dragAndDropClient",
-					// "hotkeyClient",
-					// "launcherClient",
-					// "linkerClient",
-					// "searchClient
-					// "storageClient",
-					// "windowClient",
-					// "workspaceClient",
-				],
-			},
+				clients: [],
+			}
 		});
 
-		this.batchAllocSize = BATCH_SIZE + 10;   //add a small amount of leeway on batch size
+		this.batchAllocSize = this.BATCH_SIZE + 10;   //add a small amount of leeway on batch size
 
 		this.timeout = null;
 		this.logBatch = new Array(this.batchAllocSize); //pre-init the batch array to reduce array allocations
@@ -136,11 +79,34 @@ class customService extends Finsemble.baseService {
    * @param {function} callback
    */
 	readyHandler(callback) {
-		this.createRouterEndpoints();
+		// start listening for logs
+		this.centralLoggerLogConsumer();
 		Finsemble.Clients.Logger.log("customService Service ready");
 		callback();
 	}
+	/**
+	 * Message formatting function which converts each log message from Finsemble's format to the format you wish to transmit.
+	 */
+	formatMessage(logMessage) {
+		/* Example Incoming log message format:
+*		[
+*			{
+*				"category": "system",				//Log message type: system, dev or perf
+*				"logClientName": "Finsemble",		//The registered name of the logger instance
+*				"logType": "Log",					//Log level: Error, Warning, Log, Info, Debug, Verbose
+*				"logData": "[\"SERVICE LIFECYCLE: STATE CHANGE: Service initializing\",\"windowService\"]",
+*													//JSON encoded array of message and data components of the log message
+*													//N.B. maybe be prefixed by string "*** Logging Error: ""
+*				"logTimestamp": 1544090028391.6226	//Log message timestamp for ordering use
+*			},
+*			{...},
+*			...
+*		]
+*/
 
+		//TODO: add any necessary message format changes here
+		return logMessage;
+	}
 
   /**
  * Add an incoming array of log messages to the batch and transmit if thresholds met.
@@ -149,18 +115,22 @@ class customService extends Finsemble.baseService {
 	addToBatch(dataArr) {
     if (dataArr) {
 			for (let i = 0; i < dataArr.length; i++) {
-				if (CAPTURE_LOG_CATEGORIES[dataArr[i].category] && CAPTURE_LOG_LEVELS[dataArr[i].logType]) {
-					this.logBatch[this.currBatchSize++] = FORMAT_MESSAGE(dataArr[i]);
+				const message = dataArr[i];
+
+				// only capture the log messages that meet the category and type we set
+				if (this.CAPTURE_LOG_CATEGORIES[message.category] && this.CAPTURE_LOG_LEVELS[message.logType]) {
+					this.logBatch[this.currBatchSize++] = this.formatMessage(message);
         }
-        else {
-					console.debug("discarding message", dataArr[i]);
+				else {
+					// skip the logs that don't meet the requirements
+					console.debug("discarding message", message);
         }
       }
 
-			if (this.currBatchSize >= BATCH_SIZE) {
+			if (this.currBatchSize >= this.BATCH_SIZE) {
 				this.transmitBatch();
 			} else if (!this.timeout) { //always transmit batch within minimum this.timeout
-				this.timeout = setTimeout(this.transmitBatch, TIMEOUT_MILLISECS);
+				this.timeout = setTimeout(this.transmitBatch, this.TIMEOUT_MILLISECS);
       }
     } else {
       Logger.warn("Tried to add an invalid data array to log batch", dataArr);
@@ -185,16 +155,16 @@ class customService extends Finsemble.baseService {
 		this.currBatchSize = 0;
 
     // Sort the batch by timestamp if necessary.
-    if (SORT_MESSAGES) {
+		if (this.SORT_MESSAGES) {
       toTransmit.sort((a, b) => a.logTimestamp - b.logTimestamp);
     }
 
     //transmit batch
     //TODO: Customize batch transmission here
-    console.debug("Batch to transmit: " + JSON.stringify(toTransmit, null, 2));
+		console.debug("Batch to transmit: " + JSON.stringify(toTransmit, null, 2));
 
-
-		fetch(LOGGING_ENDPOINT, {
+		// the call to your endpoint here
+		fetch(this.LOGGING_ENDPOINT, {
 			method: 'POST', // or 'PUT'
 			headers: {
 				'Content-Type': 'application/json',
@@ -217,8 +187,8 @@ class customService extends Finsemble.baseService {
    * Create a router listener for log messages.
    * @private
    */
-	createRouterEndpoints() {
-		// Add responder for myFunction
+	centralLoggerLogConsumer() {
+		// collect the logs from the central logger and send them to be processed
 		Finsemble.Clients.RouterClient.addListener("logger.service.logMessages", (error, logMessage) => {
       if (!error) {
 				this.addToBatch(logMessage.data);
@@ -229,6 +199,6 @@ class customService extends Finsemble.baseService {
 	}
 }
 
-const serviceInstance = new customService();
+const serviceInstance = new LoggingExportService();
 
 serviceInstance.start();
