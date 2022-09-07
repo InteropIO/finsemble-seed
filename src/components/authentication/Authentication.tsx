c20cf062d61480e775e81d54bc7bba7d8cad5030
Kris West <kris@cosaic.io>
2022-09-07 12:11:30 +0100

    Updated OAuth PKCA recipe for Finsemble 7.x

--- src/components/authentication/Authentication.tsx
+++ src/components/authentication/Authentication.tsx
@@ -1,10 +1,10 @@
 /*!
- * Copyright 2020 by ChartIQ, Inc.
+ * Copyright 2022 by ChartIQ, Inc.
  * All rights reserved.
  *
- * This is a sample Finsemble Authentication Component written using React hooks. It is meant as a starting point
- * for you to build your own Authentication Component. Use the `useAuth()` react hook, or the Finsemble client API
- * to interact with Finsemble's authentication capabilities.
+ * This is a sample Finsemble Authentication Component for use with the OAuth Authorization code with PKCE flow. 
+ * It is meant as a starting point for you to build your own Authentication Component. See the oAuthPKCE.ts file
+ * for functionality specific to OAuth and PKCE. 
  *
  * See https://documentation.finsemble.com/tutorial-Authentication.html for a tutorial on how to use authentication.
  */
@@ -12,112 +12,90 @@
 import React, { useState, useEffect } from "react";
 import ReactDOM from "react-dom";
 import { FinsembleProvider } from "@finsemble/finsemble-ui/react/components/FinsembleProvider";
-import { useAuth, useAuthSimulator } from "@finsemble/finsemble-ui/react/hooks";
+import { useAuth } from "@finsemble/finsemble-ui/react/hooks";
+import { authorize, getToken, getUserInfo, AuthTokenDetails } from './oAuthPKCE'
 import "@finsemble/finsemble-ui/react/assets/css/finsemble.css";
 import "@finsemble/finsemble-ui/react/assets/css/dialogs.css";
 import "@finsemble/finsemble-ui/react/assets/css/authentication.css";
 import "../../../public/assets/css/theme.css";
+import "./Authentication.css";
+
+const log = (...args: any) => {
+	FSBL.Clients.Logger.log(...args);
+	console.log(...args);
+};
+
+const error = (...args: any) => {
+	FSBL.Clients.Logger.error(...args);
+	console.error(...args);
+};
 
 export const Authentication = () => {
 	// Make sure this dialog shows on top of the splash screen
 	useEffect(() => {
 		FSBL.Clients.WindowClient.bringToFront();
 	}, []);
-	const [username, setUsername] = useState("");
-	const [password, setPassword] = useState("");
-	const [payload, setPayload] = useState<{
-		username: string;
-		password: string;
-	} | null>(null);
-	const [error, setError] = useState<string | null>(null);
+
+	const [status, setStatus] = useState("Authenticating");
+
 	const { quitApplication, publishAuthorization } = useAuth();
-	const { sendToServer } = useAuthSimulator();
 
-	/**
-	 * With React Hooks we must use `useEffect()` for effects such as server calls. Here, we use
-	 * the "payload" state as a way to trigger an effect to authenticate the user against a back
-	 * end server. Whenever the value of payload changes, this effect will run. The value of payload
-	 * changes when the form is submitted (see <form> element in the jsx).
-	 *
-	 * In this example we're using Finsemble's useAuthSimulator() hook to simulate a back end server.
-	 * This hook will allow any username/password combination by returning {result:"ok"}. But it will
-	 * return {error: "error text"} if either username or password are left blank.
-	 */
 	useEffect(() => {
-		if (!payload) return;
-
 		const authenticate = async () => {
-			const response = await sendToServer(payload.username, payload.password);
-			if (response.result === "ok") {
-				/**
-				 * This is the most important step. Once your back end server has authenticated the user
-				 * call publishAuthorization() from the useAuth() hook. The first parameter (username) is
-				 * required. The second parameter (credentials) is option. Credentials can contain anything
-				 * that is useful for session management, such as user ID, tokens, etc.
-				 */
-				publishAuthorization(payload.username, { username: payload.username });
-			} else if (response.error) {
-				setError(response.error);
+			// If your OAuth provider has an SDK you can replace the code sections with code from the SDK.
+			// You will still need to pass a username and credentials to publishAuthorization() as this allows Finsemble to access them
+			try {
+				const currentLocation = new URL(window.location.href);
+				const authorizationCode = currentLocation.searchParams.get("code");
+
+				// check for the authentication code in the search params,
+				// if it doesn't exist then we need to do the authorization step else skip it and continue to get the token
+				if (!authorizationCode) {
+					authorize();
+				} else {
+					const token: AuthTokenDetails = await getToken();
+					const accessToken = token.access_token;
+					const userInfo = await getUserInfo({ accessToken });
+					const username = userInfo.sub;
+
+					/**
+					 * This is the most important step. Once your back end server has authenticated the user
+					 * call publishAuthorization() from the useAuth() hook. The first parameter (username) is
+					 * required. The second parameter (credentials) is optional. Credentials can contain anything
+					 * that is useful for session management, such as user ID, tokens, etc.
+					 */
+					publishAuthorization(username, { token, userInfo });
+
+				}
+			} catch (err) {
+				error(err)
+				setStatus("Auth failed!")
 			}
+
 		};
 
 		authenticate();
-	}, [payload]);
-
-	const submit = () => {
-		setPayload({ username: username, password: password });
-	};
+	}, []);
 
 	/**
-	 * What follows is a cookie-cutter form written in React Hooks style. There is nothing here that
-	 * is proprietary to Finsemble. Your form should be built as needed to support your login process.
-	 * CSS Styles are imported from "Authentication.css" (see imports above).
+	 * What follows is a cookie-cutter form, written in React Hooks style, that is displayed before and
+	 * after any redirects to your login form (if no redirect is required, only this form will be displayed 
+	 * to the user). Hence, it may display a spinner or similar while requests are made and could be used
+	 * to display details of any errors that occur during auth.
+	 * 
+	 * There is nothing here that is proprietary to Finsemble. Your form should be built as needed to support 
+	 * your login process. 
+	 * 
+	 * CSS Styles are imported from "Authentication.css" & Finsemble's "authentication.css" (see imports).
 	 */
 	return (
 		<>
 			<div className="fsbl-auth-top">
-				<img className="fsbl-company-logo" />
 				<div className="fsbl-close">
 					<i className="ff-close" onClick={quitApplication}></i>
 				</div>
 			</div>
-
-			<div className="fsbl-auth-wrapper">
-				<div className="fsbl-auth-logo"></div>
-				{error && <div className="fsbl-input-error-message">{error}</div>}
-				<form
-					onSubmit={(event) => {
-						submit();
-						event.preventDefault();
-					}}
-				>
-					<input
-						className="fsbl-auth-input"
-						type="text"
-						name="username"
-						placeholder="Username"
-						value={username}
-						onChange={(event) => {
-							setUsername(event.target.value);
-						}}
-					/>
-
-					<input
-						className="fsbl-auth-input"
-						type="password"
-						name="password"
-						placeholder="Password"
-						value={password}
-						onChange={(event) => {
-							setPassword(event.target.value);
-						}}
-					/>
-
-					<button type="submit" className="fsbl-button fsbl-button-affirmative">
-						Login
-					</button>
-				</form>
-			</div>
+			<h1>{status}</h1>
 		</>
 	);
 };
