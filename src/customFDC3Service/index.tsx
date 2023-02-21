e3277eb0d79162f734bd779feef8b636d738604a
Kris West <kris@cosaic.io>
2023-02-21 21:57:57 +0000

    cleanup

--- src/customFDC3Service/index.tsx
+++ src/customFDC3Service/index.tsx
@@ -19,16 +19,7 @@ const main = () => {
 		"Channel 7": { "name": "Channel 7", "color": "#398572", "glyph": "7" },
 		"Channel 8": { "name": "Channel 8", "color": "#853556", "glyph": "8" }
 	};
-	// [
-	// 	{"id": "Channel 1", "metadata": { "name": "Channel 1", "color": "#8781BD", "glyph": "1" }},
-	// 	{"id": "Channel 2", "metadata": { "name": "Channel 2", "color": "#FFE035", "glyph": "2" }},
-	// 	{"id": "Channel 3", "metadata": { "name": "Channel 3", "color": "#89D803", "glyph": "3" }},
-	// 	{"id": "Channel 4", "metadata": { "name": "Channel 4", "color": "#FE6262", "glyph": "4" }},
-	// 	{"id": "Channel 5", "metadata": { "name": "Channel 5", "color": "#2DACFF", "glyph": "5" }},
-	// 	{"id": "Channel 6", "metadata": { "name": "Channel 6", "color": "#FFA200", "glyph": "6" }},
-	// 	{"id": "Channel 7", "metadata": { "name": "Channel 7", "color": "#398572", "glyph": "7" }},
-	// 	{"id": "Channel 8", "metadata": { "name": "Channel 8", "color": "#853556", "glyph": "8" }}
-	// ];
+
 	initialState[STATE_DISTRIBUTED_STORE_CHECKTIME_FIELD] = Date.now();
 	initialState[STATE_DISTRIBUTED_STORE_ACTIVEWINDOWNAME_FIELD] = null;
 	
