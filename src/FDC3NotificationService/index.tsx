99bfd2f79627f0bbf221295d2354b349d52558a8
Gerard L. Petersen <gerard@cosaic.io>
2023-02-23 13:42:11 +0000

    PR Fixes

--- src/FDC3NotificationService/index.tsx
+++ src/FDC3NotificationService/index.tsx
@@ -1,13 +1,9 @@
-import React from "react";
-// This line imports type declarations for Finsemble's globals such as FSBL and fdc3. You can ignore any warnings that it is defined but never used.
-// Please use global FSBL and fdc3 objects instead of importing from finsemble-core.
 import {
 	AppIdentifier, Channel,
 	ChannelError,
 	Context,
 	IntentHandler,
-	StandardErrorCallback,
-	types
+	StandardErrorCallback
 } from "@finsemble/finsemble-core";
 import {
 	IAction,
